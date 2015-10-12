800c7c466aa190eb69252759f6167e91d21b51fd
Bennett Lin <bennettl@zillow.com>
2015-10-11 20:45:11 -0700

    YB-1 Basic Express and frontend React setup.

---
+++ public/js/test.jsx
@@ -0,0 +1,14 @@
+var Test = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Testing...
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Test />,
+    document.getElementById('container')
+);
