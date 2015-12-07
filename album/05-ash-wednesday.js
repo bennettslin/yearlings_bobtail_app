6b581e6538ce997f7eb02f01834ff111af69c127
Bennett Lin <bennettl@zillow.com>
2015-12-06 21:15:23 -0800

    YB-69 Mark start times for each verse in Ash Wednesday.

--- album/05-ash-wednesday.js
+++ album/05-ash-wednesday.js
@@ -12,155 +12,155 @@ exports.song = {
                     verse: 'O Catherine! My Catherine! our futile trip is done'
                 },
                 {
-                    time: 0,
+                    time: 6,
                     verse: 'where it commenced: in my bookless room,'
                 },
                 {
-                    time: 0,
+                    time: 10,
                     verse: 'Ulysses\' tomb, surely buried she\'d be!'
                 },
                 {
-                    time: 0,
+                    time: 16,
                     verse: 'First we laid Tourette\'s, next regrets,'
                 },
                 {
-                    time: 0,
+                    time: 18,
                     verse: 'the last day she rose up and left, hers the sole assent…'
                 },
                 {
-                    time: 0,
+                    time: 23,
                     verse: 'But saving herself strictly for marriage, or really from me?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 30,
                     verse: 'Oh and for Catherine, St. Catherine of God,'
                 },
                 {
-                    time: 0,
+                    time: 34,
                     verse: 'I purge, with a dirge she\'d deride,'
                 },
                 {
-                    time: 0,
+                    time: 38,
                     verse: 'with sadness, sad for what I am not:'
                 },
                 {
-                    time: 0,
+                    time: 42,
                     verse: 'a soul to share in her eternal life.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 46,
                     verse: 'Oh Catherine, pressed for an opinion, sighed,'
                 },
                 {
-                    time: 0,
+                    time: 50,
                     verse: '"My, what wit, man," putting down her Joyce,'
                 },
                 {
-                    time: 0,
+                    time: 54,
                     verse: 'with "Joyce" oblivious to the trump she just played.'
                 },
                 {
-                    time: 0,
+                    time: 62,
                     verse: 'Couldn\'t heaven, her heaven,'
                 },
                 {
-                    time: 0,
+                    time: 65,
                     verse: 'brimming over with smart boys, fit just one art boy'
                 },
                 {
-                    time: 0,
+                    time: 69,
                     verse: 'who tried but failed to win her heart poised with brains?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 76,
                     verse: 'And as I pace, stranded here outside her Shelta,'
                 },
                 {
-                    time: 0,
+                    time: 80,
                     verse: 'in famous raincoat unknown to roam,'
                 },
                 {
-                    time: 0,
+                    time: 84,
                     verse: 'my girl in glasses went clear—my Tekakwitha—'
                 },
                 {
-                    time: 0,
+                    time: 8,
                     verse: 'for I lack the wit to spare a safe Algonquin abode.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 95,
                     verse: 'Well yes, I faked this "X", I should confess,'
                 },
                 {
-                    time: 0,
+                    time: 99,
                     verse: 'using some old used cigarette.'
                 },
                 {
-                    time: 0,
+                    time: 104,
                     verse: '"So you\'d cheat your way into heaven," she might protest.'
                 },
                 {
-                    time: 0,
+                    time: 111,
                     verse: 'Well no, I know it\'s wrong, but I did burn my palms,'
                 },
                 {
-                    time: 0,
+                    time: 115,
                     verse: 'I guess that butt was still fresh.'
                 },
                 {
-                    time: 0,
+                    time: 119,
                     verse: 'Et Catherine vaut bien une messe…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 123,
                     verse: 'On this sad day to mourn'
                 },
                 {
-                    time: 0,
+                    time: 125,
                     verse: 'us wretched souls cursed to be born,'
                 },
                 {
-                    time: 0,
+                    time: 128,
                     verse: 'a spark zips through my head to be'
                 },
                 {
-                    time: 0,
+                    time: 130,
                     verse: 'mistaken for Catholic with some smeared ash;'
                 },
                 {
-                    time: 0,
+                    time: 136,
                     verse: 'to wear it like a tragic mask.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 138,
                     verse: 'On this Ash Wednesday morn,'
                 },
                 {
-                    time: 0,
+                    time: 140,
                     verse: 'in spreading mole my sickness borne'
                 },
                 {
-                    time: 0,
+                    time: 143,
                     verse: 'upon this head for all to see.'
                 },
                 {
-                    time: 0,
+                    time: 146,
                     verse: 'And maybe if Catherine sees, she\'ll ask,'
                 },
                 {
-                    time: 0,
+                    time: 151,
                     verse: '"So where\'d you attend Mass?"'
                 }
             ]
