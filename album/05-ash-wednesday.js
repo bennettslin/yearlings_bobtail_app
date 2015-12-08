2cb0b3028b5a1eae01bfc45f52d9a98f597cad8d
Bennett Lin <bennettl@zillow.com>
2015-12-07 21:04:37 -0800

    YB-70 Begin annotations for Ash Wednesday.

--- album/05-ash-wednesday.js
+++ album/05-ash-wednesday.js
@@ -1,34 +1,62 @@
 exports.song = {
     title: 'Ash Wednesday',
     speechBubbles: {
-        narrative: 'narrative 1',
-        personal: 'personal 1'
+        /**
+         In college, I dated a nice Catholic girl named Catherine, who didn't think much of my intelligence and broke up with me right before Ash Wednesday, the first day of Lent. So I decided to ash my own forehead and bump into her, thinking that if she could picture me as a good Catholic boy, I might get another chance. It didn't happen.
+         */
+        narrative: 'Ash Wednesday narrative description',
+        personal: 'personal 1',
+        /**
+         For a song about two people who couldn't be more different, I decided to juxtapose two musical genres that couldn't be more different, Arabic maqam and Renaissance polyphony. The Arabic quartertones were recorded by playing each instrument in two takes, the second time tuned a quarter tone flat. The shrieking whistles, which remind me of San Francisco's BART trains passing in opposite directions underground, were made by overblowing into recorders.
+         */
+        music: 'Ash Wednesday music description'
     },
     lyrics: {
         left: [
             [
                 {
                     time: 0,
+                    /**
+                     Walt Whitman's O Captain! My Captain! a reference to my dying chances with Catherine.
+                     */
                     verse: 'O Catherine! My Catherine! our futile trip is done'
                 },
                 {
                     time: 6,
+                    /**
+                     Glancing around my room one night, Catherine remarked that she didn't see any books. "I don't read," I said. In hindsight, that was probably the beginning of the end.
+                     */
                     verse: 'where it commenced: in my bookless room,'
                 },
                 {
                     time: 10,
+                    /**
+                     A reference to the popular "Who's buried in Grant's tomb" trick question, of course, but also to the masterpiece novel of James Joyce, whom she greatly admired.
+                     */
                     verse: 'Ulysses\' tomb, surely buried she\'d be!'
                 },
                 {
                     time: 16,
+                    /**
+                     Jesus was "laid to rest" on the first day.
+                     */
                     verse: 'First we laid Tourette\'s, next regrets,'
                 },
                 {
                     time: 18,
+                    /**
+                     Jesus also rose on the third day.
+                     */
+                    /**
+                     Also, "hers the sole ascent."
+                     */
                     verse: 'the last day she rose up and left, hers the sole assent…'
                 },
                 {
                     time: 23,
+                    /**
+                     We never had sex, given that she was extremely devout.
+                     */
                     verse: 'But saving herself strictly for marriage, or really from me?'
                 }
             ],
@@ -79,10 +107,14 @@ exports.song = {
             [
                 {
                     time: 76,
+                    /**
+                     "Her shelter," but also, Shelta is the secret language of the Irish Travellers, an itinerant community the vast majority of whom are Roman Catholic.
+                     */
                     verse: 'And as I pace, stranded here outside her Shelta,'
                 },
                 {
                     time: 80,
+
                     verse: 'in famous raincoat unknown to roam,'
                 },
                 {
