282c5baaf5c1ebea73fb9ed4ed6083933c1e913e
Bennett Lin <bennettl@zillow.com>
2016-01-18 20:42:25 -0800

    YB-00 Add times for Garryowen.

--- album/12-garryowen.js
+++ album/12-garryowen.js
@@ -8,153 +8,153 @@ exports.song = {
         left: [
             [
                 {
-                    time: 0,
+                    time: 17,
                     verse: 'Brushed her off tonight, to swipe meats and wine'
                 },
                 {
-                    time: 0,
+                    time: 20,
                     verse: 'with your crew of chefs and thieves.'
                 },
                 {
-                    time: 0,
+                    time: 22,
                     verse: 'Tonight the rubber off, she offered as a thought,'
                 },
                 {
-                    time: 0,
+                    time: 25,
                     verse: 'if you would opt for bed not street.'
                 },
                 {
-                    time: 0,
+                    time: 28,
                     verse: 'Well you\'re not me, Garryowen. Oh no, and'
                 },
                 {
-                    time: 0,
+                    time: 33,
                     verse: 'I can\'t speak for this life you\'ve chosen.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 39,
                     verse: 'Drunk pissed punks tryst, bustin\' cars with fists,'
                 },
                 {
-                    time: 0,
+                    time: 42,
                     verse: 'the littlest fuss will incite your rage.'
                 },
                 {
-                    time: 0,
+                    time: 44,
                     verse: 'In you come, piss drunk, smug in smegma crust,'
                 },
                 {
-                    time: 0,
+                    time: 47,
                     verse: 'since hummers aren\'t love, or so she\'s claimed…'
                 },
                 {
-                    time: 0,
+                    time: 50,
                     verse: 'Well see, Garryowen, now no one,'
                 },
                 {
-                    time: 0,
+                    time: 56,
                     verse: 'buys your grief as the boy unchosen.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 61,
                     verse: 'So the celibate you\'ll play, self-medicated,'
                 },
                 {
-                    time: 0,
+                    time: 64,
                     verse: 'stewing in hatred for the world.'
                 },
                 {
-                    time: 0,
+                    time: 67,
                     verse: 'Slammed in the well, you\'ll glimpse yourself'
                 },
                 {
-                    time: 0,
+                    time: 70,
                     verse: 'with choler turned up, cringing in shame.'
                 },
                 {
-                    time: 0,
+                    time: 73,
                     verse: 'As claws you built from scratches, with your bilious malice,'
                 },
                 {
-                    time: 0,
+                    time: 75,
                     verse: 'fill up a package strapped to your scapegirl,'
                 },
                 {
-                    time: 0,
+                    time: 78,
                     verse: 'You\'ll slap her down, and push her out,'
                 },
                 {
-                    time: 0,
+                    time: 81,
                     verse: 'a crumpled castaway!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 93,
                     verse: 'Your rabid wit unleashed, Garryowen, to no end,'
                 },
                 {
-                    time: 0,
+                    time: 100,
                     verse: 'you\'d make her strip bare just to watch her freeze.'
                 },
                 {
-                    time: 0,
+                    time: 106,
                     verse: 'You won\'t always have her for your pet.'
                 },
                 {
-                    time: 0,
+                    time: 111,
                     verse: 'Someday she won\'t be there; someday yet!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 116,
                     verse: 'Final trip on a tanker, you\'ll drop your anger'
                 },
                 {
-                    time: 0,
+                    time: 120,
                     verse: 'to which you\'ve chained her; she\'ll sink into'
                 },
                 {
-                    time: 0,
+                    time: 123,
                     verse: 'the briny end. Now that you\'re cleansed,'
                 },
                 {
-                    time: 0,
+                    time: 126,
                     verse: 'aren\'t you the faltering dear?'
                 },
                 {
-                    time: 0,
+                    time: 128,
                     verse: 'They told her, "Never help a self-abuser'
                 },
                 {
-                    time: 0,
+                    time: 131,
                     verse: 'see himself as well in you."'
                 },
                 {
-                    time: 0,
+                    time: 134,
                     verse: 'You\'d smite your kitten good, galled that smitten'
                 },
                 {
-                    time: 0,
+                    time: 137,
                     verse: 'she would opt just not to hear!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 139,
                     verse: 'Toora-loora-li, Garryowen, who owes no one,'
                 },
                 {
-                    time: 0,
+                    time: 145,
                     verse: 'becomes someone\'s last-ditch alibi.'
                 },
                 {
-                    time: 0,
+                    time: 150,
                     verse: 'You\'re a purer lie…'
                 }
             ]
