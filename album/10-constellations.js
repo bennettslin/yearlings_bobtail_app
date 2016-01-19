4958d98c0b22265e057fbc6051a3268a684ee165
Bennett Lin <bennettl@zillow.com>
2016-01-18 17:44:19 -0800

    YB-00 Add times for Constellations.

--- album/10-constellations.js
+++ album/10-constellations.js
@@ -8,147 +8,147 @@ exports.song = {
         left: [
             [
                 {
-                    time: 0,
+                    time: 4,
                     verse: 'reverends, paused stiff in tandem.'
                 },
                 {
-                    time: 0,
+                    time: 7,
                     verse: 'Heavin\' some, Ana whispures a low sigh, bein\'…'
                 },
                 {
-                    time: 0,
+                    time: 11,
                     verse: 'of de exempt, exit us the Viking den…'
                 },
                 {
-                    time: 0,
+                    time: 19,
                     verse: 'Walkyriens: to Walhall o\' mead-Tussin!'
                 },
                 {
-                    time: 0,
+                    time: 22,
                     verse: 'Quouffs uppraissed, this boundless Kalevalley ends.'
                 },
                 {
-                    time: 0,
+                    time: 26,
                     verse: 'We ride as Finnugreec tongue Lapps are highmen.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 34,
                     verse: 'Makeyars mygrate, from among ol\' psealed off drains.'
                 },
                 {
-                    time: 0,
+                    time: 41,
                     verse: 'Then down the steppes we charged,'
                 },
                 {
-                    time: 0,
+                    time: 45,
                     verse: 'Drinkhis Khan, once Toomujin, has slain the teeto-Tartars!'
                 },
                 {
-                    time: 0,
+                    time: 49,
                     verse: 'Tomer-layin\' the seed, Howie led the feat of avatars…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 56,
                     verse: 'Indus-trail sewerce, questurns Indo Hairapins:'
                 },
                 {
-                    time: 0,
+                    time: 60,
                     verse: 'Forsi ahuro or Sensegreat devas\'dayshone?'
                 },
                 {
-                    time: 0,
+                    time: 64,
                     verse: 'To soma up, our-youn\' invedas unsolft quarry rest…'
                 },
                 {
-                    time: 0,
+                    time: 70,
                     verse: 'Ourevian battles spent, a\'duct Uighuress.'
                 },
                 {
-                    time: 0,
+                    time: 75,
                     verse: 'Are Melee tapirs the dusc-horse yet…?'
                 },
                 {
-                    time: 0,
+                    time: 79,
                     verse: 'From the Khlmair city Ingcar Wet, to war: scimitary of\'fence!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 86,
                     verse: 'Pour sod, fourfeit tundr\'s Siburial plains…'
                 },
                 {
-                    time: 0,
+                    time: 94,
                     verse: 'Upridge o\' lawn trod on by forc\'d nations sank…'
                 },
                 {
-                    time: 0,
+                    time: 101,
                     verse: 'But past a cross Bearing Straight,'
                 },
                 {
-                    time: 0,
+                    time: 103,
                     verse: 'herows—ourauras left—to Audamn\'s grave.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 109,
                     verse: 'And now without a chart,'
                 },
                 {
-                    time: 0,
+                    time: 112,
                     verse: 'just the winds and the constellations known by heart,'
                 },
                 {
-                    time: 0,
+                    time: 116,
                     verse: 'I\'ll sail this sea, there is only me and my northern star!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 124,
                     verse: 'The mistodawn, ha\'nt\'d by sobre-trooth tiedgirl cries,'
                 },
                 {
-                    time: 0,
+                    time: 128,
                     verse: 'sunk in tarpid lies.'
                 },
                 {
-                    time: 0,
+                    time: 131,
                     verse: 'In Hollowscene broughtour ancister toughtim rite,'
                 },
                 {
-                    time: 0,
+                    time: 135,
                     verse: 'he sees Lowkey and Freyed!'
                 },
                 {
-                    time: 0,
+                    time: 139,
                     verse: 'Noworse and Nunavatter collide, Leif as the Red\'s son arrives.'
                 },
                 {
-                    time: 0,
+                    time: 147,
                     verse: 'How old are these mummeries I have left to mine?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 154,
                     verse: 'Pullar switch, in-som\'umiak skims through channels snow blitz\'d…'
                 },
                 {
-                    time: 0,
+                    time: 161,
                     verse: 'Then pounds arrhythmetic heart,'
                 },
                 {
-                    time: 0,
+                    time: 165,
                     verse: 'Enguished buzzereds come unsated find Rumspring is parched.'
                 },
                 {
-                    time: 0,
+                    time: 169,
                     verse: 'In mouth Deseret, black Letter-Day Sensed ahead… "Rejoyce!" bellow the'
                 }
             ]
