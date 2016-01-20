0c383df9a62568162153775b95e2fbbf953c3410
Bennett Lin <bennettl@zillow.com>
2016-01-19 20:52:49 -0800

    YB-00 Add times to Kyon?

--- album/14-kyon.js
+++ album/14-kyon.js
@@ -8,137 +8,137 @@ exports.song = {
         right: [
             [
                 {
-                    time: 0,
+                    time: 12,
                     verse: 'So you fled to her nest on this stork,'
                 },
                 {
-                    time: 0,
+                    time: 17,
                     verse: 'history shed, having never grown your horns.'
                 },
                 {
-                    time: 0,
+                    time: 22,
                     verse: 'A present sent to her of a newborn…'
                 },
                 {
-                    time: 0,
+                    time: 26,
                     verse: 'So baby, tell me then, when you take in her breasts,'
                 },
                 {
-                    time: 0,
+                    time: 31,
                     verse: 'are you suckling to be fed?'
                 },
                 {
-                    time: 0,
+                    time: 36,
                     verse: 'Is it just her womb you bed?'
                 },
                 {
-                    time: 0,
+                    time: 40,
                     verse: 'Well, have some more…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 47,
                     verse: 'And how you always burned, for the slight;'
                 },
                 {
-                    time: 0,
+                    time: 50,
                     verse: 'spurned at first, you couldn\'t let it die!'
                 },
                 {
-                    time: 0,
+                    time: 53,
                     verse: 'You\'d nurse your bruises: why, the nerve of me, to lie'
                 },
                 {
-                    time: 0,
+                    time: 56,
                     verse: 'in reserve while I had to learn to find'
                 },
                 {
-                    time: 0,
+                    time: 58,
                     verse: 'all the points you had! Then you\'d resent'
                 },
                 {
-                    time: 0,
+                    time: 61,
                     verse: 'plunders past that forced my defence…'
                 },
                 {
-                    time: 0,
+                    time: 63,
                     verse: 'But dear, let\'s make a pact: if you\'re single then,'
                 },
                 {
-                    time: 0,
+                    time: 66,
                     verse: 'I get you back? And…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 68,
                     verse: 'I\'ll be stronger—not a bother, more modest, I promise—'
                 },
                 {
-                    time: 0,
+                    time: 73,
                     verse: 'to parent my knight-errant as the sole keeper of his sheep herd.'
                 },
                 {
-                    time: 0,
+                    time: 78,
                     verse: 'And when sobered by the time-lapse to spring forward on a fallback…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 85,
                     verse: 'my ticklish piglet, you\'ll bounce back to me, giggly, having fully gorged yourself.'
                 },
                 {
-                    time: 0,
+                    time: 93,
                     verse: 'Bursting belly told by extra holes punched in your belt.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 104,
                     verse: 'A shank\'s dustless silhouette bares the sill\'s sheen,'
                 },
                 {
-                    time: 0,
+                    time: 109,
                     verse: 'your severed stinger left in me never lost its sting.'
                 },
                 {
-                    time: 0,
+                    time: 116,
                     verse: 'But other nights you\'d lap my wound clean'
                 },
                 {
-                    time: 0,
+                    time: 121,
                     verse: 'to leave me as before, with only instinct to adore.'
                 },
                 {
-                    time: 0,
+                    time: 127,
                     verse: 'Besides, a you-shaped pussing sore'
                 },
                 {
-                    time: 0,
+                    time: 132,
                     verse: 'bleeds just once, then never more…'
                 },
                 {
-                    time: 0,
+                    time: 138,
                     verse: 'So are we agreed, honeybee? Come for me? At thirty-three?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 174,
                     verse: 'You say you woke one crystal morning, to find all those thoughts had gone away:'
                 },
                 {
-                    time: 0,
+                    time: 180,
                     verse: 'your fear of death, your fear of never dying, and sadness for what it was.'
                 },
                 {
-                    time: 0,
+                    time: 186,
                     verse: 'You just knew Bobtail\'s your priority, so for Bobtail I\'ll gladly wait.'
                 },
                 {
-                    time: 0,
+                    time: 192,
                     verse: 'And darling, don\'t you worry, these were only practise cuts…'
                 }
             ]
