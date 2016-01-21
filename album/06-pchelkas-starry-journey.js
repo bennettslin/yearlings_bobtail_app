8d0de657055034e547a7992fa96fb2d3b4e06c42
Bennett Lin <bennettl@zillow.com>
2016-01-20 19:20:39 -0800

    YB-00 Add times to Pchelka's Starry Journey.

--- album/06-pchelkas-starry-journey.js
+++ album/06-pchelkas-starry-journey.js
@@ -14,257 +14,253 @@ exports.song = {
             ],
             [
                 {
-                    time: 0,
+                    time: 11,
                     verse: 'Mother, woken from her nap,'
                 },
                 {
-                    time: 0,
+                    time: 13,
                     verse: 'hears a piercing scream and thunders,'
                 },
                 {
-                    time: 0,
+                    time: 15,
                     verse: '"Why\'d you hit your baby brother, when he so looks up to you!'
                 },
                 {
-                    time: 0,
+                    time: 19,
                     verse: 'Don\'t you remember days past,'
                 },
                 {
-                    time: 0,
+                    time: 21,
                     verse: 'when you wanted to buy a fortress'
                 },
                 {
-                    time: 0,
+                    time: 23,
                     verse: 'but your daddy couldn\'t afford it,'
                 },
                 {
-                    time: 0,
+                    time: 25,
                     verse: 'so you brought him into your room?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 27,
                     verse: 'You stowed away beneath your captain bed.'
                 },
                 {
-                    time: 0,
+                    time: 30,
                     verse: 'We didn\'t hear a peep throughout the entire weekend!"'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 34,
                     verse: '<em>Poor Sam Peabody!</em>'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 38,
                     verse: 'Oh brother, when all you know is bleat,'
                 },
                 {
-                    time: 0,
+                    time: 40,
                     verse: 'they\'ll catch you by your cape and beat you,'
                 },
                 {
-                    time: 0,
+                    time: 42,
                     verse: 'threatening never to release you till you learn to kick it back.'
                 },
                 {
-                    time: 0,
+                    time: 46,
                     verse: 'Feigning sick, I chanced to read'
                 },
                 {
-                    time: 0,
+                    time: 48,
                     verse: 'these words in a comic to me speaking:'
                 },
                 {
-                    time: 0,
+                    time: 50,
                     verse: '"Are you tired of being the weakling?"…'
                 },
                 {
-                    time: 0,
+                    time: 52,
                     verse: 'I clipped and mailed the ad.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 54,
                     verse: 'Soon send away offers were piling up high,'
                 },
                 {
-                    time: 0,
+                    time: 57,
                     verse: 'and then one day one came and made me cry and cry.'
                 },
                 {
-                    time: 0,
+                    time: 61,
                     verse: 'Red pouches unspent for years, at once counted and sent,'
                 },
                 {
-                    time: 0,
+                    time: 64,
                     verse: 'while all the weeks I waited, weighing those words in bold text:'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 68,
                     verse: '"Are you a Beyonder sent down to be Earth-fostered;'
                 },
                 {
-                    time: 0,
+                    time: 71,
                     verse: 'do you often feel like a wanderer lost?"'
                 },
                 {
-                    time: 0,
+                    time: 76,
                     verse: 'But waiting for Goddard, our two impatient paupers,'
                 },
                 {
-                    time: 0,
+                    time: 79,
                     verse: 'Gogo and his Didi, sauntered off…'
                 },
                 {
-                    time: 0,
+                    time: 83,
                     verse: 'Till in hunger they were led into Huntsville and fed,'
                 },
                 {
-                    time: 0,
+                    time: 86,
                     verse: 'dragged to sled by hunter\'s belt and launched!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 90,
                     verse: '"Far from home and shaped like common men,'
                 },
                 {
-                    time: 0,
+                    time: 93,
                     verse: 'Beyonders bleed the most, their roots unknown to them.'
                 },
                 {
-                    time: 0,
+                    time: 98,
                     verse: 'We\'ve received your fee, and per the tests,'
                 },
                 {
-                    time: 0,
+                    time: 101,
                     verse: 'you are indeed a seed from that nebular nest.'
                 },
                 {
-                    time: 0,
+                    time: 105,
                     verse: 'Is this too much at once? We\'re trusting mum\'s to be the word.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 112,
                     verse: 'Beyonders pine for a love no mortals give,'
                 },
                 {
-                    time: 0,
+                    time: 116,
                     verse: 'one they\'ll find only once we build the mothership.'
                 },
                 {
-                    time: 0,
+                    time: 119,
                     verse: 'Your monthly tithe shall fund good tidings soon to come.'
                 },
                 {
-                    time: 0,
+                    time: 123,
                     verse: 'But till then, here\'s how you\'re to live…"'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 134,
                     verse: 'So thus we learned to act the dunce'
                 },
                 {
-                    time: 0,
+                    time: 136,
                     verse: 'as spies among the carnal fallen,'
                 },
                 {
-                    time: 0,
+                    time: 138,
                     verse: 'twin friars cast here in pollens from a star\'s placenta sac.'
                 },
                 {
-                    time: 0,
+                    time: 141,
                     verse: 'Braced for an earth stay unloved,'
                 },
                 {
-                    time: 0,
+                    time: 143,
                     verse: 'twits daily murdered for their virtue.'
                 },
                 {
-                    time: 0,
+                    time: 145,
                     verse: 'Mirthful, the merciless would hurt you,'
                 },
                 {
-                    time: 0,
+                    time: 147,
                     verse: 'as omerta turned their backs.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 149,
                     verse: 'Stung by venomous vipers, our muted youths lapsed;'
                 },
                 {
-                    time: 0,
+                    time: 153,
                     verse: 'we knew the shortest cyphers are the toughest ones to crack.'
                 },
                 {
-                    time: 0,
+                    time: 156,
                     verse: 'But every hero has a heel, we\'d realise, blind before the squad.'
                 },
                 {
-                    time: 0,
+                    time: 160,
                     verse: 'The only spies who get to feel love first unearth their own plots!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 164,
                     verse: 'Mushing onward, Mushka\'s dragged by collar'
                 },
                 {
-                    time: 0,
+                    time: 167,
                     verse: 'to his slaughter under undying dawn.'
                 },
                 {
-                    time: 0,
+                    time: 171,
                     verse: 'Choked, he\'s soon a goner, no one heard his hollers;'
                 },
                 {
-                    time: 0,
+                    time: 175,
                     verse: 'this pack honours only the idiot\'s rod.'
                 },
                 {
-                    time: 0,
+                    time: 178,
                     verse: 'Next cabins come equipped with pentobarbitone syringe,'
                 },
                 {
-                    time: 0,
+                    time: 182,
                     verse: 'nothing too rich for glorious cosmonauts!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 186,
                     verse: 'I\'m sorry I bailed, Xiao Xiang, mush on…'
                 },
                 {
-                    time: 0,
+                    time: 189,
                     verse: 'I couldn\'t ditch my wails, I needed the pod.'
                 },
                 {
-                    time: 0,
-                    verse: '"Kids say you\'re hopped on drugs, son.'
+                    time: 193,
+                    verse: '"Kids say you\'re hopped on drugs, son, with Mum we\'ll need a word."'
                 },
                 {
-                    time: 0,
-                    verse: 'With Mum we\'ll need a word."'
-                },
-                {
-                    time: 0,
+                    time: 200,
                     verse: 'Mushka, per their bargain, I can\'t ever return.'
                 }
             ]
