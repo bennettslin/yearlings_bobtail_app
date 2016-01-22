c32874749ee2a565f4b69149d51a0c2f5cfd6d29
Bennett Lin <bennettl@zillow.com>
2016-01-21 21:29:17 -0800

    YB-0 Add times for On a Golden Cord.

--- album/13-on-a-golden-cord.js
+++ album/13-on-a-golden-cord.js
@@ -8,211 +8,233 @@ exports.song = {
         left: [
             [
                 {
-                    time: 0,
-                    verse: 'On a golden cord, once worn, now shorn,'
+                    time: 5,
+                    verse: 'On a golden cord,'
                 },
                 {
-                    time: 0,
+                    time: 7,
+                    verse: 'once worn, now shorn,'
+                },
+                {
+                    time: 9,
                     verse: 'safely kept with bread and pocket lint.'
                 },
                 {
-                    time: 0,
-                    verse: 'Not of its own accord, but broken for what its promise meant.'
+                    time: 12,
+                    verse: 'Not of its own accord,'
                 },
                 {
-                    time: 0,
+                    time: 15,
+                    verse: 'but broken for what its promise meant.'
+                },
+                {
+                    time: 18,
                     verse: 'And so she said, "Now it\'s yours again…'
-                },
+                }
+            ],
+            [
                 {
-                    time: 0,
+                    time: 26,
                     verse: 'And how you look tonight!'
                 },
                 {
-                    time: 0,
-                    verse: 'So polite, yet resigned, with wounds dressed and feelings put away.'
+                    time: 28,
+                    verse: 'So polite, yet resigned,'
                 },
                 {
-                    time: 0,
-                    verse: 'Full of patient replies… But how can I trust a word you say?'
+                    time: 31,
+                    verse: 'with wounds dressed and feelings put away.'
                 },
                 {
-                    time: 0,
+                    time: 33,
+                    verse: 'Full of patient replies…'
+                },
+                {
+                    time: 36,
+                    verse: 'But how can I trust a word you say?'
+                },
+                {
+                    time: 40,
                     verse: 'So spill away.. and please, dear, you have no more secrets saved.'
                 },
                 {
-                    time: 0,
+                    time: 47,
                     verse: 'I\'m sorry, but I went through your mail today…"'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 54,
                     verse: 'Immerse the moulted form in holy foggy night,'
                 },
                 {
-                    time: 0,
+                    time: 57,
                     verse: 'emerged beside a pauper\'s grave from dreams.'
                 },
                 {
-                    time: 0,
+                    time: 61,
                     verse: 'The wraith surveys the widow on his pyre;'
                 },
                 {
-                    time: 0,
+                    time: 64,
                     verse: 'torched the bellyaches on suttee.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 65,
                     verse: 'Had I grown dispirited in all this act,'
                 },
                 {
-                    time: 0,
+                    time: 68,
                     verse: 'by disparate elements, like kindling gathered up?'
                 },
                 {
-                    time: 0,
+                    time: 72,
                     verse: 'Guilt pardons the spectre taunting twin streetlamps;'
                 },
                 {
-                    time: 0,
+                    time: 75,
                     verse: 'hatchet man who\'s burnt enough.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 76,
                     verse: 'But a precious gift as a shedded chain,'
                 },
                 {
-                    time: 0,
+                    time: 79,
                     verse: 'has lain to rust, one more cruel memento.'
                 },
                 {
-                    time: 0,
+                    time: 83,
                     verse: 'Then if life were real, not just drunken play…'
                 },
                 {
-                    time: 0,
+                    time: 86,
                     verse: 'But what do I know?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 87,
                     verse: 'Just that those playful pups with their hateful fangs'
                 },
                 {
-                    time: 0,
+                    time: 90,
                     verse: 'knew the restraint of those born as hunters,'
                 },
                 {
-                    time: 0,
+                    time: 93,
                     verse: 'while this supposed dove chafing in his cage,'
                 },
                 {
-                    time: 0,
+                    time: 96,
                     verse: 'just learned he\'s pecked to death his precious other.'
                 },
                 {
-                    time: 0,
+                    time: 98,
                     verse: 'But should it matter since you loved her once,'
                 },
                 {
-                    time: 0,
+                    time: 101,
                     verse: 'when you really thought the world that simple?'
                 },
                 {
-                    time: 0,
+                    time: 104,
                     verse: 'And who\'s to judge how much you cared because'
                 },
                 {
-                    time: 0,
+                    time: 107,
                     verse: 'the gesture then was just a little more worth working for?…'
                 },
                 {
-                    time: 0,
+                    time: 113,
                     verse: 'Now the jester teeters towards…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 116,
                     verse: 'the taunting, streetswept dawn.'
                 },
                 {
-                    time: 0,
-                    verse: 'Thoughts spawned then dropped, of gathering up the suttee dust.'
+                    time: 118,
+                    verse: 'Thoughts spawned then dropped,'
                 },
                 {
-                    time: 0,
+                    time: 121,
+                    verse: 'of gathering up the suttee dust.'
+                },
+                {
+                    time: 123,
                     verse: 'Some things just come to loss:'
                 },
                 {
-                    time: 0,
+                    time: 126,
                     verse: 'this bracelet, its lustre; me, my lust.'
                 },
                 {
-                    time: 0,
+                    time: 130,
                     verse: 'Or was I once, something not what I\'ve become?'
                 },
                 {
-                    time: 0,
+                    time: 137,
                     verse: 'Have I just killed the thing I love? '
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 144,
                     verse: 'Of fateful thoughts hung on a gilded clasp,'
                 },
                 {
-                    time: 0,
+                    time: 147,
                     verse: 'which this Ubermensch reavowed he\'d come to snip.'
                 },
                 {
-                    time: 0,
+                    time: 150,
                     verse: 'And a smarting head throbs once his brain\'s warring halves,'
                 },
                 {
-                    time: 0,
+                    time: 154,
                     verse: 'each attack with the proper fist.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 155,
                     verse: 'A sober plea finds the present you,'
                 },
                 {
-                    time: 0,
+                    time: 158,
                     verse: 'my alias drunk, in panic and starved for leads.'
                 },
                 {
-                    time: 0,
+                    time: 161,
                     verse: 'And will she label lies oaths we vowed we could,'
                 },
                 {
-                    time: 0,
+                    time: 164,
                     verse: 'with Garry\'s sole witness relieved?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 166,
                     verse: 'So a plan devised divides a slave once wed;'
                 },
                 {
-                    time: 0,
+                    time: 169,
                     verse: 'a bid for greatness\' sake, he\'s sold on as holy war.'
                 },
                 {
-                    time: 0,
+                    time: 172,
                     verse: 'When longed by nothing you then fight the best,'
                 },
                 {
-                    time: 0,
+                    time: 176,
                     verse: 'for kohl-eyed houris as reward.'
                 }
             ]
@@ -220,211 +242,235 @@ exports.song = {
         right: [
             [
                 {
-                    time: 0,
+                    time: 5,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 7,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 7,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 12,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 15,
                     verse: '…'
                 },
                 {
-                    time: 0,
-                    verse: '…'
-                },
-                {
-                    time: 0,
-                    verse: '…'
-                },
-                {
-                    time: 0,
-                    verse: '…'
-                },
-                {
-                    time: 0,
+                    time: 18,
                     verse: '…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 26,
+                    verse: '…'
+                },
+                {
+                    time: 28,
+                    verse: '…'
+                },
+                {
+                    time: 31,
+                    verse: '…'
+                },
+                {
+                    time: 33,
+                    verse: '…'
+                },
+                {
+                    time: 36,
+                    verse: '…'
+                },
+                {
+                    time: 40,
+                    verse: '…'
+                },
+                {
+                    time: 47,
+                    verse: '…'
+                }
+            ],
+            [
+                {
+                    time: 54,
                     verse: 'Returned a golden cord, this sole belonging signed.'
                 },
                 {
-                    time: 0,
+                    time: 57,
                     verse: 'Leave early, time\'s up, cross your date released.'
                 },
                 {
-                    time: 0,
+                    time: 61,
                     verse: 'The jailer waves a bit, though uninspired,'
                 },
                 {
-                    time: 0,
+                    time: 64,
                     verse: 'for the well-behaved on the wing.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 65,
                     verse: 'As I soak in spirits amidst wanted ads—'
                 },
                 {
-                    time: 0,
+                    time: 68,
                     verse: 'my desperate ailments tied in this shambler\'s dusk—'
                 },
                 {
-                    time: 0,
+                    time: 72,
                     verse: 'still hardened against her fawning, misty glance,'
                 },
                 {
-                    time: 0,
+                    time: 75,
                     verse: 'past this avenue Möbius.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 76,
                     verse: 'But the present slips past a debt unpaid,'
                 },
                 {
-                    time: 0,
+                    time: 79,
                     verse: 'that slays you once before you\'ll let it go.'
                 },
                 {
-                    time: 0,
+                    time: 83,
                     verse: 'And if night were peeled off the front of day…'
                 },
                 {
-                    time: 0,
+                    time: 86,
                     verse: 'But what do I know?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 87,
+                    verse: 'Odelay!'
+                },
+                {
+                    time: 90,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 93,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 96,
+                    verse: '…'
+                }
+            ],
+            [
+                {
+                    time: 98,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 101,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 104,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 107,
                     verse: '…'
                 },
                 {
-                    time: 0,
-                    verse: '…'
-                },
-                {
-                    time: 0,
-                    verse: '…'
-                },
-                {
-                    time: 0,
+                    time: 113,
                     verse: 'Mush! Mush!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 116,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 118,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 121,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 123,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 126,
                     verse: '…'
                 },
                 {
-                    time: 0,
+                    time: 130,
+                    verse: '…'
+                },
+                {
+                    time: 137,
                     verse: '…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 144,
                     verse: 'Once navels locked upon the wilted grass,'
                 },
                 {
-                    time: 0,
+                    time: 147,
                     verse: 'digging through her innie, the outie snug-to-fit.'
                 },
                 {
-                    time: 0,
+                    time: 150,
                     verse: 'Then the guardian knot of this frayed cord detached,'
                 },
                 {
-                    time: 0,
+                    time: 154,
                     verse: 'leaked the bandage upon her wrist.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 155,
                     verse: 'The former me might have left a clue'
                 },
                 {
-                    time: 0,
+                    time: 158,
                     verse: 'by trail of crumbs, which vanished as bartered feed.'
                 },
                 {
-                    time: 0,
+                    time: 161,
                     verse: 'Until we strangle cries knowing how we stood,'
                 },
                 {
-                    time: 0,
+                    time: 164,
                     verse: 'when buried whole with the deceased.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 166,
                     verse: 'Though, the master mind reminds us, straight ahead,'
                 },
                 {
-                    time: 0,
+                    time: 169,
                     verse: 'a bigger plate awaiting nulls one half full before.'
                 },
                 {
-                    time: 0,
+                    time: 172,
                     verse: 'And fond sighs of reunion I must rest;'
                 },
                 {
-                    time: 0,
+                    time: 176,
                     verse: 'forego like jewelry that she wore.'
                 }
             ]
