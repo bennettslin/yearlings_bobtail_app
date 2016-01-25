df47bab004a09ff0c13577da295ea52adbc22c10
Bennett Lin <bennettl@zillow.com>
2016-01-24 20:57:39 -0800

    YB-0 Add times to Vegan Proclamation.

--- album/17-vegan-proclamation.js
+++ album/17-vegan-proclamation.js
@@ -12,149 +12,149 @@ exports.song = {
                     verse: '<em>"If you\'re just starting out, you need to decide right now:</em>'
                 },
                 {
-                    time: 0,
+                    time: 3,
                     verse: '<em>either you stick with quality vinyl, or you buy only the bargain bin LPs;</em>'
                 },
                 {
-                    time: 0,
+                    time: 8,
                     verse: '<em>you can\'t have both. Because a scratched record will chip a good needle…</em>'
                 },
                 {
-                    time: 0,
+                    time: 12,
                     verse: '<em>and a chipped needle will scratch a good record…"</em>'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 15,
                     verse: 'So shall we, Odin, lightly tread'
                 },
                 {
-                    time: 0,
+                    time: 19,
                     verse: 'past the rape racks where our furor led,'
                 },
                 {
-                    time: 0,
+                    time: 23,
                     verse: 'to a soiled girl fouled on the false rib from our chest?'
                 },
                 {
-                    time: 0,
+                    time: 30,
                     verse: 'We baulked at our lives born unfair,'
                 },
                 {
-                    time: 0,
+                    time: 35,
                     verse: 'set to rock this pout from horse to chair,'
                 },
                 {
-                    time: 0,
+                    time: 39,
                     verse: 'as a tempered hammer cooled by striking the smouldering air…'
                 },
                 {
-                    time: 0,
+                    time: 46,
                     verse: 'Well, first pets always bear the worst,'
                 },
                 {
-                    time: 0,
+                    time: 50,
                     verse: 'mere primers on how not to hurt,'
                 },
                 {
-                    time: 0,
+                    time: 55,
                     verse: 'and now I know why the battered kitten purrs…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 60,
                     verse: '<em> "No, I\'ll smile because I believe in the big crunch,</em>'
                 },
                 {
-                    time: 0,
+                    time: 64,
                     verse: '<em>and meanwhile, the universe is a cyanided box…</em>'
                 },
                 {
-                    time: 0,
+                    time: 68,
                     verse: '<em>Because if we can\'t ever be known to be dead,</em>'
                 },
                 {
-                    time: 0,
+                    time: 71,
                     verse: '<em>we just might always be alive to God…"</em>'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 74,
                     verse: 'Torn straps unsalvaged sows have chewed,'
                 },
                 {
-                    time: 0,
+                    time: 78,
                     verse: 'form a trail which leads to a bloody broom,'
                 },
                 {
-                    time: 0,
+                    time: 82,
                     verse: 'its bristles scrape discharge encrusted on leather boots.'
                 },
                 {
-                    time: 0,
+                    time: 90,
                     verse: 'Through an alley strewn with burger shrouds,'
                 },
                 {
-                    time: 0,
+                    time: 94,
                     verse: 'a lone rallied youth strikes himself from his cloud,'
                 },
                 {
-                    time: 0,
+                    time: 98,
                     verse: 'reversing his stomach and his swastikas inside out…'
                 },
                 {
-                    time: 0,
+                    time: 106,
                     verse: 'Well, if first cuts are the worst ones bled…'
                 },
                 {
-                    time: 0,
+                    time: 110,
                     verse: 'then none by us now, to any flesh!'
                 },
                 {
-                    time: 0,
+                    time: 114,
                     verse: 'This shipwrecked world is large enough for us to make amends…'
                 },
                 {
-                    time: 0,
+                    time: 120,
                     verse: 'and to be good again…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 138,
                     verse: '"So all your leads have left and now you\'re sad, too?'
                 },
                 {
-                    time: 0,
+                    time: 146,
                     verse: 'Someday you\'ll breathe again; for one, you have to…'
                 },
                 {
-                    time: 0,
+                    time: 154,
                     verse: 'I don\'t know how where once I cowered I now could fly.'
                 },
                 {
-                    time: 0,
+                    time: 161,
                     verse: 'I still love you for the airholes, dear; and now, good night."'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 186,
                     verse: 'The stardust marks us, Odin, sparks zip through our heads…'
                 },
                 {
-                    time: 0,
+                    time: 194,
                     verse: 'And can we start as children? Would we want to then?'
                 },
                 {
-                    time: 0,
+                    time: 201,
                     verse: 'The diapered white dwarf, gurgling, curls up for his sleep…'
                 },
                 {
-                    time: 0,
+                    time: 209,
                     verse: 'Our ashed heads bow from chivalry to our Siva\'s feet…'
                 }
             ]
