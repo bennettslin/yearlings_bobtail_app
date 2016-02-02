456500f79eae9672d1d076da91620922c06f0b8a
Bennett Lin <bennettl@zillow.com>
2016-02-01 21:56:10 -0800

    YB-0 Separate Good Night Sita verses.

--- album/08-good-night-sita.js
+++ album/08-good-night-sita.js
@@ -9,7 +9,297 @@ exports.song = {
             [
                 {
                     time: 0,
-                    verse: '"Good night," she says or should I should you refuse by a gutter gurgling then we\'ll choose either never live yet live on or to love first without once knowing love when cesspit dead ends for further speech are these stuttered sentences we speak for this kid wearing no decoder ring Dutch courage wimped out to going Dutch but even no sponge nor a barnacle minds swept in unchartable tides they just always know to pry for the innermost matryoshka\'s heart of gold inside till then crack up another doll to find the chrysalis unchristened to all who\'d buy this aping jaw fixed to his skull swaggering down these barren exhibit halls but totems so revered when defined by ambrosia beers in time are seen with a focus grown clear and slowly the story erodes pidgin palaver exchanged can only transfer good faith hyped up like some disastrous play that starts with a lone ringing phone but even so just that it ever rang at all for if the tactic is to stall you\'re just ecstatic she called what this understudy sought from his number strutting on the wall we whine when first we got shunned then wise up and learn to shotgun last show\'s set still propped up so we\'ll breeze through our parts though each kind new ultimatum to his tribe left old and jaded the boy brave who cultivated brave rejection into art but died unfinished alas hotlines get busy just as thoughts slide to slitting wrists that hint of sampler cologne and a lass sniffing out these rote charms always rides in single go-karts as we wind an endless flowchart with every given answer a no and her veggie wishbone\'s short end in these clutching fingers of steel too untrusting to ever unpeel from a dummy steering wheel settled for hugging only curbs and having only tires squeal when boys who mope know the beauty of simply limping away blind as the chirping crosswalks guide where willows softly conspire when boys who cope only risk invites denied I sigh, "Good night."'
+                    verse: '"Good night"'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'she says or should I should you refuse'
+                },
+                {
+                    time: 0,
+                    verse: 'by a gutter gurgling then we\'ll choose'
+                },
+                {
+                    time: 0,
+                    verse: 'either never live yet live on or to'
+                },
+                {
+                    time: 0,
+                    verse: 'love first without once knowing love'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'when cesspit dead ends for further speech'
+                },
+                {
+                    time: 0,
+                    verse: 'are these stuttered sentences we speak'
+                },
+                {
+                    time: 0,
+                    verse: 'for this kid wearing no decoder ring'
+                },
+                {
+                    time: 0,
+                    verse: 'Dutch courage wimped out to going Dutch'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'but even no sponge'
+                },
+                {
+                    time: 0,
+                    verse: 'nor a barnacle minds'
+                },
+                {
+                    time: 0,
+                    verse: 'swept in unchartable tides'
+                },
+                {
+                    time: 0,
+                    verse: 'they just always know to pry'
+                },
+                {
+                    time: 0,
+                    verse: 'for the innermost matryoshka\'s'
+                },
+                {
+                    time: 0,
+                    verse: 'heart of gold inside'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'till then crack up another doll'
+                },
+                {
+                    time: 0,
+                    verse: 'to find the chrysalis unchristened to all'
+                },
+                {
+                    time: 0,
+                    verse: 'who\'d buy this aping jaw fixed to his skull'
+                },
+                {
+                    time: 0,
+                    verse: 'swaggering down these barren exhibit halls'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'till then crack up another doll'
+                },
+                {
+                    time: 0,
+                    verse: 'to find the chrysalis unchristened to all'
+                },
+                {
+                    time: 0,
+                    verse: 'who\'d buy this aping jaw fixed to his skull'
+                },
+                {
+                    time: 0,
+                    verse: 'swaggering down these barren exhibit halls'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'but totems so revered'
+                },
+                {
+                    time: 0,
+                    verse: 'when defined by ambrosia beers'
+                },
+                {
+                    time: 0,
+                    verse: 'in time are seen with a focus grown clear'
+                },
+                {
+                    time: 0,
+                    verse: 'and slowly the story erodes'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'pidgin palaver exchanged'
+                },
+                {
+                    time: 0,
+                    verse: 'can only transfer good faith'
+                },
+                {
+                    time: 0,
+                    verse: 'hyped up like some disastrous play'
+                },
+                {
+                    time: 0,
+                    verse: 'that starts with a lone ringing phone'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'but even so'
+                },
+                {
+                    time: 0,
+                    verse: 'just that it ever rang at all'
+                },
+                {
+                    time: 0,
+                    verse: 'for if the tactic is to stall'
+                },
+                {
+                    time: 0,
+                    verse: 'you\'re just ecstatic she called'
+                },
+                {
+                    time: 0,
+                    verse: 'what this understudy sought'
+                },
+                {
+                    time: 0,
+                    verse: 'from his number strutting on the wall'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'we whine when first we got shunned'
+                },
+                {
+                    time: 0,
+                    verse: 'then wise up and learn to shotgun'
+                },
+                {
+                    time: 0,
+                    verse: 'last show\'s set still propped up'
+                },
+                {
+                    time: 0,
+                    verse: 'so we\'ll breeze through our parts'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'though each kind new ultimatum'
+                },
+                {
+                    time: 0,
+                    verse: 'to his tribe left old and jaded'
+                },
+                {
+                    time: 0,
+                    verse: 'the boy brave who cultivated'
+                },
+                {
+                    time: 0,
+                    verse: 'brave rejection into art'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'but died unfinished alas'
+                },
+                {
+                    time: 0,
+                    verse: 'hotlines get busy just as'
+                },
+                {
+                    time: 0,
+                    verse: 'thoughts slide to slitting wrists that'
+                },
+                {
+                    time: 0,
+                    verse: 'hint of sampler cologne'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'and a lass sniffing out these rote charms'
+                },
+                {
+                    time: 0,
+                    verse: 'always rides in single go-karts'
+                },
+                {
+                    time: 0,
+                    verse: 'as we wind an endless flowchart'
+                },
+                {
+                    time: 0,
+                    verse: 'with every given answer a no'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'and her veggie wishbone\'s'
+                },
+                {
+                    time: 0,
+                    verse: 'short end in these clutching fingers of steel'
+                },
+                {
+                    time: 0,
+                    verse: 'too untrusting to ever unpeel'
+                },
+                {
+                    time: 0,
+                    verse: 'from a dummy steering wheel'
+                },
+                {
+                    time: 0,
+                    verse: 'settled for hugging only curbs'
+                },
+                {
+                    time: 0,
+                    verse: 'and having only tires squeal'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: 'when boys who mope'
+                },
+                {
+                    time: 0,
+                    verse: 'know the beauty of simply limping away blind'
+                },
+                {
+                    time: 0,
+                    verse: 'as the chirping crosswalks guide'
+                },
+                {
+                    time: 0,
+                    verse: 'where willows softly conspire'
+                },
+                {
+                    time: 0,
+                    verse: 'when boys who cope'
+                },
+                {
+                    time: 0,
+                    verse: 'only risk invites denied I sigh'
+                }
+            ],
+            [
+                {
+                    time: 0,
+                    verse: '"Good night."'
                 }
             ]
         ]
