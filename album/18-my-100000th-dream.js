417d773f1abc79737e8b424d4242e7add6e04409
Bennett Lin <bennettl@zillow.com>
2016-02-01 20:03:14 -0800

    YB-0 Add times to My 100,000th Dream.

--- album/18-my-100000th-dream.js
+++ album/18-my-100000th-dream.js
@@ -8,133 +8,133 @@ exports.song = {
         right: [
             [
                 {
-                    time: 0,
+                    time: 13,
                     verse: 'My hundred thousandth dream, I bumped into my Sita.'
                 },
                 {
-                    time: 0,
+                    time: 19,
                     verse: 'Silent for a while, we searched our eyes,'
                 },
                 {
-                    time: 0,
+                    time: 23,
                     verse: 'then we smiled and like old times,'
                 },
                 {
-                    time: 0,
+                    time: 27,
                     verse: 'I took her shoulder as we drove from El Torito.'
                 },
                 {
-                    time: 0,
+                    time: 34,
                     verse: 'I said never again we\'ll slave our whole lives away!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 43,
                     verse: 'As pretty as the rainbow, Sita! Sita!'
                 },
                 {
-                    time: 0,
+                    time: 49,
                     verse: 'Did I ever live another life?'
                 },
                 {
-                    time: 0,
+                    time: 56,
                     verse: 'As precious as a summer, Sita! Sita!'
                 },
                 {
-                    time: 0,
+                    time: 63,
                     verse: 'Did I ever live another life?'
                 },
                 {
-                    time: 0,
+                    time: 70,
                     verse: 'And were you mine?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 74,
                     verse: 'My hundred thousandth dream, I kissed my senorita,'
                 },
                 {
-                    time: 0,
+                    time: 80,
                     verse: 'on the pampas where the gopis played,'
                 },
                 {
-                    time: 0,
+                    time: 84,
                     verse: 'and the seasons glide away.'
                 },
                 {
-                    time: 0,
+                    time: 89,
                     verse: 'I can\'t remember how or when my heart was Sita\'s.'
                 },
                 {
-                    time: 0,
+                    time: 94,
                     verse: 'We were young, so sure of life, never knowing time unkind!'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 103,
                     verse: 'I spent a lifetime waiting, Sita! Sita!'
                 },
                 {
-                    time: 0,
+                    time: 110,
                     verse: 'Did I ever live another life?'
                 },
                 {
-                    time: 0,
+                    time: 117,
                     verse: 'We blow the wind in whistles, Sita! Sita!'
                 },
                 {
-                    time: 0,
+                    time: 124,
                     verse: 'Did I ever live another life?'
                 },
                 {
-                    time: 0,
+                    time: 131,
                     verse: 'When you were mine?'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 163,
                     verse: 'My hundred thousandth dream, on the cliffs of El Cerrito,'
                 },
                 {
-                    time: 0,
+                    time: 169,
                     verse: 'she says now it\'s dark and getting late,'
                 },
                 {
-                    time: 0,
+                    time: 174,
                     verse: 'but we\'ll always have today.'
                 },
                 {
-                    time: 0,
+                    time: 178,
                     verse: 'Why does my last thought in this world belong to Sita?'
                 },
                 {
-                    time: 0,
+                    time: 183,
                     verse: 'Now the light spills warm and bright… gently fills these eyes…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 192,
                     verse: 'She\'ll always come to take me, Sita! Sita!'
                 },
                 {
-                    time: 0,
+                    time: 199,
                     verse: 'Will I ever live another life?'
                 },
                 {
-                    time: 0,
+                    time: 206,
                     verse: 'Real or imaginary, Sita! Sita!'
                 },
                 {
-                    time: 0,
+                    time: 213,
                     verse: 'Will I ever live another life?'
                 },
                 {
-                    time: 0,
+                    time: 220,
                     verse: 'When you\'ll be mine?'
                 }
             ]
