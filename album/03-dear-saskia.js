d777f8c7bd86b29a562c7094aaaca858858f020b
Bennett Lin <bennettl@zillow.com>
2016-02-01 21:18:28 -0800

    YB-0 Add times for Dear Saskia.

--- album/03-dear-saskia.js
+++ album/03-dear-saskia.js
@@ -8,157 +8,157 @@ exports.song = {
         right: [
             [
                 {
-                    time: 0,
+                    time: 5,
                     verse: 'Dearest Sasha,'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 7,
                     verse: 'It\'s nearing autumn here with record highs.'
                 },
                 {
-                    time: 0,
+                    time: 10,
                     verse: 'Is it searing in Boston? Hope this letter finds'
                 },
                 {
-                    time: 0,
+                    time: 13,
                     verse: 'your first semester fine while I\'m out of town…'
                 },
                 {
-                    time: 0,
+                    time: 18,
                     verse: 'You\'d love Taiwan, surrounded by walls of mountains,'
                 },
                 {
-                    time: 0,
+                    time: 22,
                     verse: 'their peaks sealed and shielded by foggy clouds.'
                 },
                 {
-                    time: 0,
+                    time: 26,
                     verse: 'Beneath this ceiling yields the scene a reeling feel'
                 },
                 {
-                    time: 0,
+                    time: 30,
                     verse: 'of some giant\'s house.'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 33,
                     verse: 'So… before I left, you said'
                 },
                 {
-                    time: 0,
+                    time: 37,
                     verse: 'I should keep your kid name, with everything it meant.'
                 },
                 {
-                    time: 0,
+                    time: 41,
                     verse: 'But then in your embrace, I felt you prepped to shed…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 48,
                     verse: 'And I\'d reflect inside withering amber eyes—'
                 },
                 {
-                    time: 0,
+                    time: 52,
                     verse: 'your pupil\'s pupal fly, a pet petrified—'
                 },
                 {
-                    time: 0,
+                    time: 56,
                     verse: 'with you raving over how he\'s getting you'
                 },
                 {
-                    time: 0,
+                    time: 64,
                     verse: 'staying kosher. Though, you know, I would have too…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 73,
                     verse: 'And I sniffled daily when you skipped a grade.'
                 },
                 {
-                    time: 0,
+                    time: 77,
                     verse: 'One bus seat up you moved, letting rippled braids'
                 },
                 {
-                    time: 0,
+                    time: 80,
                     verse: 'caress, sun-streaked and loose, a wet-cheeked papoose'
                 },
                 {
-                    time: 0,
+                    time: 84,
                     verse: 'sadly weaned from you…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 88,
                     verse: 'Whom I knew I\'d lose the day,'
                 },
                 {
-                    time: 0,
+                    time: 92,
                     verse: 'you flew enraged and shooed them from their game;'
                 },
                 {
-                    time: 0,
-                    verse: 'untied this "tetherboy", soothing in your lecture voice.'
+                    time: 95,
+                    verse: 'untied this "tetherboy," soothing in your lecture voice.'
                 },
                 {
-                    time: 0,
+                    time: 103,
                     verse: 'Oh but Sasha, if anarchy reigns,'
                 },
                 {
-                    time: 0,
+                    time: 107,
                     verse: 'then no one lives when narcing to the aides,'
                 },
                 {
-                    time: 0,
+                    time: 110,
                     verse: 'who shrug when thus annoyed, yawning, "Boys will beat boys…"'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 118,
                     verse: 'Well Sasha… okay, you saw the cuts.'
                 },
                 {
-                    time: 0,
+                    time: 122,
                     verse: 'But though my jaw was stuck, so now I talk Canuck,'
                 },
                 {
-                    time: 0,
+                    time: 126,
                     verse: 'there\'s been plenty days I\'ve really been through worse.'
                 },
                 {
-                    time: 0,
+                    time: 134,
                     verse: 'Anyway, I\'m feeling healed. Still, hugging hurts…'
                 }
             ],
             [
                 {
-                    time: 0,
+                    time: 143,
                     verse: 'And Sasha, I know you\'ll have them all,'
                 },
                 {
-                    time: 0,
+                    time: 147,
                     verse: 'while if I last the fall playing possum'
                 },
                 {
-                    time: 0,
+                    time: 151,
                     verse: 'is a toss-up. But where some haemophile'
                 },
                 {
-                    time: 0,
+                    time: 154,
                     verse: 'might bleed the Nile to nausea,'
                 },
                 {
-                    time: 0,
+                    time: 158,
                     verse: 'I can wash up, and hope the bastards croak,'
                 },
                 {
-                    time: 0,
+                    time: 162,
                     verse: 'on rat turds choked.'
                 }
             ]
