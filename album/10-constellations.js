a324d4e73fbfb046c57a04676b4cc01b23a49988
Bennett Lin <bennettl@zillow.com>
2016-02-19 21:08:27 -0800

    YB-0 Continue notes for Constellations annotations.

--- album/10-constellations.js
+++ album/10-constellations.js
@@ -1,9 +1,14 @@
 exports.song = {
     title: 'Constellations!',
     speechBubbles: {
-        narrative: 'One summer night, my friends Ana, Howie, Tomer, and I drank Robitussin and explored the steam tunnels under UCLA pretending to be Mongols and Vikings. ',
+        /**
+         * Mention being back visiting LA.
+         */
+        narrative: 'One summer night, my friends Ana, Howie, Tomer, and I drank Robitussin and explored the steam tunnels under UCLA pretending to be Mongols and Vikings invading the globe. ',
         backstory: [
-            // TODO: Mention Police's Ghost in the Machine,
+            /**
+             * Finnegans Wake density of information, similar to Robitussin effect. Police's Ghost in the Machine
+             */
             'After finishing James Joyce\'s <em>Ulysses</em>, I set out to read <em>Finnegan\'s Wake</em>, and while I only got through the first forty pages or so, ',
         ]
     },
@@ -11,94 +16,157 @@ exports.song = {
         left: [
             [
                 {
+                    /**
+                     * First line of Finnegans Wake, tendency of Robitussin to make you stiff.
+                     */
                     time: 4,
                     verse: 'reverends, paused stiff in tandem.'
                 },
                 {
+                    /**
+                     * Manna in the Bible was psilocybin. Alternative publications. Not full blown conspiracy theorist, but I was interested in the history of drug use, and it seemed obvious that there was a lot more that was hidden.
+                     */
                     time: 7,
                     verse: 'Heavin\' some, Ana whispures a low sigh, bein\'…'
                 },
                 {
+                    /**
+                     * DXM, Exodus, Vicodin
+                     */
                     time: 11,
                     verse: 'of de exempt, exit us the Viking den…'
                 },
                 {
+                    /**
+                     * Walgreen's, Valhalla, medicine, generic brands end in Tussin,
+                     */
                     time: 19,
                     verse: 'Walkyriens: to Walhall o\' mead-Tussin!'
                 },
                 {
+                    /**
+                     * Coughs suppressed, quaffs appraised, Cali valley, Kalevala.
+                     */
                     time: 22,
                     verse: 'Quouffs uppraissed, this boundless Kalevalley ends.'
                 },
                 {
+                    /**
+                     * Finno-Ugric language, fenugreek flavour of cough syrup, laps our hymen.
+                     */
                     time: 26,
                     verse: 'We ride as Finnugreec tongue Lapps are highmen.'
                 }
             ],
             [
                 {
+                    /**
+                     * Magyars migrate, make yours my grate. Mongol's pseudephedrine
+                     */
                     time: 34,
                     verse: 'Makeyars mygrate, from among ol\' psealed off drains.'
                 },
                 {
+                    /**
+                     * The Mongols descended from the steppes.
+                     */
                     time: 41,
                     verse: 'Then down the steppes we charged,'
                 },
                 {
+                    /**
+                     * Genghis Khan once Temujin, drink is gone, too much in, teeter-totter, teetotaler, Genghis Khan defeated the Tatars to unite the Mongols.
+                     */
                     time: 45,
                     verse: 'Drinkhis Khan, once Toomujin, has slain the teeto-Tartars!'
                 },
                 {
+                    /**
+                     * Tamerlane the seed of Genghis Khan, Tomer laying the seed (or breadcrumbs), lead the feet, avatar is reincarnation of deity
+                     */
                     time: 49,
                     verse: 'Tomer-layin\' the seed, Howie led the feat of avatars…'
                 }
             ],
             [
                 {
+                    /**
+                     * Indus trail source, industrial sewers, quest turns into hairpins, questions Indo-Harappan
+                     */
                     time: 56,
                     verse: 'Indus-trail sewerce, questurns Indo Hairapins:'
                 },
                 {
+                    /**
+                     * Farsi ahura, Sanskrit deva's day shone, foresee a hero or sense great devastation
+                     */
                     time: 60,
                     verse: 'Forsi ahuro or Sensegreat devas\'dayshone?'
                 },
                 {
+                    /**
+                     * to sum up, soma, Aryan in Vedas, our young invaders, unsolved query rest, on soft quarry rest
+                     */
                     time: 64,
                     verse: 'To soma up, our-youn\' invedas unsolft quarry rest…'
                 },
                 {
+                    /**
+                     * Arabian battles spent, our Evian bottles spent, a duct we egress, abduct Uighuress
+                     */
                     time: 70,
                     verse: 'Ourevian battles spent, a\'duct Uighuress.'
                 },
                 {
+                    /**
+                     * Our melee tapers the discourse, are Malay tapirs the dusk horse? (Mention the Eohippus dawn horse, new flora for explorers)
+                     */
                     time: 75,
                     verse: 'Are Melee tapirs the dusc-horse yet…?'
                 },
                 {
+                    /**
+                     * Khmer city Angkor Wat, calm air sitting in car wet, drenched from Evian bottles in car, climbed the cemetery fence, scimitar offense
+                     */
                     time: 79,
                     verse: 'From the Khlmair city Ingcar Wet, to war: scimitary of\'fence!'
                 }
             ],
             [
                 {
+                    /**
+                     * Poor sod, four feet under, forfeit Siberian plains
+                     */
                     time: 86,
                     verse: 'Pour sod, fourfeit tundr\'s Siburial plains…'
                 },
                 {
+                    /**
+                     * Up ridge of lawn, a bridge of land, First Nations
+                     */
                     time: 94,
                     verse: 'Upridge o\' lawn trod on by forc\'d nations sank…'
                 },
                 {
+                    /**
+                     * Across Bering Strait
+                     */
                     time: 101,
                     verse: 'But past a cross Bearing Straight,'
                 },
                 {
+                    /**
+                     * He rows. (First Nations came in canoes?) Aurora borealis to the left. Our auras left. Adam's grave.
+                     */
                     time: 103,
                     verse: 'herows—ourauras left—to Audamn\'s grave.'
                 }
             ],
             [
                 {
+                    /**
+                     * comment
+                     */
                     time: 109,
                     verse: 'And now without a chart,'
                 },
