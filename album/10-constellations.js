81963293925ba85f1c1f070cd90c8b4d0ea91a78
Bennett Lin <bennettl@zillow.com>
2016-02-24 21:13:14 -0800

    YB-0 Begin annotations of Constellations.

--- album/10-constellations.js
+++ album/10-constellations.js
@@ -1,83 +1,252 @@
 exports.song = {
     title: 'Constellations!',
     speechBubbles: {
-        narrative: 'One night back in Los Angeles, some friends and I explored the steam tunnels under UCLA while high on Robitussin. I kept getting us to pretend we were Mongols and Vikings and other waves of human migration throughout history. In hindsight, my friends were probably just humouring me. I wound up doing a lot of Robitussin that summer, and by the end of it, I was pretty insane. I eventually regained my grip on reality, but the world always seemed kind of different after that.',
-        backstory: 'While reading <em>Finnegans Wake</em>, I was impressed by how Joyce uses literary puns to create multiple layers of meaning within a dreamlike setting. It seemed like the perfect device for conveying a Robitussin trip, where time seems to stand still, and everything feels at once epic and unreal. The Police\'s <em>Ghost in the Machine</em> was also my soundtrack of choice on Robitussin, so I borrowed its style and atmosphere for this song.'
+        narrative: 'One night back in Los Angeles, my friends Ana, Howie, Tomer, and I explored the steam tunnels under UCLA while high on Robitussin. I kept getting us to pretend we were Mongols and Vikings and other waves of historical migration. In hindsight, they were probably just humouring me. I wound up doing a lot of Robitussin that summer, and by the end of it, I was pretty insane. I eventually regained my grip on reality, but the world always seemed rather different after that.',
+        backstory: 'While reading <em>Finnegans Wake</em>, I was impressed by how Joyce uses literary puns to create multiple layers of meaning within a dreamlike setting. It seemed like the perfect device for conveying a Robitussin trip, in which time seems to stand still, and everything feels at once epic and unreal. The Police\'s <em>Ghost in the Machine</em> was also my soundtrack of choice on Robitussin, so I borrowed a bit of its style and atmosphere for this song.'
+    },
+    annotations: {
+        reverends: {
+            description: '<em>Finnegans Wake</em> opens with the line "riverrun, past Eve and Adam\'s." I treated Robitussin as a semireligious experience, which typically opened with The Police\'s "Secret Journey," with its line, "You will be a holy man."'
+        },
+        paused_stiff_in_tandem: {
+            description: 'Robitussin in its early stages will make your limbs feel stiff and mechanical. Having taken it an hour prior, we slowly make our way through the UCLA campus.'
+        },
+        heavin_some_ana: {
+            description: 'The first time you down a whole bottle of Robitussin, you may feel like retching.'
+        },
+        whispures_a_low_sign_bein: {
+            description: '"Heaven\'s manna was pure psilocybin." At the time, I was intrigued by the role of psychedelic drugs in human history that I had understandably never been taught. One theory is that manna from heaven, based on its description in the Bible, was a psilocybin mushroom.',
+            reference: 'https://en.wikipedia.org/wiki/Manna'
+        },
+        de_exempt: {
+            description: 'DXM is the standard abbreviation for dextromethorphan, the active ingredient in Robitussin.'
+        },
+        exit_us: {
+            description: 'The Book of Exodus recounts how the Israelites discovered manna during their trek out of Egypt.'
+        },
+        viking_den: {
+            description: 'I was the seasoned Robitussin user in the group. The others assumed its effects would be weaker than those of prescribed painkillers such as Vicodin. That night proved them very wrong.'
+        },
+        walkyriens: {
+            description: 'Backtrack to earlier that night. We buy the Robitussin from a Walgreens pharmacy. Also, the Valkyries in Norse mythology were noble maidens who escorted fallen warriors from the battlefield.'
+        },
+        walhall_o: {
+            description: 'The Valkyries would bring the dead heroes to Valhalla, a majestic hall ruled by the god Odin. Also, the Walgreens aisles are stocked with generic versions of brand-name drugs, each with the prefix "Wal" in its name: Wal-Dryl, Wal-Phed…'
+        },
+        mead_tussin: {
+            description: 'Mead was the drink of the Norse gods, and we go with Wal-Tussin as our medicine of choice.'
+        },
+        quouffs_uppraissed: {
+            description: 'Having appraised our quaffs, we now upraise them. And whatever coughs we might have, they will definitely be suppressed.'
+        },
+        boundless_kalevalley_ends: {
+            // TODO: Revisit about leaving Valley metaphorically. Through drugs.
+            description: 'The Kalevala is the national epic poem of Finland. Also, as a teenager I hated growing up in the San Fernando Valley of Southern California, and the days before I could leave seemed to drag on forever.'
+        },
+        finnugreec_tongue: {
+            description: 'The Finnish language belongs in the Finno-Ugric family. Also, the licorice taste of Robitussin lingering in our mouths is reminiscent of the spice fenugreek.',
+            reference: 'https://en.wikipedia.org/wiki/Finno-Ugric_languages'
+        },
+        lapps_are_highmen: {
+            description: 'The Lapps are a Finno-Ugric people indigenous to the northern regions of Scandinavia. And of course, we are literally high, as the Robitussin "laps our hymen," since my friends are trying it for the first time.'
+        },
+        makeyars_mygrate: {
+            description: 'Cut back to the present scene. "Make yours my grate." Howie and Tomer are sharing their knowledge of the hidden entrances to the steam tunnels, which they have explored before. Also, the Magyars are a Finno-Ugric people who migrated from the Ural Mountains and established the state of Hungary.'
+        },
+        mong_ol: {
+            // TODO: Elaborate on Mongols
+            description: 'Magyars and Mongols share common genetic origins.'
+        },
+        psealed_off_drains: {
+            // TODO: Need to spell out sealed off drains?
+            description: 'In those days, Robitussin would also contain pseudoephedrine, a decongestant, as an active ingredient. Because of its use in the manufacture of methamphetamine, however, this is no longer the case.'
+        },
+        down_the_steppes: {
+            description: 'In the 13th century, the Mongols swept down from the Central Asian steppes and began their conquests.'
+        },
+        drinkhis_khan: {
+            description: ''
+        },
+        once_toomujin: {
+            description: ''
+        },
+        slain_the_teeto_tartars: {
+            description: ''
+        },
+
+
     },
     lyrics: {
         left: [
             [
                 {
-                    /**
-                     * First line of Finnegans Wake, tendency of Robitussin to make you stiff.
-                     */
                     time: 4,
-                    verse: 'reverends, paused stiff in tandem.'
+                    verse: [
+                        {
+                            anchor: 'reverends,',
+                            annotationKey: 'reverends'
+                        },
+                        {
+                            anchor: 'paused stiff in tandem.',
+                            annotationKey: 'paused_stiff_in_tandem'
+                        }
+                    ]
                 },
                 {
-                    /**
-                     * Manna in the Bible was psilocybin. Alternative publications. Not full blown conspiracy theorist, but I was interested in the history of drug use, and it seemed obvious that there was a lot more that was hidden.
-                     */
                     time: 7,
-                    verse: 'Heavin\' some, Ana whispures a low sigh, bein\'…'
+                    verse: [
+                        {
+                            anchor: 'Heavin\' some, Ana',
+                            annotationKey: 'heavin_some_ana'
+                        },
+                        {
+                            anchor: 'whispures a low sigh, bein\'…',
+                            annotationKey: 'whispures_a_low_sign_bein'
+                        }
+                    ]
                 },
                 {
                     /**
                      * DXM, Exodus, Vicodin
                      */
                     time: 11,
-                    verse: 'of de exempt, exit us the Viking den…'
+                    verse: [
+                        'of',
+                        {
+                            anchor: 'de exempt,',
+                            annotationKey: 'de_exempt'
+                        },
+                        {
+                            anchor: 'exit us',
+                            annotationKey: 'exit_us'
+                        },
+                        'the',
+                        {
+                            anchor: 'Viking den…',
+                            annotationKey: 'viking_den'
+                        }
+                    ]
                 },
                 {
-                    /**
-                     * Walgreen's, Valhalla, medicine, generic brands end in Tussin,
-                     */
                     time: 19,
-                    verse: 'Walkyriens: to Walhall o\' mead-Tussin!'
+                    verse: [
+                        {
+                            anchor: 'Walkyriens:',
+                            annotationKey: 'walkyriens'
+                        },
+                        'to',
+                        {
+                            anchor: 'Walhall o\'',
+                            annotationKey: 'walhall_o'
+                        },
+                        {
+                            anchor: 'mead-Tussin!',
+                            annotationKey: 'mead_tussin'
+                        }
+                    ]
                 },
                 {
-                    /**
-                     * Coughs suppressed, quaffs appraised, Cali valley, Kalevala.
-                     */
                     time: 22,
-                    verse: 'Quouffs uppraissed, this boundless Kalevalley ends.'
+                    verse: [
+                        {
+                            anchor: 'Quouffs uppraissed,',
+                            annotationKey: 'quouffs_uppraissed'
+                        },
+                        'this',
+                        {
+                            anchor: 'boundless Kalevalley ends.',
+                            annotationKey: 'boundless_kalevalley_ends'
+                        }
+                    ]
                 },
                 {
-                    /**
-                     * Finno-Ugric language, fenugreek flavour of cough syrup, laps our hymen.
-                     */
                     time: 26,
-                    verse: 'We ride as Finnugreec tongue Lapps are highmen.'
+                    verse: [
+                        'We ride as',
+                        {
+                            anchor: 'Finnugreec tongue',
+                            annotationKey: 'finnugreec_tongue'
+                        },
+                        {
+                            anchor: 'Lapps are highmen.',
+                            annotationKey: 'lapps_are_highmen'
+                        }
+                    ]
                 }
             ],
             [
                 {
-                    /**
-                     * Magyars migrate, make yours my grate. Mongol's pseudephedrine
-                     */
                     time: 34,
-                    verse: 'Makeyars mygrate, from among ol\' psealed off drains.'
+                    verse: [
+                        {
+                            anchor: 'Makeyars mygrate,',
+                            annotationKey: 'makeyars_mygrate'
+                        },
+                        'from',
+                        {
+                            anchor: 'among ol\'',
+                            annotationKey: 'mong_ol'
+                        },
+                        {
+                            anchor: 'psealed off drains.',
+                            annotationKey: 'psealed_off_drains'
+                        }
+                    ]
                 },
+            ],
+            [
                 {
-                    /**
-                     * The Mongols descended from the steppes.
-                     */
                     time: 41,
-                    verse: 'Then down the steppes we charged,'
+                    verse: [
+                        'Then',
+                        {
+                            anchor: 'down the steppes',
+                            annotationKey: 'down_the_steppes'
+                        },
+                        'we charged,'
+                    ]
                 },
                 {
                     /**
                      * Genghis Khan once Temujin, drink is gone, too much in, teeter-totter, teetotaler, Genghis Khan defeated the Tatars to unite the Mongols.
                      */
                     time: 45,
-                    verse: 'Drinkhis Khan, once Toomujin, has slain the teeto-Tartars!'
+                    verse: [
+                        {
+                            anchor: 'Drinkhis Khan,',
+                            annotationKey: 'drinkhis_khan'
+                        },
+                        {
+                            anchor: 'once Toomujin,',
+                            annotationKey: 'once_toomujin'
+                        },
+                        'has',
+                        {
+                            anchor: 'slain the teeto-Tartars!',
+                            annotationKey: 'slain_the_teeto_tartars'
+                        }
+                    ]
                 },
                 {
                     /**
                      * Tamerlane the seed of Genghis Khan, Tomer laying the seed (or breadcrumbs), lead the feet, avatar is reincarnation of deity
                      */
                     time: 49,
-                    verse: 'Tomer-layin\' the seed, Howie led the feat of avatars…'
+                    verse: [
+                        {
+                            anchor: 'Tomer-layin\' the seed,',
+                            annotationKey: 'tomer_layin_the_seed'
+                        },
+                        {
+                            anchor: 'Howie led the feat',
+                            annotationKey: 'howie_led_the_feat'
+                        },
+                        'of',
+                        {
+                            anchor: 'avatars…',
+                            annotationKey: 'avatars'
+                        }
+                    ]
                 }
             ],
             [
