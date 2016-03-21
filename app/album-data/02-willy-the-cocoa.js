8c0a05f90e485452f5d3fd7cc4f68a0044d7fe96
Bennett Lin <bennettl@zillow.com>
2016-03-20 21:10:50 -0700

    YB-0 Add annotations to Willy the Cocoa.

--- app/album-data/02-willy-the-cocoa.js
+++ app/album-data/02-willy-the-cocoa.js
@@ -33,7 +33,7 @@ module.exports = {
             description: 'Occasionally, a coyote would wander down from the surrounding hillsides and get hit by traffic. Of course, the Valley is also like one big lower jaw, and .'
         },
         wayside_bottle: {
-            description: 'Willy begins his daily rounds by filling up a shopping cart with recyclables found in garbage bins or littered along the sidewalks.'
+            description: 'Willy begins his daily rounds by filling up a shopping cart with recyclables found in garbage bins and littered along the sidewalks.'
         },
         pinhead_in_its_shadow: {
             description: 'Willy has microcephaly, a medical condition in which the brain fails to develop properly, resulting in a head that is abnormally small in size. He peers through an empty soda bottle, the top of which is tapered like the outline of his skull.'
@@ -48,16 +48,16 @@ module.exports = {
             description: 'Some families help Willy by leaving out their discarded recyclables for him to gather in bulk.'
         },
         haircutting_to_the_sun: {
-            description: 'Our mother is cutting my brother\'s hair on the front lawn, since the cleanup afterwards will be easier.'
+            description: 'My mother is cutting my brother\'s hair on the front lawn, since the cleanup afterwards will be easier.'
         },
         suddenly_baby_shrieks: {
             description: 'As a child, my younger brother was typically calm and quiet in demeanour. However, when made to do things he didn\'t enjoy or didn\'t understand, such as sitting through a haircut, he was prone to frenzied outbursts, which were sometimes scary given how likely he was to injure himself.'
         },
         gap_toothed_coma: {
-            description: 'Or "gap-toothed comb."'
+            description: 'Also, "gap-toothed comb."'
         },
         pair_of_seizures: {
-            description: 'Or "pair of scissors."'
+            description: 'Also, "pair of scissors."'
         },
         see_your_kid_with_whiskers: {
             description: 'Willy jokingly remarks that given my brother\'s distaste for haircuts, he will likely sport an unshaven look as an adult. But in many legends, the changeling is not a child of any sort, but in fact a fully grown troll who is perhaps centuries old. So Willy might also be implying that he is immune to the enchantment that disguises my brother\'s true form in a childlike appearance.'
@@ -78,7 +78,7 @@ module.exports = {
             description: 'There were times I was so angry at my brother that I laid down some serious hurt. He never fought back.'
         },
         bearded_baby: {
-            description: 'A changeling out of disguise might literally have a beard. Of course, I was also always frustrated with having to engage my brother at a level far below his actual age.'
+            description: 'A changeling out of disguise might literally have a beard. Of course, I was also frustrated with always having to engage my brother at a level far below his actual age.'
         },
         baffled_sobs: {
             description: 'The hardest thing about having hurt my brother is that I could never really apologise, since he could never truly comprehend that I was sorry. I suppose that for him, it just became another part of what life was.'
@@ -87,9 +87,51 @@ module.exports = {
             description: 'I spent a long time wondering why my brother was born with autism and I was not, when our circumstances weren\'t much different. It didn\'t seem far-fetched to suppose that cognitive abilities are a fixed supply in the womb, and that I had selfishly taken my brother\'s share.'
         },
         killcrop: {
-            description: 'In Germanic folklore, a killcrop is an infant suspected of being a changeling due to its voracious appetite. The etymology is straightforward and reflects the tragic reality of past times, when children unable to work for their keep could lead to a family\'s ruin.'
+            description: 'In Germanic folklore, a killcrop is an infant suspected of being a changeling due to its voracious appetite. The etymology is plain and simple—one who "kills crops"—and reflects the tragic reality of past times, when children unable to work for their keep could lead to a family\'s ruin.'
         },
         soused_loafers: {
+            description: 'In his zeal to grab at a littered beer bottle, Willy steps into a puddle and splashes sewage water onto a pair of gangbangers standing nearby. "Soused loafers" can refer to drenched shoes, as well as to drunken delinquents.'
+        },
+        hit_this_witch_with_switch: {
+            description: 'It was widely believed that tricking a changeling into revealing its true nature would force it to return the stolen child. The methods for doing so included hitting it with a switch, a flexible rod designed to inflict punishment.'
+        },
+        sack_spilled_glass_swung: {
+            description: 'Eager for a fight, the gangbangers start pushing Willy around. Amidst the commotion, his shopping cart gets turned over, spilling a garbage bag full of empty glass bottles onto the ground. One of the gangbangers grabs a bottle and smashes it against Willy\'s face.'
+        },
+        past_noon_toll: {
+            description: 'Immediately past noon, the ring of a town bell is heard exactly once.'
+        },
+        unsalvaged_soul: {
+            description: 'Given that abuse of an underdeveloped child often led to its death, religious doctrine in the past typically gave assurance that such children were devoid of souls. I came to a different conclusion. If my brother and I can be made of the same physical matter yet so different in mental presence, then it made more sense to believe that no one has a soul.'
+        },
+        willys_stalkless_bulb: {
+            description: 'This incident happened to Willy, of course, but the lyrics haven\'t mentioned his name until now, since it easily could have been my brother. A "stalkless bulb" can refer to a brain severed from its spine, as well as to a dormant plant ready to be set in the ground.'
+        },
+        sawdust_month: {
+            description: 'My father liked to build furniture in the garage, and I would help my mother spread the leftover sawdust in our backyard garden. Also, it was the "saddest month" when Willy was killed.'
+        },
+        mulch_soften_asphodel: {
+            description: 'The asphodel is a popular garden plant, as well as an immortal flower of the underworld in Greek mythology. Since we are made of the same physical matter as the mulch we are using to soften the soil, Willy\'s body must have also "softened the asphalt" beneath which it lay.'
+        },
+        baby_bluebonnets: {
+            description: 'The bluebonnet is another garden flower, and a bonnet is what my mother tucks my brother\'s head in to protect him from the sun. But in light of Willy\'s murder, I wonder how well-protected his head is against other things.'
+        },
+        forgotten_amongst_the_mail: {
+            description: 'Having recently kicked my brother in anger over a shredded comic book, I\'m now indifferent to the latest release from my monthly subscription.'
+        },
+        how_marbleless: {
+            description: 'Or, "how marvellous." Some friends of my parents, in their poor attempts to empathise, would marvel at how nice it must be for my brother to go through life with a permanent lack of understanding.'
+        },
+        imprunt_age_of_mind: {
+            description: '"Imprinting" is a period during which an infant acquires certain behaviour, such as emotional attachment. My mother sometimes blamed herself for having failed to imprint my brother correctly. An "imp-runt" is also literally the weakest child in a litter of goblins, and thus a prime choice to be replaced with a human.'
+        },
+        i_am_seven: {
+            description: ''
+        },
+        heavy_fears: {
+            description: ''
+        },
+        not_yet_webelos: {
             description: ''
         }
     },
@@ -275,9 +317,6 @@ module.exports = {
             ],
             [
                 {
-                    /**
-                     * comic book
-                     */
                     time: 71,
                     verse: [
                         'Prying',
@@ -293,9 +332,6 @@ module.exports = {
                     ]
                 },
                 {
-                    /**
-                     * changelings were actually old
-                     */
                     time: 74,
                     verse: [
                         'a',
@@ -346,9 +382,6 @@ module.exports = {
             ],
             [
                 {
-                    /**
-                     * Killcrop was an insatiable child
-                     */
                     time: 106,
                     verse: [
                         {
@@ -359,9 +392,6 @@ module.exports = {
                     ]
                 },
                 {
-                    /**
-                     * set shoes, drunk thugs.
-                     */
                     time: 110,
                     verse: [
                         {
@@ -372,85 +402,137 @@ module.exports = {
                     ]
                 },
                 {
-                    /**
-                     * Fix a changeling by hitting it.
-                     */
                     time: 113,
-                    verse: 'Hit this witch with switch, whispers wind, to fix his switcheroo…'
+                    verse: [
+                        {
+                            anchor: 'Hit this witch with switch,',
+                            annotationKey: 'hit_this_witch_with_switch'
+                        },
+                        'whispers wind, to fix his switcheroo…'
+                    ]
                 },
                 {
                     time: 119,
                     verse: 'Milksop cornered into scuffle,'
                 },
                 {
-                    /**
-                     * Gangbanger takes glass from Willy and smashes his head.
-                     */
                     time: 122,
-                    verse: 'a sack-spilled glass swung sounds the past-noon toll.'
+                    verse: [
+                        'a',
+                        {
+                            anchor: 'sack-spilled glass swung',
+                            annotationKey: 'sack_spilled_glass_swung'
+                        },
+                        'sounds the',
+                        {
+                            anchor: 'past-noon toll.',
+                            annotationKey: 'past_noon_toll'
+                        }
+                    ]
                 },
                 {
                     time: 126,
                     verse: [
                         {
-                            italic: 'Shelled skull has freed unsalvaged soul…'
+                            italic: [
+                                'Shelled skull has freed',
+                                {
+                                    anchor: 'unsalvaged soul…',
+                                    annotationKey: 'unsalvaged_soul'
+                                }
+                            ]
                         }
                     ]
                 },
                 {
                     time: 128,
-                    verse: 'dripping Willy\'s stalkless bulb.'
+                    verse: [
+                        'dripping',
+                        {
+                            anchor: 'Willy\'s stalkless bulb.',
+                            annotationKey: 'willys_stalkless_bulb'
+                        }
+                    ]
                 }
             ],
             [
                 {
-                    /**
-                     * saddest month
-                     */
                     time: 131,
-                    verse: 'Oh, and August was the sawdust month'
+                    verse: [
+                        'Oh, and August was the',
+                        {
+                            anchor: 'sawdust month',
+                            annotationKey: 'sawdust_month'
+                        }
+                    ]
                 },
                 {
-                    /**
-                     * Helping mom with gardening, asphodel immortal flower
-                     */
                     time: 134,
-                    verse: 'that saw this mulch soften asphodel.'
+                    verse: [
+                        'that saw this',
+                        {
+                            anchor: 'mulch soften asphodel.',
+                            annotationKey: 'mulch_soften_asphodel'
+                        }
+                    ]
                 },
                 {
-                    /**
-                     * comic books no longer read
-                     */
                     time: 137,
-                    verse: 'Baby bluebonnets bloomed as new comics soon'
+                    verse: [
+                        {
+                            anchor: 'Baby bluebonnets',
+                            annotationKey: 'baby_bluebonnets'
+
+                        },
+                        'bloomed as new comics soon'
+                    ]
                 },
                 {
                     time: 140,
-                    verse: 'lay forgotten amongst the mail.'
+                    verse: [
+                        'lay',
+                        {
+                            anchor: 'forgotten amongst the mail.',
+                            annotationKey: 'forgotten_amongst_the_mail'
+                        }
+                    ]
                 }
             ],
             [
                 {
-                    /**
-                     * marvellous
-                     * imagining them talking about my brother
-                     */
                     time: 143,
-                    verse: 'But how marbleless, some reckoned,'
+                    verse: [
+                        'But',
+                        {
+                            anchor: 'how marbleless,',
+                            annotationKey: 'how_marbleless'
+                        },
+                        'some reckoned,'
+                    ]
                 },
                 {
-                    /**
-                     * imprint
-                     */
                     time: 146,
-                    verse: 'being ever stuck in imp-runt age of mind!'
+                    verse: [
+                        'being ever stuck in',
+                        {
+                            anchor: 'imp-runt age of mind!',
+                            annotationKey: 'imprunt_age_of_mind'
+                        }
+                    ]
                 },
                 {
                     /**
                      * Something my own brother would say? Are you six?
                      */
                     time: 149,
-                    verse: 'Are you sick? I am seven, he\'d reply…'
+                    verse: [
+                        'Are you sick?',
+                        {
+                            anchor: 'I am seven,',
+                            annotationKey: 'i_am_seven'
+                        },
+                        'he\'d reply…'
+                    ]
                 },
                 {
                     time: 155,
@@ -468,14 +550,24 @@ module.exports = {
             [
                 {
                     time: 167,
-                    verse: 'Such heavy fears, Mummy laughs,'
+                    verse: [
+                        'Such',
+                        {
+                            anchor: 'heavy fears,',
+                            annotationKey: 'heavy_fears'
+                        },
+                        'Mummy laughs,'
+                    ]
                 },
                 {
-                    /**
-                     * Way below. Cub scouts
-                     */
                     time: 171,
-                    verse: 'for a cub not yet Webelos!'
+                    verse: [
+                        'for a cub',
+                        {
+                            anchor: 'not yet Webelos!',
+                            annotationKey: 'not_yet_webelos'
+                        }
+                    ]
                 },
                 {
                     /**
