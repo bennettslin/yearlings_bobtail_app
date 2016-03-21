0f41e37a26a3bec523b953f73e8b1b31446928da
Bennett Lin <bennettslin@gmail.com>
2016-03-21 00:36:41 -0700

    YB-0 Add annotation to Willy the Cocoa.

--- app/album-data/02-willy-the-cocoa.js
+++ app/album-data/02-willy-the-cocoa.js
@@ -18,7 +18,7 @@ module.exports = {
         },
     ],
     speechBubbles: {
-        narrative: 'Willy was an autistic man who wandered our neighbourhood collecting recyclables for his living. Kids called him "The Cocoa," referring to both his Coke bottles and his seeming a bit "cuckoo." One day, some gangbangers decided to pick a fight and smashed his head open with a bottle, killing him instantly. My younger brother was diagnosed with autism at around this time, and in light of Willy\'s murder, I would lie awake in fear of what lay in store for him.',
+        narrative: 'Willy was an autistic man who wandered our neighbourhood collecting recyclables for his living. Kids called him "The Cocoa," referring to both his Coke bottles and his seeming a bit "cuckoo." One day, some gangbangers decided to pick a fight and smashed his head open with a bottle, killing him instantly. My younger brother was diagnosed with severe autism at around this time, and in light of Willy\'s murder, I would lie awake in fear of what lay in store for him.',
         backstory: 'In folklore of the past, changelings were trolls left in place of stolen human children, and served to explain the developmentally disabled. Sonically, I wanted to capture a band of woodland trolls in secluded revelry after such a heist, and figured it was akin to the swamp rock of Creedence Clearwater Revival, with mandolins and Cajun accordions thrown in. Lyrically, I tried to recreate my mishearings of adult conversation when my parents were first learning about autism. I spent a week telling everyone that my brother was "artistic," for example.'
     },
     annotations: {
@@ -29,8 +29,7 @@ module.exports = {
             description: 'The sight of kids splashing around a burst fire hydrant wasn\'t really a Los Angeles thing, but it happened once or twice in my neighbourhood.'
         },
         dying_coyotes_jaw: {
-            // TODO: Specifically mention people being like worms?
-            description: 'Occasionally, a coyote would wander down from the surrounding hillsides and get hit by traffic. Of course, the Valley is also like one big lower jaw, and .'
+            description: 'Occasionally, a coyote would wander down from the surrounding hillsides and get hit by traffic. Of course, the Valley is also like one big lower jaw, with all its inhabitants helping it to decompose.'
         },
         wayside_bottle: {
             description: 'Willy begins his daily rounds by filling up a shopping cart with recyclables found in garbage bins and littered along the sidewalks.'
@@ -39,10 +38,10 @@ module.exports = {
             description: 'Willy has microcephaly, a medical condition in which the brain fails to develop properly, resulting in a head that is abnormally small in size. He peers through an empty soda bottle, the top of which is tapered like the outline of his skull.'
         },
         chilled_colas_free: {
-            description: 'At the recycling center where Willy drops off his recyclables, a complimentary drink is part of his reimbursement.'
+            description: 'At the recycling center where Willy drops off his recyclables, a complimentary soft drink is part of his reimbursement.'
         },
         he_ends_his_stroll: {
-            description: 'Or else, "he ends as troll." In most legends, changelings wittingly participate in the deceit, and actively take pains to hide their true form.'
+            description: 'Or else, "he ends as troll." In most legends, changelings wittingly participate in the deceit, and take pains to hide their true form when around others.'
         },
         our_tins_and_our_jars: {
             description: 'Some families help Willy by leaving out their discarded recyclables for him to gather in bulk.'
@@ -87,7 +86,7 @@ module.exports = {
             description: 'I spent a long time wondering why my brother was born with autism and I was not, when our circumstances weren\'t much different. It didn\'t seem far-fetched to suppose that cognitive abilities are a fixed supply in the womb, and that I had selfishly taken my brother\'s share.'
         },
         killcrop: {
-            description: 'In Germanic folklore, a killcrop is an infant suspected of being a changeling due to its voracious appetite. The etymology is plain and simple—one who "kills crops"—and reflects the tragic reality of past times, when children unable to work for their keep could lead to a family\'s ruin.'
+            description: 'In Germanic folklore, a killcrop is an infant suspected of being a changeling due to its voracious appetite. The etymology is plain and simple—one who "kills crops"—and reflects the primary concerns of past times, when children unable to work for their keep could lead to a family\'s ruin.'
         },
         soused_loafers: {
             description: 'In his zeal to grab at a littered beer bottle, Willy steps into a puddle and splashes sewage water onto a pair of gangbangers standing nearby. "Soused loafers" can refer to drenched shoes, as well as to drunken delinquents.'
@@ -102,7 +101,7 @@ module.exports = {
             description: 'Immediately past noon, the ring of a town bell is heard exactly once.'
         },
         unsalvaged_soul: {
-            description: 'Given that abuse of an underdeveloped child often led to its death, religious doctrine in the past typically gave assurance that such children were devoid of souls. I came to a different conclusion. If my brother and I can be made of the same physical matter yet so different in mental presence, then it made more sense to believe that no one has a soul.'
+            description: 'Given that abuse of an underdeveloped child often led to its death, religious doctrine in the past typically gave assurance that such children were devoid of souls. But I came to a different conclusion. If my brother and I were so alike in physical matter, despite being so different in mental presence, then it made more sense to believe that no one has a soul.'
         },
         willys_stalkless_bulb: {
             description: 'This incident happened to Willy, of course, but the lyrics haven\'t mentioned his name until now, since it easily could have been my brother. A "stalkless bulb" can refer to a brain severed from its spine, as well as to a dormant plant ready to be set in the ground.'
@@ -111,7 +110,7 @@ module.exports = {
             description: 'My father liked to build furniture in the garage, and I would help my mother spread the leftover sawdust in our backyard garden. Also, it was the "saddest month" when Willy was killed.'
         },
         mulch_soften_asphodel: {
-            description: 'The asphodel is a popular garden plant, as well as an immortal flower of the underworld in Greek mythology. Since we are made of the same physical matter as the mulch we are using to soften the soil, Willy\'s body must have also "softened the asphalt" beneath which it lay.'
+            description: 'The asphodel is a popular garden plant, as well as an immortal flower of the underworld in Greek mythology. Since we are made of the same physical matter as the mulch we are using to soften the soil, I remark that Willy\'s body must have also "softened the asphalt" beneath which it lay.'
         },
         baby_bluebonnets: {
             description: 'The bluebonnet is another garden flower, and a bonnet is what my mother tucks my brother\'s head in to protect him from the sun. But in light of Willy\'s murder, I wonder how well-protected his head is against other things.'
@@ -120,7 +119,7 @@ module.exports = {
             description: 'Having recently kicked my brother in anger over a shredded comic book, I\'m now indifferent to the latest release from my monthly subscription.'
         },
         how_marbleless: {
-            description: 'Or, "how marvellous." Some friends of my parents, in their poor attempts to empathise, would marvel at how nice it must be for my brother to go through life with a permanent lack of understanding.'
+            description: 'Or, "how marvellous." Sometimes friends of my parents, in a poor attempt to empathise, would marvel at how nice it must be for my brother to go through life with a permanent lack of understanding.'
         },
         imprunt_age_of_mind: {
             description: '"Imprinting" is a period during which an infant acquires certain behaviour, such as emotional attachment. My mother sometimes blamed herself for having failed to imprint my brother correctly. An "imp-runt" is also literally the weakest child in a litter of goblins, and thus a prime choice to be replaced with a human.'
