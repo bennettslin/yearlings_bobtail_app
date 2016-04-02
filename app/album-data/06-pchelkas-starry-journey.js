600bad9e5d2105eb1589bb674bb223e0b5b4119e
Bennett Lin <bennettl@zillow.com>
2016-04-01 21:55:49 -0700

    YB-0 Add annotation to Pchelka.

--- app/album-data/06-pchelkas-starry-journey.js
+++ app/album-data/06-pchelkas-starry-journey.js
@@ -7,21 +7,27 @@ module.exports = {
         /**
          * mail order cult
          */
-        narrative: 'At the height of my years of getting bullied at school, a strange letter arrived promising spiritual answers. With each new payment I sent, it was revealed to me that I belonged to an ancient race of extraterrestrials, with a mission to shut myself from the world. I did so by adopting my brother\'s composure, and felt a psychic bond with him as a result, even as my sanity chipped away. In time, I came to see that I was in a mail order cult. It took a long while to recover.',
-        backstory: 'Pchelka and Mushka were dogs sent into orbit under the Soviet space program. They were killed upon reentry. In keeping with the Russian theme, I used furiously plucked mandolins to evoke the balalaika. Since Tuva is a Russian republic, I also threw in some Tuvan throat singing, where overtones in a lone voice sound like accompaniment by a second voice. I liked the implied notion that my brother is singing along with me in spirit.'
+        narrative: 'At the height of my years of getting bullied at school, a strange letter arrived promising spiritual answers. With each new payment I sent, it was revealed to me that I belonged to an ancient race of extraterrestrials, with a mission to shut myself from the world. I did so by adopting my brother\'s composure, and felt a psychic bond with him as a result, even as my sanity chipped away. In time, I came to see that I was in a mail order cult. It took me a long while to recover.',
+        backstory: 'Pchelka and Mushka were dogs sent into orbit under the Soviet space program. They were killed upon reentry. In keeping with the Russian theme, the spirit of the balalaika is evoked by frenetically plucked mandolins. Since Tuva is a Russian republic, I also threw in Tuvan throat singing, where overtones in a lone voice sound like accompaniment by a second voice. I liked the implied notion that my brother is singing along with me in spirit.'
     },
     annotations: {
         worst_nuisance_on_the_beach: {
-            description: ''
+            description: [
+                'While the experience of being in a mail order cult was painful, there is a comic absurdity to it as well, which this line captures beautifully. In a humourous passage from Leonard Cohen\'s otherwise tragic',
+                {
+                    italic: 'Beautiful Losers,'
+                },
+                'the character F. tires of getting bullied at the beach. He thus enrolls in the Charles Axis bodybuilding course advertised in a comic book, only to discover that Charles Axis is the one who bullies him.'
+            ]
+        },
+        poor_sam_peabody: {
+            description: 'When my family visited Canada one late summer, the white-throated sparrow was in breeding season, and we heard its song everywhere. I found the sound so haunting and soothing, and longed to hear it again ever afterwards. "Poor Sam Peabody, Peabody, Peabody" is a mnemonic used by birdwatchers to mimic the song in cadence and pitch.'
         }
     },
     lyrics: {
         left: [
             [
                 {
-                    /**
-                     * Beautiful Losers, comic book.
-                     */
                     time: 0,
                     verse: {
                         emphasis: [
@@ -90,7 +96,10 @@ module.exports = {
                      */
                     time: 34,
                     verse: {
-                        emphasis: 'Poor Sam Peabody!'
+                        emphasis: {
+                            anchor: 'Poor Sam Peabody!',
+                            annotationKey: 'poor_sam_peabody'
+                        }
                     }
                 }
             ],
