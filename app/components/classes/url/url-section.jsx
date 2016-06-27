5504ceba1fcaf1a4c3e5bf95916f9d0b1c417b02
Bennett Lin <bennettslin@gmail.com>
2016-06-26 22:46:35 -0700

    Add notes.

--- app/components/classes/url/url-section.jsx
+++ app/components/classes/url/url-section.jsx
@@ -9,7 +9,7 @@ class UrlSection extends React.Component {
     // TODO: Force refresh of same page if user clicks on link again.
     // TODO: Automatically scroll to hide search bar on top.
     // TODO: Add navigation and exit button.
-    // TODO: Browser's forward and back buttons should not affect iframe.
+    // TODO: Browser's forward and back buttons should not affect iframe. http://www.webdeveasy.com/back-button-behavior-on-a-page-with-an-iframe/
     render() {
         return (
             <div ref="url"
