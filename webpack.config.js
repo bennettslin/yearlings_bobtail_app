ed6e8af6bf45ee5b519651a8c8fdd37b6fc79fa6
Bennett Lin <bennettl@zillow.com>
2016-07-23 16:35:02 -0700

    Clean up from attempts at server-side rendering.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
-const webpack = require('webpack');
-const path = require('path');
+/**
+ * TODO: Production build and server-side rendering.
+ * See tutorials:
+ * http://survivejs.com/webpack/building-with-webpack/
+ * https://github.com/reactjs/react-router-tutorial (lessons 11 and 13)
+ */
 
-const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build_dev: path.join(__dirname, 'build_dev'),
-    style: path.join(__dirname, 'app/less')
-};
+const webpack = require('webpack'),
+    path = require('path'),
+    PATHS = {
+        app: path.join(__dirname, 'app'),
+        build: path.join(__dirname, 'build'),
+        less: path.join(__dirname, 'app/less')
+    };
 
 module.exports = {
 
@@ -18,15 +24,42 @@ module.exports = {
     },
 
     output: {
-        path: PATHS.build_dev,
+        path: PATHS.build,
         filename: 'bundle.js'
     },
 
-    plugins: process.env.NODE_ENV === 'production' ? [
+    devServer: {
+        contentBase: PATHS.build,
+
+        /**
+         * Enable history API fallback so HTML5 History API based
+         * routing works. This is a good default that will come
+         * in handy in more complicated setups.
+         */
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+
+        // Display only errors to reduce the amount of output.
+        stats: 'errors-only',
+
+        /**
+         * Parse host and port from env so this is easy to customize.
+         * 0.0.0.0 is available to all network devices, unlike default
+         * localhost.
+         */
+        host: process.env.HOST,
+        port: 1337 || process.env.PORT
+    },
+
+    plugins: process.env.NODE_ENV !== 'production' ? [
+        new webpack.HotModuleReplacementPlugin()
+    ] : [
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin()
-    ] : [],
+    ],
 
     module: {
         loaders: [
@@ -38,7 +71,7 @@ module.exports = {
                 // http://survivejs.com/webpack/loading-assets/loading-styles/
                 test: /\.less$/,
                 loader: 'style!css!less',
-                include: PATHS.style
+                include: PATHS.less
             },
             {
                 // Set up jsx. This accepts js too, thanks to RegExp.
@@ -46,7 +79,7 @@ module.exports = {
                 loader: 'babel-loader?presets[]=es2015&presets[]=react',
                 /**
                  * Parse only app files! Without this it will go through the
-                 * entire project. In addition to being slow, that will most
+                 * entire project, which in addition to being slow, will most
                  * likely result in an error.
                  */
                 include: PATHS.app
