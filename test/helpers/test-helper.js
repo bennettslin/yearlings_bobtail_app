696b98ddac882e9d90ff018836042637bb883095
Bennett Lin <bennettl@zillow.com>
2016-07-26 18:56:30 -0700

    Add comment to unit tests.

--- test/helpers/test-helper.js
+++ test/helpers/test-helper.js
@@ -1,3 +1,8 @@
+/**
+ * These unit tests break because the file paths need to be resolved through
+ * Webpack first, and Mocha isn't presently being loaded in Webpack.
+ */
+
 import { jsdom } from 'jsdom'
 global.document = jsdom('')
 global.window = document.defaultView
