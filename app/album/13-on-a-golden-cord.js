920623611d5d24abf19460a4a3b0ddfe5d1712fb
Bennett Lin <bennettl@zillow.com>
2016-08-01 19:57:45 -0700

    Modify Golden Cord annotations.

--- app/album/13-on-a-golden-cord.js
+++ app/album/13-on-a-golden-cord.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: `On a Golden Cord`,
     overviews: {
-        narrative: `After a long night out with friends, I might shuffle on to Sita's place, slip into bed with her, and quickly doze off. But one night Sita was waiting to confront me, having discovered my plans to break up with her while snooping around on my computer earlier. In tears, she returned the gold bracelet I had given her at the start of our relationship, then kicked me out. With nowhere to go, I wandered the streets until sunrise.`,
+        narrative: `After a long night out with friends, I might shuffle on to Sita's place, slip into bed with her, and quickly doze off. But one night, Sita was waiting to confront me, having discovered my plans to break up with her. In tears, she returned the gold bracelet I had given her at the start of our relationship, then kicked me out. With nowhere to go, I wandered the streets until sunrise.`,
         // TODO: Mention that while I was always yielding to Garryowen, it was unusual for Garryowen to yield to me.
         music: [
             `To make sense of the breakup, I had to reconcile contradictory thoughts in my head, much like the citizens of George Orwell's`,
@@ -37,7 +37,7 @@ module.exports = {
                     {
                         anchor: `bread and pocket lint.`,
                         annotation: {
-                            description: `After Sita returned the bracelet, I put it in the front pocket of my jacket, which is where I stashed bread rolls from the cafeteria to munch on later. With my hands tucked in as well to keep warm, I stroked the bracelet over and over across my fingers that night, just as I turned over the thoughts in my head.`
+                            description: `After Sita returned the bracelet, I put it in the front pocket of my jacket, which is where I stashed bread rolls from the cafeteria to munch on later. With my hands tucked in as well to keep warm, I stroked the bracelet over and over that night, just as I turned over the thoughts in my head.`
                         }
                     }
                 ]
@@ -104,7 +104,7 @@ module.exports = {
                         {
                             anchor: `went through your mail`,
                             annotation: {
-                                description: `Confused about why I had grown unusually distant, Sita finally broke down earlier that day and sifted through the emails on my computer, which I kept at her place because it was a desktop. She thus discovered that I was flirting online with a woman I had met at one of my shows, to whom I was giving assurances that I would be single again soon.`
+                                description: `Confused about why I had grown unusually distant, Sita finally caved in that day and decided to snoop around on my computer, which I kept at her place because it was a desktop. She thus discovered that I was flirting online with a woman I had met at one of my shows, to whom I was giving assurances that I would be single again soon.`
                             }
                         },
                         `today…"`
