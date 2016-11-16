233eaa9f8dc6283f3fb3cd909b6942f181b94c48
Bennett Lin <bennettslin@gmail.com>
2016-11-16 00:29:35 -0800

    Remove comment.

--- app/album/16-cremated.js
+++ app/album/16-cremated.js
@@ -197,7 +197,6 @@ module.exports = {
                 left: `As comedy teams work, a hoot the times`,
                 right: [
                     {
-                        // TODO: Mention phoenix
                         anchor: `His god humming "Freebird,"`,
                         annotation: {
                             description: [
