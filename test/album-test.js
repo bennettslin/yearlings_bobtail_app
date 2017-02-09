4f66ff28972f8c1d6f846205fa5449185deca90d
Bennett Lin <bennettl@zillow.com>
2017-02-08 21:06:56 -0800

    Change path for album unit test.

--- test/album-test.js
+++ test/album-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
-import Album from '../app/components/album.jsx'
+import Album from '../app/components/admin/album.jsx'
 
 describe('Album', () => {
     let album
