9a9843bd64228c8bf2dcbec6f25dd7a2ed1ab0fe
Bennett Lin <bennettl@zillow.com>
2017-02-19 11:53:38 -0800

    Add music annotation to Odin.

--- app/album/04-odin.js
+++ app/album/04-odin.js
@@ -913,6 +913,22 @@ module.exports = {
             },
             {
                 unitMap: true,
+                dotStanza: {
+                    anchor: DOT_STANZA_CONSTANT,
+                    annotation: {
+                        description: [
+                            `Recording vocals in doublespeaker rhyme turned out to be trickier than I had assumed. The lines rarely lined up naturally because even when speaking in rhythm, we tend to shorten or lengthen our syllables based on the relative strength of consonants, the spaces between words, and so on. I had to improvise a system of`,
+                            {
+                                anchor: `ligature`,
+                                wiki: `Ligature_(music)`
+                            },
+                            `to keep my syllabic durations and offsets in sync.`
+                        ],
+                        dotKeys: {
+                            [SONGWRITING]: true
+                        }
+                    }
+                },
                 sectionClass: VERSES
             }
         ],
@@ -1362,7 +1378,7 @@ module.exports = {
         },
         {
             taskName: `music annotations`,
-            workedHours: 2,
+            workedHours: 3,
             neededHours: 3
         },
         {
