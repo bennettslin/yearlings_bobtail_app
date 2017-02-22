81512b1843cbc46b71ad796c8bda5d4d4dfdb63f
Bennett Lin <bennettl@zillow.com>
2017-02-21 18:15:27 -0800

    Modify music annotation in Golden Cord.

--- app/album/13-on-a-golden-cord.js
+++ app/album/13-on-a-golden-cord.js
@@ -148,7 +148,7 @@ module.exports = {
                                 anchor: `Johnny Horton,`,
                                 wiki: `Johnny_Horton`
                             },
-                            `whose albums were the first ones I owned because my mother loved him in her teenybopper years. None of my other friends knew who he was, until one day I met a Taiwanese-American girl whose mother had the same obsession. So I guess Horton was big in Taiwan, although he probably never knew that, having died in 1960 at age 35.`
+                            `whose albums were the first ones I owned because my mother loved him in her teenybopper years. None of my other friends knew who he was, until one day I met another Taiwanese-American kid whose mother had the same obsession. So I guess Horton was big in Taiwan, though he probably never knew that, having died in 1960 at age 35.`
                         ],
                         dotKeys: {
                             [BACKSTORY]: true,
