b346222600ebe6b5d4c70f743a1cf0be31b9694f
Bennett Lin <bennettl@zillow.com>
2017-03-25 16:24:21 -0700

    Continue composing.

--- webpack.config.js
+++ webpack.config.js
@@ -23,75 +23,49 @@ const webpack = require('webpack'),
         less: path.resolve(__dirname, 'app/less')
     };
 
-const commonConfig = {
+const commonConfig = merge([
+    {
+        /**
+         * Entry accepts a path or an object of entries. We'll be using the latter
+         * form given it's convenient with more complex configurations.
+         */
+        entry: {
+            app: PATHS.app
+        },
 
-    resolve: {
-        // import from files without specifying extensions.
-        extensions: ['', '.js', '.jsx', '.less', '.mp3', '.pdf']
+        output: {
+            path: PATHS.build,
+            filename: 'bundle.js'
+        },
+
+        plugins: process.env.NODE_ENV !== 'production' ? [
+            new webpack.HotModuleReplacementPlugin()
+        ] : [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin()
+        ],
+
+        resolve: {
+            // import from files without specifying extensions.
+            extensions: ['', '.js', '.jsx', '.less', '.mp3', '.pdf']
+        }
     },
-
-    /**
-     * Entry accepts a path or an object of entries. We'll be using the latter
-     * form given it's convenient with more complex configurations.
-     */
-    entry: {
-        app: PATHS.app
-    },
-
-    output: {
-        path: PATHS.build,
-        filename: 'bundle.js'
-    },
-
-    /**
-     * From Enzyme docs:
-     * http://airbnb.io/enzyme/docs/guides/webpack.html
-     */
-    externals: {
-        'react/addons': true,
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true
-    },
-
-    plugins: process.env.NODE_ENV !== 'production' ? [
-        new webpack.HotModuleReplacementPlugin()
-    ] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin()
-    ],
-
-    module: {
-        loaders: [
-            {
-                // Set up jsx. This accepts js too, thanks to RegExp.
-                test: /\.jsx?$/,
-                enforce: 'pre',
-                loaders: [
-                    'babel-loader?presets[]=es2015&presets[]=react',
-                    'eslint-loader'
-                ],
-                /**
-                 * Parse only app files! Without this it will go through the
-                 * entire project, which in addition to being slow, will most
-                 * likely result in an error.
-                 */
-                include: PATHS.app
-            },
-            {
-                // http://survivejs.com/webpack/loading-assets/loading-styles/
-                test: /\.less$/,
-                loader: 'style!css!less',
-                include: PATHS.less
-            },
-            {
-                test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif|mp3|pdf)$/,
-                loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
-                include: PATHS.app
-            }
-        ]
-    }
-}
+    parts.loadJavaScript({
+        /**
+         * Parse only app files! Without this it will go through the
+         * entire project, which in addition to being slow, will most
+         * likely result in an error.
+         */
+        include: PATHS.app
+    }),
+    parts.loadStyles({
+        include: PATHS.less
+    }),
+    parts.loadUrls({
+        include: PATHS.app
+    })
+])
 
 const productionConfig = merge([]);
 const developmentConfig = merge([
--- webpack.parts.js
+++ webpack.parts.js
@@ -25,3 +25,47 @@ exports.devServer = ({ contentBase, host, port } = {}) => ({
         stats: 'errors-only'
     }
 })
+
+exports.loadJavaScript = ({ include }) => ({
+    module: {
+        loaders: [
+            {
+                include,
+
+                // Set up jsx. This accepts js too, thanks to RegExp.
+                test: /\.jsx?$/,
+                enforce: 'pre',
+                loaders: [
+                    'babel-loader?presets[]=es2015&presets[]=react',
+                    'eslint-loader'
+                ]
+            }
+        ]
+    }
+})
+
+exports.loadStyles = ({ include }) => ({
+    module: {
+        loaders: [
+            {
+                include,
+
+                // http://survivejs.com/webpack/loading-assets/loading-styles/
+                test: /\.less$/,
+                loader: 'style!css!less'
+            }
+        ]
+    }
+})
+
+exports.loadUrls = ({ include }) => ({
+    module: {
+        loaders: [
+            {
+                include,
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif|mp3|pdf)$/,
+                loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+            }
+        ]
+    }
+})
