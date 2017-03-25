30ec33c33e6db59c61553505fc2089d00622c33e
Bennett Lin <bennettl@zillow.com>
2017-03-25 16:59:01 -0700

    Rename root to app.

--- app/index.js
+++ app/index.js
@@ -15,5 +15,5 @@ ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <Router history={browserHistory} routes={routes} />
     </Provider>,
-    document.getElementById('root')
+    document.getElementById('app')
 )
