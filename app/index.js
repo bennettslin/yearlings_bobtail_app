78dbc2f3e1420f14b4bb70a2f1073c1d2dfb4908
Bennett Lin <bennettl@zillow.com>
2017-03-25 17:50:20 -0700

    Get rid of router code.

--- app/index.js
+++ app/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom'
 import { createStore,
          applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
+// import { Router, browserHistory } from 'react-router'
 
 import reducers from './redux/reducers'
-import routes from './routes'
+// import routes from './routes'
+import Wrapper from './routes'
 
 // From Udemy tutorial. This will allow for asynchronous Redux actions.
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <Router history={browserHistory} routes={routes} />
+        {/* <Router history={browserHistory} routes={routes} /> */}
+        <Wrapper />
     </Provider>,
     document.getElementById('app')
 )
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, IndexRoute } from 'react-router'
+// import { Route, IndexRoute } from 'react-router'
 
 import './less/main.less'
 import App from './components/app'
@@ -9,22 +9,22 @@ import { prepareAlbumData } from './helpers/album-build-helper'
 // Restructure raw data to be usable by app.
 prepareAlbumData(AlbumData)
 
-class Root extends Component {
-    render() {
-        return (
-            <div className="index">
-                {this.props.children}
-            </div>
-        )
-    }
-}
+// class Root extends Component {
+//     render() {
+//         return (
+//             <div className="index">
+//                 {this.props.children}
+//             </div>
+//         )
+//     }
+// }
 
 /**
  * FIXME: This component passes the album data as props to the app. This is just
  * a workaround until I figure out how to structure the components to play well
  * with React Router.
  */
-class Wrapper extends React.Component {
+export default class Wrapper extends Component {
     render() {
         return (
             <App {...AlbumData} />
@@ -32,9 +32,9 @@ class Wrapper extends React.Component {
     }
 }
 
-export default (
-    <Route path="/" component={Root}>
-        <IndexRoute component={Wrapper} />
-        <Route path="/yearlings_bobtail" component={Wrapper} />
-    </Route>
-)
+// export default (
+//     <Route path="/" component={Root}>
+//         <IndexRoute component={Wrapper} />
+//         <Route path="/yearlings_bobtail" component={Wrapper} />
+//     </Route>
+// )
