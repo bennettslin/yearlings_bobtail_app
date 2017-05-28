93130e15aef2fb56a5b4644fee975ea1db28cd8d
Bennett Lin <bennettslin@gmail.com>
2017-05-28 09:46:23 -0700

    Overview checks if component should update.

--- app/components/overview/overview-transition-popup.jsx
+++ app/components/overview/overview-transition-popup.jsx
@@ -1,51 +1,74 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import OverviewSection from './overview-section'
 import TransitionPopup from '../popup/transition-popup'
+import { getComponentShouldUpdate } from '../../helpers/general-helper'
 
 /*************
  * CONTAINER *
  *************/
 
-const OverviewTransitionPopup = ({
+class OverviewTransitionPopup extends Component {
 
-    handlePopupContainerClick,
+    shouldComponentUpdate(nextProps) {
+        const { props } = this,
+            componentShouldUpdate = getComponentShouldUpdate({
+                props,
+                nextProps,
+                updatingPropsArray: [
+                    'overviewText',
+                    'selectedOverviewIndex',
+                    'selectedTitleIndex',
+                    'isPhone',
+                    'isLogue',
+                    'inOverviewSubfield'
+                ]
+            })
 
-...other }) => {
+        return componentShouldUpdate
+    }
 
-    const { isLogue,
-            inOverviewSubfield,
-            selectedOverviewIndex,
-            selectedTitleIndex } = other,
+    render() {
+        const { isLogue,
+                inOverviewSubfield,
+                selectedOverviewIndex,
+                selectedTitleIndex,
+                handlePopupContainerClick,
+                ...other } = this.props,
 
-        myChild = (
-            <OverviewSection {...other} />
+            myChild = (
+                <OverviewSection {...other}
+                    isLogue={isLogue}
+                />
+            )
+
+        let isVisible
+
+        if (isLogue) {
+            isVisible = !inOverviewSubfield
+        } else {
+            isVisible = selectedOverviewIndex ? false : inOverviewSubfield
+        }
+
+        // Always hide overview when title is selected.
+        if (selectedTitleIndex) {
+            isVisible = false
+        }
+
+        return (
+            <TransitionPopup
+                popupClassName="overview"
+                isVisible={isVisible}
+                handlePopupContainerClick={handlePopupContainerClick}
+                myChild={myChild}
+            />
         )
-
-    let isVisible
-
-    if (isLogue) {
-        isVisible = !inOverviewSubfield
-    } else {
-        isVisible = selectedOverviewIndex ? false : inOverviewSubfield
     }
-
-    // Always hide overview when title is selected.
-    if (selectedTitleIndex) {
-        isVisible = false
-    }
-
-    return (
-        <TransitionPopup
-            popupClassName="overview"
-            isVisible={isVisible}
-            handlePopupContainerClick={handlePopupContainerClick}
-            myChild={myChild}
-        />
-    )
 }
 
 OverviewTransitionPopup.propTypes = {
+    overviewText: PropTypes.string.isRequired,
+    isPhone: PropTypes.bool.isRequired,
     isLogue: PropTypes.bool.isRequired,
     inOverviewSubfield: PropTypes.bool.isRequired,
     selectedOverviewIndex: PropTypes.number.isRequired,
