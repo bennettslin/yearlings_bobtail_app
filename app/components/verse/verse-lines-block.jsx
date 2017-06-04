944aacac66d73f76ac4acc85a4a2076b20dcd9e4
Bennett Lin <bennettslin@gmail.com>
2017-06-04 13:12:57 -0700

    Create verse lines component, with bug.

--- app/components/verse/verse-lines-block.jsx
+++ app/components/verse/verse-lines-block.jsx
@@ -1,6 +1,9 @@
 // Component to show all lines in a single verse.
 import React from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import VerseLine from './verse-line'
+import { DOUBLESPEAKER_KEYS, TITLE, LEFT, CENTRE, LYRIC } from '../../constants/lyrics'
 
 /*************
  * CONTAINER *
@@ -8,31 +11,72 @@ import PropTypes from 'prop-types'
 
 const VerseLinesBlock = ({
 
+    isDoubleSpeaker,
 
 ...other }) => {
-    return (
-        <VerseLinesBlockView {...other}
-        />
-    )
+
+    if (isDoubleSpeaker) {
+        return (
+            <VerseLinesBlockView {...other} />
+        )
+
+    } else {
+        const { inVerseBar,
+                isSelected,
+                verseObject,
+                hiddenLyricColumnKey,
+                columnKey,
+                isTitle } = other,
+
+            lyricsLineProps = {
+                inVerseBar,
+                verseSelected: isSelected,
+                text: columnKey ? verseObject[columnKey] : verseObject[LYRIC] || verseObject[CENTRE],
+                isVerseBeginningSpan: verseObject.isVerseBeginningSpan,
+                isVerseEndingSpan: verseObject.isVerseEndingSpan,
+                isHidden: !!hiddenLyricColumnKey,
+                columnKey: columnKey || isTitle ? TITLE : LEFT
+            }
+
+        return <VerseLine {...other} {...lyricsLineProps} />
+    }
+}
+
+VerseLinesBlock.defaultProps = {
+    isDoubleSpeaker: false
 }
 
 VerseLinesBlock.propTypes = {
-
+    isDoubleSpeaker: PropTypes.bool.isRequired
 }
 
 /****************
  * PRESENTATION *
  ****************/
 
-const VerseLinesBlockView = ({
+const VerseLinesBlockView = (props) => {
+    const { hiddenLyricColumnKey } = props
 
-...other }) => (
-
-
-)
+    return (
+        <div className={classnames(
+            'double-lines-block',
+            // FIXME: Take care of this in CSS.
+            { 'hidden-left': hiddenLyricColumnKey === LEFT }
+        )}>
+            {DOUBLESPEAKER_KEYS.map((columnKey, index) => {
+                return (
+                    <VerseLinesBlock {...props}
+                        key={index}
+                        columnKey={columnKey}
+                    />
+                )
+            })}
+        </div>
+    )
+}
 
 VerseLinesBlockView.propTypes = {
-
+    hiddenLyricColumnKey: PropTypes.string.isRequired
 }
 
 export default VerseLinesBlock
