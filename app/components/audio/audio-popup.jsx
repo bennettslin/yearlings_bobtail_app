e76efd0d265dd7166101778233f4f6fc6336109a
Bennett Lin <bennettslin@gmail.com>
2017-06-14 20:23:22 -0700

    Minor change.

--- app/components/audio/audio-popup.jsx
+++ app/components/audio/audio-popup.jsx
@@ -26,8 +26,7 @@ class AudioPopup extends Component {
                     'selectedAnnotationIndex',
                     'selectedScoreIndex',
                     'selectedTitleIndex',
-                    'selectedWikiIndex',
-                    'isTimerInAudio'
+                    'selectedWikiIndex'
                 ]
             })
 
