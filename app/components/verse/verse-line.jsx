3f0585a5ba08985bdb2a6e81c16fd152d8222754
Bennett Lin <bennettslin@gmail.com>
2017-06-17 19:29:46 -0700

    Delete setDomWidth in verse line.

--- app/components/verse/verse-line.jsx
+++ app/components/verse/verse-line.jsx
@@ -5,22 +5,15 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import classnames from 'classnames'
 import TextBlock from '../text/text-block'
-import { TITLE, CENTRE } from '../../constants/lyrics'
+import { TITLE } from '../../constants/lyrics'
 import { getComponentShouldUpdate } from '../../helpers/general-helper'
 
 class VerseLine extends Component {
 
-    componentDidMount() {
-        /**
-         * FIXME: Setting timeout of zero seems to work! Keep debugging code in
-         * place for now. Rip it out once confident that this is indeed the
-         * final fix for this issue.
-         */
-
-        window.setTimeout(this.setDOMWidth.bind(this, true), 0)
-        window.setTimeout(this.setDOMWidth.bind(this), 1)
-    }
-
+    /**
+     * NOTE: There is still some lingering weirdness with calculating width,
+     * but only in Chrome and Safari, it seems.
+     */
     shouldComponentUpdate(nextProps) {
         const { props } = this,
             componentShouldUpdate = getComponentShouldUpdate({
@@ -41,78 +34,20 @@ class VerseLine extends Component {
         return componentShouldUpdate
     }
 
-    /**
-     * Ugly workaround to adjust line width to fit child text when text wraps
-     * onto a new line, but it works. (Well, fingers crossed.) Update: Seems
-     * this only needs to be done when component is updated, not when it is
-     * first mounted?
-     */
-    componentWillUpdate() {
-        this.setDOMWidth(true)
-    }
-
-    componentDidUpdate() {
-        this.setDOMWidth()
-    }
-
-    setDOMWidth(unset, debugLogOnly) {
-        /**
-         * TODO: I'm not sure why this method is buggy with centre columns. So
-         * I'm excluding it for now, since the only songs that have them, the
-         * doublespeaker ones, don't need them for wide layout, and of course
-         * centre columns become left columns in narrow layout.
-         */
-
-        /**
-         * TODO: Now that the issue seems resolved, can we not exclude centre
-         * columns?
-         */
-        if (this.props.columnKey !== TITLE && this.props.columnKey !== CENTRE) {
-            const parent = this.myParent
-            if (parent) {
-                if (unset) {
-                    // parent.removeAttribute('style')
-
-                    // Just an arbitrarily large number.
-                    parent.style.maxWidth = '5000px'
-
-                } else {
-                    // FIXME: The issue seems to be that the offsetWidth is wrong when this method gets called in componentDidMount.
-
-                    const child = this.myChild,
-                        offsetWidth = child.offsetWidth
-
-                    // if (this.props.text === 'Oh, has Willy the Cocoa come') {
-                    //     console.error('offsetWidth', offsetWidth);
-                    // }
-
-                    if (!debugLogOnly) {
-                        // Allow for 10px padding on each side.
-                        parent.style.maxWidth = offsetWidth - 20 + 'px'
-                    }
-                }
-            }
-        }
-    }
-
     render() {
         const { columnKey,
                 ...other } = this.props
 
         return (
             <div
-                ref={(node) => (this.myParent = node)}
                 className={classnames(
                     'line',
                     { [columnKey]: columnKey !== TITLE }
                 )}
             >
-                <span ref={(node) => (this.myChild = node)}>
-                    <TextBlock {...other}
-                        ref={(node) => (this.myReactChild = node)}
-                        isLyric={true}
-                    />
-                </span>
+                <TextBlock {...other}
+                    isLyric={true}
+                />
             </div>
         )
     }
