f054c4250086de3d44003ebbd5b967f4017be9ba
Bennett Lin <bennettslin@gmail.com>
2017-06-30 17:34:12 -0700

    Begin work to make overviews look nice in logues.

--- app/components/overview/overview-section.jsx
+++ app/components/overview/overview-section.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import TextBlock from '../text/text-block'
 import OverviewToggle from './overview-toggle'
-import { getSongOverview } from '../../helpers/data-helper'
+import { getSongOverview, getSongIsLogue } from '../../helpers/data-helper'
 import { getIsToggleInOverview } from '../../helpers/responsive-helper'
 import { SHOWN, OVERVIEW_OPTIONS } from '../../constants/options'
 
@@ -28,12 +28,23 @@ class OverviewSection extends Component {
 
     render() {
         const { deviceIndex,
+                isHeightlessLyricColumn,
                 selectedOverviewIndex,
+                selectedSongIndex,
                 overviewIndex } = this.props,
 
             overviewText = getSongOverview(overviewIndex),
-            isToggleInOverview = getIsToggleInOverview(deviceIndex),
-            isEnabled = OVERVIEW_OPTIONS[selectedOverviewIndex] === SHOWN
+            isLogue = getSongIsLogue(selectedSongIndex),
+
+            /**
+             * Always show in phone. Also show when is logue and is heightless
+             * lyric.
+             */
+            isToggleInOverview = getIsToggleInOverview(deviceIndex) ||
+                (isLogue && isHeightlessLyricColumn),
+            isEnabled =
+                OVERVIEW_OPTIONS[selectedOverviewIndex] === SHOWN &&
+                !isLogue
 
         return (
             <div className="section overview-section">
@@ -60,6 +71,7 @@ OverviewSection.propTypes = {
     // Through Redux.
     deviceIndex: PropTypes.number.isRequired,
     selectedOverviewIndex: PropTypes.number.isRequired,
+    selectedSongIndex: PropTypes.number.isRequired,
 
     // From parent.
     overviewIndex: PropTypes.number.isRequired,
@@ -68,8 +80,12 @@ OverviewSection.propTypes = {
 
 export default connect(({
     deviceIndex,
-    selectedOverviewIndex
+    isHeightlessLyricColumn,
+    selectedOverviewIndex,
+    selectedSongIndex
 }) => ({
     deviceIndex,
-    selectedOverviewIndex
+    isHeightlessLyricColumn,
+    selectedOverviewIndex,
+    selectedSongIndex
 }))(OverviewSection)
