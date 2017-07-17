dd20ac3bbf470ebb237c7417ef5e302bf3390ee5
Bennett Lin <bennettslin@gmail.com>
2017-07-16 21:19:06 -0700

    Add drawing hour for Bennett in Golden Cord.

--- app/album/13-on-a-golden-cord.js
+++ app/album/13-on-a-golden-cord.js
@@ -35,6 +35,7 @@ module.exports = {
             actors: {
                 bennett: {
                     todo: true,
+                    workedHours: 1,
                     description: 'back, cradling bracelet, blank'
                 },
                 stephanie: {
