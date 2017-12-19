496ff22f07640dc8ecd6869f88dccec6a338ae00
Bennett Lin <bennettslin@gmail.com>
2017-12-18 21:23:13 -0800

    Log hours for beginning theatre seating and balcony calculations.

--- app/album/remaining-tasks.js
+++ app/album/remaining-tasks.js
@@ -6,8 +6,8 @@ module.exports = [
     },
     {
         taskName: `Theatre seats and balconies calculations.`,
-        workedHours: 0,
-        neededHours: 3
+        workedHours: 3,
+        neededHours: 6
     },
     {
         taskName: `Keep admin stuff in dev, not in production.`,
