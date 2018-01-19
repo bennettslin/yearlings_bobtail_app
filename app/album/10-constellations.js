1c9a7854feaa103eba27dfe5aa93a4ea75f53251
Bennett Lin <bennettslin@gmail.com>
2018-01-18 20:19:54 -0800

    Correct log mistake.

--- app/album/10-constellations.js
+++ app/album/10-constellations.js
@@ -32,7 +32,7 @@ module.exports = {
             actors: {
                 bennett: {
                     todo: true,
-                    workedHours: 1.75,
+                    workedHours: 1.25,
                     description: 'side, walking'
                 },
                 ana: {
