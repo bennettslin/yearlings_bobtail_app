2e4ddca8901d1e77d492b23914fe808dcb7e5628
bennettlincomet <bennettl@zillowgroup.com>
2018-02-21 06:20:32 -0800

    Allow Redux dev tools extension.

--- app/index.js
+++ app/index.js
@@ -21,8 +21,13 @@ if (process.env.NODE_ENV !== 'production' && turnOnWDYULogging) {
 // From Udemy tutorial. This will allow for asynchronous Redux actions.
 // const createStoreWithMiddleware = applyMiddleware()(createStore)
 
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <Router>
             <Switch>
                 <Route exact path="/" component={RoutingContainer} />
