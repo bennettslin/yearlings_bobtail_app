191a71a4d77879bb978fb1fb2b268f9a589de813
bennettlincomet <bennettl@zillowgroup.com>
2018-03-09 19:34:24 -0800

    Log Miriam masked woman tops.

--- app/album/07-m.js
+++ app/album/07-m.js
@@ -154,7 +154,7 @@ module.exports = {
                 miriam: {
                     maskedWoman: {
                         todo: true,
-                        workedHours: 1.75,
+                        workedHours: 2.5,
                         description: 'side, holding baby, looking back at Bennett'
                     },
                 }
@@ -177,7 +177,7 @@ module.exports = {
                 miriam: {
                     maskedWoman: {
                         todo: true,
-                        workedHours: 1.75,
+                        workedHours: 2.5,
                         description: 'side, walking away'
                     },
                 },
