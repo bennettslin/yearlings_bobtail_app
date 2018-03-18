ee1d1257bde6ba176c444a8918739da79eb2c7d8
Bennett Lin <bennettslin@gmail.com>
2018-03-18 00:12:30 -0700

    Log revealing tops.

--- app/album/14-kyon.js
+++ app/album/14-kyon.js
@@ -128,7 +128,7 @@ module.exports = {
                 liz: {
                     sita: {
                         todo: true,
-                        workedHours: 1.75,
+                        workedHours: 2.5,
                         description: 'side, seated, asleep'
                     }
                 }
--- app/album/17-vegan-proclamation.js
+++ app/album/17-vegan-proclamation.js
@@ -150,7 +150,7 @@ module.exports = {
                 amy: {
                     demonstrator: {
                         todo: true,
-                        workedHours: 1.75,
+                        workedHours: 2.5,
                         description: 'back, passing out flyers'
                     }
                 },
