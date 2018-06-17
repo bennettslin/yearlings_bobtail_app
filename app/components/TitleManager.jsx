ec416c4c7b1a193a6ca3849bb4fd156157494f77
Bennett Lin <bennettl@zillowgroup.com>
2018-06-17 09:35:38 -0700

    Add more placeholder managers.

---
+++ app/components/TitleManager.jsx
@@ -0,0 +1,32 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+class TitleManager extends Component {
+
+    static defaultProps = {
+    }
+
+    static propTypes = {
+        getRef: PropTypes.func.isRequired
+    }
+
+    componentDidMount() {
+        this.props.getRef(this)
+    }
+
+    render() {
+        return null
+    }
+}
+
+const mapStateToProps = (state) => (state)
+
+const bindDispatchToProps = (dispatch) => (
+    bindActionCreators({
+
+    }, dispatch)
+)
+
+export default connect(mapStateToProps, bindDispatchToProps)(TitleManager)
