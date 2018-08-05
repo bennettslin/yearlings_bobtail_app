ec1e7882b20295cac0e3cb4d59bc80915b9796ae
Bennett Lin <bennettl@zillowgroup.com>
2018-08-05 11:37:53 -0700

    Simplify YearlingsBobtail file.

--- app/album/YearlingsBobtail.js
+++ app/album/YearlingsBobtail.js
@@ -1,4 +1,6 @@
 import { HYPHENATED_SONG_PATHS } from '../constants/paths'
+import { getTwoDigitStringForNumber } from '../helpers/formatHelper'
+import { getArrayOfLength } from '../helpers/generalHelper'
 
 module.exports = {
     title: `Yearling's Bobtail`,
@@ -59,68 +61,27 @@ module.exports = {
             description: `Dream Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
         },
     },
-    mp3s: [
-        require(`../assets/mp3s/01_${HYPHENATED_SONG_PATHS[1]}.mp3`),
-        require(`../assets/mp3s/02_${HYPHENATED_SONG_PATHS[2]}.mp3`),
-        require(`../assets/mp3s/03_${HYPHENATED_SONG_PATHS[3]}.mp3`),
-        require(`../assets/mp3s/04_${HYPHENATED_SONG_PATHS[4]}.mp3`),
-        require(`../assets/mp3s/05_${HYPHENATED_SONG_PATHS[5]}.mp3`),
-        require(`../assets/mp3s/06_${HYPHENATED_SONG_PATHS[6]}.mp3`),
-        require(`../assets/mp3s/07_${HYPHENATED_SONG_PATHS[7]}.mp3`),
-        require(`../assets/mp3s/08_${HYPHENATED_SONG_PATHS[8]}.mp3`),
-        require(`../assets/mp3s/09_${HYPHENATED_SONG_PATHS[9]}.mp3`),
-        require(`../assets/mp3s/10_${HYPHENATED_SONG_PATHS[10]}.mp3`),
-        require(`../assets/mp3s/11_${HYPHENATED_SONG_PATHS[11]}.mp3`),
-        require(`../assets/mp3s/12_${HYPHENATED_SONG_PATHS[12]}.mp3`),
-        require(`../assets/mp3s/13_${HYPHENATED_SONG_PATHS[13]}.mp3`),
-        require(`../assets/mp3s/14_${HYPHENATED_SONG_PATHS[14]}.mp3`),
-        require(`../assets/mp3s/15_${HYPHENATED_SONG_PATHS[15]}.mp3`),
-        require(`../assets/mp3s/16_${HYPHENATED_SONG_PATHS[16]}.mp3`),
-        require(`../assets/mp3s/17_${HYPHENATED_SONG_PATHS[17]}.mp3`),
-        require(`../assets/mp3s/18_${HYPHENATED_SONG_PATHS[18]}.mp3`)
-    ],
-    scores: [
-        require(`../assets/scores/01_${HYPHENATED_SONG_PATHS[1]}.pdf`),
-        require(`../assets/scores/02_${HYPHENATED_SONG_PATHS[2]}.pdf`),
-        require(`../assets/scores/03_${HYPHENATED_SONG_PATHS[3]}.pdf`),
-        require(`../assets/scores/04_${HYPHENATED_SONG_PATHS[4]}.pdf`),
-        require(`../assets/scores/05_${HYPHENATED_SONG_PATHS[5]}.pdf`),
-        require(`../assets/scores/06_${HYPHENATED_SONG_PATHS[6]}.pdf`),
-        require(`../assets/scores/07_${HYPHENATED_SONG_PATHS[7]}.pdf`),
-        require(`../assets/scores/08_${HYPHENATED_SONG_PATHS[8]}.pdf`),
-        require(`../assets/scores/09_${HYPHENATED_SONG_PATHS[9]}.pdf`),
-        require(`../assets/scores/10_${HYPHENATED_SONG_PATHS[10]}.pdf`),
-        require(`../assets/scores/11_${HYPHENATED_SONG_PATHS[11]}.pdf`),
-        require(`../assets/scores/12_${HYPHENATED_SONG_PATHS[12]}.pdf`),
-        require(`../assets/scores/13_${HYPHENATED_SONG_PATHS[13]}.pdf`),
-        require(`../assets/scores/14_${HYPHENATED_SONG_PATHS[14]}.pdf`),
-        require(`../assets/scores/15_${HYPHENATED_SONG_PATHS[15]}.pdf`),
-        require(`../assets/scores/16_${HYPHENATED_SONG_PATHS[16]}.pdf`),
-        require(`../assets/scores/17_${HYPHENATED_SONG_PATHS[17]}.pdf`),
-        require(`../assets/scores/18_${HYPHENATED_SONG_PATHS[18]}.pdf`)
-    ],
-    songs: [
-        require(`./00_${HYPHENATED_SONG_PATHS[0]}`),
-        require(`./01_${HYPHENATED_SONG_PATHS[1]}`),
-        require(`./02_${HYPHENATED_SONG_PATHS[2]}`),
-        require(`./03_${HYPHENATED_SONG_PATHS[3]}`),
-        require(`./04_${HYPHENATED_SONG_PATHS[4]}`),
-        require(`./05_${HYPHENATED_SONG_PATHS[5]}`),
-        require(`./06_${HYPHENATED_SONG_PATHS[6]}`),
-        require(`./07_${HYPHENATED_SONG_PATHS[7]}`),
-        require(`./08_${HYPHENATED_SONG_PATHS[8]}`),
-        require(`./09_${HYPHENATED_SONG_PATHS[9]}`),
-        require(`./10_${HYPHENATED_SONG_PATHS[10]}`),
-        require(`./11_${HYPHENATED_SONG_PATHS[11]}`),
-        require(`./12_${HYPHENATED_SONG_PATHS[12]}`),
-        require(`./13_${HYPHENATED_SONG_PATHS[13]}`),
-        require(`./14_${HYPHENATED_SONG_PATHS[14]}`),
-        require(`./15_${HYPHENATED_SONG_PATHS[15]}`),
-        require(`./16_${HYPHENATED_SONG_PATHS[16]}`),
-        require(`./17_${HYPHENATED_SONG_PATHS[17]}`),
-        require(`./18_${HYPHENATED_SONG_PATHS[18]}`),
-        require(`./19_${HYPHENATED_SONG_PATHS[19]}`),
-    ],
+    mp3s: getArrayOfLength(18, 1).map(index => (
+        require(`../assets/mp3s/${
+            getTwoDigitStringForNumber(index)
+        }_${
+            HYPHENATED_SONG_PATHS[index]
+        }.mp3`)
+    )),
+    scores: getArrayOfLength(18, 1).map(index => (
+        require(`../assets/scores/${
+            getTwoDigitStringForNumber(index)
+        }_${
+            HYPHENATED_SONG_PATHS[index]
+        }.pdf`)
+    )),
+    songs: getArrayOfLength(20).map(index => (
+        require(`./${
+            getTwoDigitStringForNumber(index)
+        }_${
+            HYPHENATED_SONG_PATHS[index]
+        }`)
+    )),
     remainingTasks: require('./tasks/remainingTasks')
         .concat(require('./tasks/detailedDrawingTasks'))
         .concat(require('./tasks/illustratorDrawingTasks'))
--- app/helpers/formatHelper.js
+++ app/helpers/formatHelper.js
@@ -176,3 +176,7 @@ export const getCharStringForNumber = (myNumber) => {
     // 10 is a, 11 is b, and so forth.
     return myNumber < 10 ? myNumber : String.fromCharCode(myNumber + 87)
 }
+
+export const getTwoDigitStringForNumber = (myNumber) => {
+    return myNumber < 10 ? `0${myNumber}` : myNumber
+}
