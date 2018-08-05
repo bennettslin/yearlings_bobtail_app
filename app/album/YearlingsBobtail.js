20a59903057d237ce9e2226e4770283d77cebc12
Bennett Lin <bennettl@zillowgroup.com>
2018-08-05 11:43:56 -0700

    Move tips.

--- app/album/YearlingsBobtail.js
+++ app/album/YearlingsBobtail.js
@@ -5,62 +5,7 @@ import { getArrayOfLength } from '../helpers/generalHelper'
 module.exports = {
     title: `Yearling's Bobtail`,
     bookStartingIndices: [1, 10],
-    tips: {
-        1: {
-            description: `Didi Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        2: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        3: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        4: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        5: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        6: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        7: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        8: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        9: {
-            description: `Grasshoppers Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        10: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        11: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        12: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        13: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        14: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        15: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        16: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        17: {
-            description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-        18: {
-            description: `Dream Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
-        },
-    },
+    tips: require('./tips/tips'),
     mp3s: getArrayOfLength(18, 1).map(index => (
         require(`../assets/mp3s/${
             getTwoDigitStringForNumber(index)
---
+++ app/album/tips/tips.js
@@ -0,0 +1,56 @@
+module.exports = {
+    1: {
+        description: `Didi Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    2: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    3: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    4: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    5: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    6: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    7: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    8: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    9: {
+        description: `Grasshoppers Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    10: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    11: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    12: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    13: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    14: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    15: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    16: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    17: {
+        description: `Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+    18: {
+        description: `Dream Lorem ipsum dolor sit amet, ne eros libris vim. Ignota suscipit vim an, te mundi indoctum sit. Doctus epicuri sed an. Quodsi fabulas quo an, amet patrioque in est, nam eu aliquid deterruisset.`
+    },
+}
