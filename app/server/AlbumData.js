1bf96cf3931f896e1c788bc7deb025913b446bcc
Bennett Lin <bennettl@zillowgroup.com>
2018-08-05 11:48:16 -0700

    Move album to server.

--- app/server/AlbumData.js
+++ app/server/AlbumData.js
@@ -1,6 +1,47 @@
-import RawData from '../album/YearlingsBobtail'
 import { parseAlbumData } from './buildHelpers/albumHelper'
 
-// Restructure raw data to be usable by app.
-const AlbumData = parseAlbumData(RawData)
-export default AlbumData
+import { HYPHENATED_SONG_PATHS } from '../constants/paths'
+import { getTwoDigitStringForNumber } from '../helpers/formatHelper'
+import { getArrayOfLength } from '../helpers/generalHelper'
+
+const rawAlbum = {
+        title: `Yearling's Bobtail`,
+        bookStartingIndices: [1, 10],
+        tips: require('../album/tips/tips'),
+        mp3s: getArrayOfLength(18, 1).map(index => (
+            require(`../assets/mp3s/${
+                getTwoDigitStringForNumber(index)
+            }_${
+                HYPHENATED_SONG_PATHS[index]
+            }.mp3`)
+        )),
+        scores: getArrayOfLength(18, 1).map(index => (
+            require(`../assets/scores/${
+                getTwoDigitStringForNumber(index)
+            }_${
+                HYPHENATED_SONG_PATHS[index]
+            }.pdf`)
+        )),
+        songs: getArrayOfLength(20).map(index => (
+            require(`../album/songs/${
+                getTwoDigitStringForNumber(index)
+            }_${
+                HYPHENATED_SONG_PATHS[index]
+            }`)
+        )),
+        remainingTasks: require('../album/tasks/remainingTasks')
+            .concat(require('../album/tasks/detailedDrawingTasks'))
+            .concat(require('../album/tasks/illustratorDrawingTasks'))
+            .concat(require('../album/tasks/remainingDrawingTasks'))
+            .concat(require('../album/tasks/realisticDrawingTasks'))
+            .concat(require('../album/tasks/sceneTasks'))
+            .concat(require('../album/tasks/svgDrawingTasks')),
+
+        completedTasks: require('../album/tasks/completedTasks')
+            .concat(require('../album/tasks/completedDrawingTasks'))
+    },
+
+    // Restructure raw data to be usable by app.
+    album = parseAlbumData(rawAlbum)
+
+export default album
