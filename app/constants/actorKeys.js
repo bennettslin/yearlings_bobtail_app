56278a18a4e91d49faf053bd34f9aec7d0f66c37
Bennett Lin <bennettl@zillowgroup.com>
2018-08-04 19:07:01 -0700

    Begin presence keys.

---
+++ app/constants/actorKeys.js
@@ -0,0 +1,74 @@
+const
+    YOUNG_BENNETT = 'youngBennett',
+    PRETEEN_BENNETT = 'preteenBennett',
+    BENNETT = 'bennett',
+    OLD_BENNETT = 'oldBennett',
+
+    ANITA = 'anita',
+    ESTHER = 'esther',
+
+    WILLY = 'willy',
+    MOTHER = 'mother',
+    FATHER = 'father',
+    CHRISTOPHER = 'christopher',
+
+    SASHA = 'sasha',
+    BRAD = 'brad',
+    ANDREW = 'andrew',
+
+    CATHERINE = 'catherine',
+
+    MARA = 'mara',
+    JACOB = 'jacob',
+    ANA = 'ana',
+
+    HOWIE = 'howie',
+    TOMER = 'tomer',
+
+    LIZ = 'liz',
+    KHARI = 'khari',
+
+    MIRIAM = 'miriam',
+    TRISTAN = 'tristan',
+    NESTOR = 'nestor',
+
+    AMY = 'amy',
+    STEPHANIE = 'stephanie',
+    WADE = 'wade',
+
+    BENNETT_REFLECTION = 'bennettReflection',
+    LIZ_REFLECTION = 'lizReflection',
+    KHARI_REFLECTION = 'khariReflection'
+
+export {
+    OLD_BENNETT,
+    ANITA,
+    ESTHER,
+    YOUNG_BENNETT,
+    WILLY,
+    MOTHER,
+    CHRISTOPHER,
+    PRETEEN_BENNETT,
+    FATHER,
+    HOWIE,
+    TOMER,
+    SASHA,
+    BRAD,
+    ANDREW,
+    TRISTAN,
+    BENNETT,
+    CATHERINE,
+    WADE,
+    AMY,
+    STEPHANIE,
+    JACOB,
+    MARA,
+    ANA,
+    MIRIAM,
+    LIZ,
+    KHARI,
+    NESTOR,
+    BENNETT_REFLECTION,
+    LIZ_REFLECTION,
+    KHARI_REFLECTION
+}
