eebe567be973eb7ff170f8b18d6af3dbeaa42ae5
Bennett Lin <bennettslin@gmail.com>
2018-09-11 20:52:02 -0700

    Add cutout components.

---
+++ app/components/Presences/Cutout/Cutouts/OaklandHouses/Volkswagen.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+
+import Svg from 'components/Svg/Svg'
+
+const propTypes = {
+    // From parent.
+    className: PropTypes.any
+}
+
+const Volkswagen = ({
+
+    className,
+
+...other }) => {
+
+    return (
+        <Svg
+            className={cx(
+                'Volkswagen',
+                className
+            )}
+        >
+            <rect
+                className={cx(
+                    'Cutout__temporaryRect'
+                )}
+                {...other}
+            />
+            <text
+                className={cx(
+                    'Presence__temporaryText'
+                )}
+                {...other}
+            >
+                {cx(
+                    'Volkswagen'
+                )}
+            </text>
+        </Svg>
+    )
+}
+
+Volkswagen.propTypes = propTypes;
+
+export default Volkswagen
---
+++ app/components/Presences/Cutout/Cutouts/OutdoorWedding/WeddingCrowd.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+
+import Svg from 'components/Svg/Svg'
+
+const propTypes = {
+    // From parent.
+    className: PropTypes.any
+}
+
+const WeddingCrowd = ({
+
+    className,
+
+...other }) => {
+
+    return (
+        <Svg
+            className={cx(
+                'WeddingCrowd',
+                className
+            )}
+        >
+            <rect
+                className={cx(
+                    'Cutout__temporaryRect'
+                )}
+                {...other}
+            />
+            <text
+                className={cx(
+                    'Presence__temporaryText'
+                )}
+                {...other}
+            >
+                {cx(
+                    'WeddingCrowd'
+                )}
+            </text>
+        </Svg>
+    )
+}
+
+WeddingCrowd.propTypes = propTypes;
+
+export default WeddingCrowd
