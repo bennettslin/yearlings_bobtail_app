da0e621a8bcad2abc854c0c3890e4f46eaf2c65a
Bennett Lin <bennettl@zillowgroup.com>
2018-11-04 21:01:40 -0800

    Delete redundant tasks.

--- app/album/tasks/detailedDrawingTasks.js
+++ app/album/tasks/detailedDrawingTasks.js
@@ -1,15 +1,5 @@
 module.exports = [
     {
-        taskName: `Johnny Horton album`,
-        workedHours: 0,
-        neededHours: 3
-    },
-    {
-        taskName: `Bobtail Yearlings album`,
-        workedHours: 0,
-        neededHours: 3
-    },
-    {
         taskName: `Yearlings logo on kick drum (M, Garryowen)`,
         workedHours: 0,
         neededHours: 1
