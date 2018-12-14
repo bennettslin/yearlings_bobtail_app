b323c6f5bd9766cd2b0eb5266eb57570af3a6cba
Bennett Lin <bennettl@zillowgroup.com>
2018-12-14 08:42:15 -0800

    Lint and prettier changes.

--- app/handlers/ScoreHandler/Dispatcher/index.jsx
+++ app/handlers/ScoreHandler/Dispatcher/index.jsx
@@ -3,7 +3,6 @@
 import { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import { updateToggleStore } from 'flux/toggle/action'
 
 class ScoreDispatcher extends PureComponent {
@@ -59,10 +58,7 @@ const mapStateToProps = ({
     isSelectedLogue
 })
 
-const bindDispatchToProps = (dispatch) => (
-    bindActionCreators({
-        updateToggleStore
-    }, dispatch)
-)
-
-export default connect(mapStateToProps, bindDispatchToProps)(ScoreDispatcher)
+export default connect(
+    mapStateToProps,
+    { updateToggleStore }
+)(ScoreDispatcher)
