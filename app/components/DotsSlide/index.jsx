9b80ce75a243918efde0b856a780c2b25fb83485
Bennett Lin <bennettl@zillowgroup.com>
2019-01-07 07:45:40 -0800

    Dots slide mounts upon transition in.

--- app/components/DotsSlide/index.jsx
+++ app/components/DotsSlide/index.jsx
@@ -2,10 +2,10 @@
 
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import cx from 'classnames'
 import { connect } from 'react-redux'
 import { resetInteractivatedDots } from 'flux/dotsSlide/action'
 
+import CSSTransition from 'react-transition-group/CSSTransition'
 import StopPropagationDispatcher from '../../dispatchers/StopPropagation'
 import DotsSlideSelect from './Select'
 
@@ -16,12 +16,16 @@ import { DOT_KEYS_ARRAY_CONFIGS } from './constants'
 
 const mapStateToProps = ({
     accessStore: { accessedDotIndex },
-    toggleStore: { isAccessOn },
+    toggleStore: {
+        isAccessOn,
+        isDotsSlideShown
+    },
     dotsStore: { dotsBitNumber },
     dotsSlideStore: { dotsSlideBitNumber }
 }) => ({
     accessedDotIndex,
     isAccessOn,
+    isDotsSlideShown,
     dotsBitNumber,
     dotsSlideBitNumber
 })
@@ -31,6 +35,7 @@ class DotsSlide extends PureComponent {
     static propTypes = {
     // Through Redux.
         isAccessOn: PropTypes.bool.isRequired,
+        isDotsSlideShown: PropTypes.bool.isRequired,
         accessedDotIndex: PropTypes.number,
         dotsBitNumber: PropTypes.number.isRequired,
         dotsSlideBitNumber: PropTypes.number.isRequired,
@@ -42,6 +47,16 @@ class DotsSlide extends PureComponent {
         this.props.resetInteractivatedDots()
     }
 
+    getSelectedDotKeys() {
+        const { dotsBitNumber } = this.props
+        return getDotKeysFromBitNumber(dotsBitNumber)
+    }
+
+    getInteractivatedDotKeys() {
+        const { dotsSlideBitNumber } = this.props
+        return getDotKeysFromBitNumber(dotsSlideBitNumber)
+    }
+
     _getRefs = (payload) => {
         populateRefs(this, payload)
     }
@@ -49,65 +64,74 @@ class DotsSlide extends PureComponent {
     render() {
         const {
                 isAccessOn,
-                accessedDotIndex,
-                dotsBitNumber,
-                dotsSlideBitNumber
+                isDotsSlideShown,
+                accessedDotIndex
             } = this.props,
 
-            selectedDotKeys = getDotKeysFromBitNumber(dotsBitNumber),
-            interactivatedDotKeys = getDotKeysFromBitNumber(dotsSlideBitNumber)
+            selectedDotKeys = this.getSelectedDotKeys(),
+            interactivatedDotKeys = this.getInteractivatedDotKeys()
 
         return (
-            <div className="DotsSlide">
-                <div
-                    {...{
-                        className: cx(
-                            'DotsSlideSelects'
-                        ),
-                        onClick: this._handleContainerClick,
-                        onTouchStart: this._handleContainerClick
-                    }}
-                >
-                    {DOT_KEYS_ARRAY_CONFIGS.map((arrayConfig, arrayIndex) => {
-                        const {
-                            dotKeysArray,
-                            startingIndex
-                        } = arrayConfig
+            <CSSTransition
+                mountOnEnter
+                {...{
+                    in: isDotsSlideShown,
+                    timeout: 200,
+                    classNames: {
+                        enterActive: 'DotsSlide__shown',
+                        enterDone: 'DotsSlide__shown'
+                    }
+                }}
+            >
+                <div className="DotsSlide">
+                    <div
+                        {...{
+                            className: 'DotsSlideSelects',
+                            onClick: this._handleContainerClick,
+                            onTouchStart: this._handleContainerClick
+                        }}
+                    >
+                        {DOT_KEYS_ARRAY_CONFIGS.map((arrayConfig, arrayIndex) => {
+                            const {
+                                dotKeysArray,
+                                startingIndex
+                            } = arrayConfig
 
-                        return (
-                            <div
-                                key={arrayIndex}
-                                className="DotsSlideSelects__row"
-                            >
-                                {dotKeysArray.map((dotKey, index) => {
-                                    const
-                                        dotIndex = index + startingIndex,
-                                        isAccessed =
-                                            isAccessOn &&
-                                            accessedDotIndex === dotIndex,
-                                        isSelected = selectedDotKeys[dotKey],
-                                        isInteractivated =
-                                            interactivatedDotKeys[dotKey]
+                            return (
+                                <div
+                                    key={arrayIndex}
+                                    className="DotsSlideSelects__row"
+                                >
+                                    {dotKeysArray.map((dotKey, index) => {
+                                        const
+                                            dotIndex = index + startingIndex,
+                                            isAccessed =
+                                                isAccessOn &&
+                                                accessedDotIndex === dotIndex,
+                                            isSelected = selectedDotKeys[dotKey],
+                                            isInteractivated =
+                                                interactivatedDotKeys[dotKey]
 
-                                    return (
-                                        <DotsSlideSelect
-                                            key={index}
-                                            {...{
-                                                dotIndex,
-                                                dotKey,
-                                                isAccessed,
-                                                isSelected,
-                                                isInteractivated
-                                            }}
-                                        />
-                                    )
-                                })}
-                            </div>
-                        )
-                    })}
+                                        return (
+                                            <DotsSlideSelect
+                                                key={index}
+                                                {...{
+                                                    dotIndex,
+                                                    dotKey,
+                                                    isAccessed,
+                                                    isSelected,
+                                                    isInteractivated
+                                                }}
+                                            />
+                                        )
+                                    })}
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <StopPropagationDispatcher {...{ getRefs: this._getRefs }} />
                 </div>
-                <StopPropagationDispatcher {...{ getRefs: this._getRefs }} />
-            </div>
+            </CSSTransition>
         )
     }
 }
