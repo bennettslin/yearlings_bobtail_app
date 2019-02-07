44d2685ff266be71e9ccce3354dd7cce4b98ffc4
Bennett Lin <bennettslin@gmail.com>
2019-02-06 22:52:11 -0800

    Add bug.

--- app/album/tasks/remaining/bugs/index.js
+++ app/album/tasks/remaining/bugs/index.js
@@ -3,5 +3,10 @@ export default [
         taskName: `Weird bug where accessing between shiv wormholes will sometimes have access on left shiv, but be selected on right. Repros only very infrequently, so probably low priority.`,
         workedHours: 0,
         neededHours: 1
+    },
+    {
+        taskName: `Bug where upon selecting new song, neither lyric nor carousel have gradient? Only seen in home laptop so far.`,
+        workedHours: 0,
+        neededHours: 1
     }
 ]
