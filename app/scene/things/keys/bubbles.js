018d71bb5049eb3721f7ffcee64b3207ccafbcd4
Bennett Lin <bennettl@zillowgroup.com>
2019-06-18 08:28:40 -0700

    Minor changes.

--- app/scene/things/keys/bubbles.js
+++ app/scene/things/keys/bubbles.js
@@ -5,14 +5,8 @@ export const ESCAPE_POD_SKETCHBOOK = 'escapePodSketchbook'
 export const CINEMA_THOUGHTS = 'cinemaThoughts'
 export const GO_KART_THOUGHT = 'goKartThought'
 export const WAITING_ROOM_THOUGHTS = 'waitingRoomThoughts'
-
-// TODO
 export const TARPIT_THOUGHT = 'tarpitThought'
-// TODO
 export const SIBLING_THOUGHT = 'siblingThought'
-// TODO
 export const KHARI_ATTACK_SPEECH = 'khariAttackSpeech'
-// TODO
 export const LIZ_DEFENCE_SPEECH = 'lizDefenceSpeech'
-// TODO
 export const LIZ_THOUGHT = 'lizThought'
