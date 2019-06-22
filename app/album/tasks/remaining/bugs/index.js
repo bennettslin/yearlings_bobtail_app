4bbb6dd474c3bd4c8afbf139476a9cce1781cb7e
Bennett Lin <bennettl@zillowgroup.com>
2019-06-22 13:57:12 -0700

    Add a bug task.

--- app/album/tasks/remaining/bugs/index.js
+++ app/album/tasks/remaining/bugs/index.js
@@ -3,5 +3,10 @@ export default [
         taskName: `If annotation in URL requires a selected dot, force select that dot. Use dot bit number.`,
         workedHours: 0,
         neededHours: 3
+    },
+    {
+        taskName: `Clicking on Filmstrip dismisses overlay. Ensure that clicking on Banner also does not dismiss overlay`,
+        workedHours: 0,
+        neededHours: 2
     }
 ]
