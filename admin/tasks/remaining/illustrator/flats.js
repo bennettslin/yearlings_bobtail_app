09e4360a0796ca02440ca0ff6be32d61fa261efb
Bennett Lin <bennettslin@gmail.com>
2019-07-23 22:50:08 -0700

    Minor change.

--- admin/tasks/remaining/illustrator/flats.js
+++ admin/tasks/remaining/illustrator/flats.js
@@ -30,16 +30,6 @@ export default [
         neededHours: 1.5
     },
     {
-        taskName: 'taiwanClouds',
-        workedHours: 0,
-        neededHours: 1.5
-    },
-    {
-        taskName: 'taiwanFog',
-        workedHours: 0,
-        neededHours: 1.5
-    },
-    {
         taskName: 'taiwanMountainsBridged',
         workedHours: 0,
         neededHours: 1.5
--- admin/tasks/remaining/illustrator/panels.js
+++ admin/tasks/remaining/illustrator/panels.js
@@ -5,6 +5,16 @@ export default [
         neededHours: 1.5
     },
     {
+        taskName: 'taiwanClouds',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'taiwanFog',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
         taskName: 'deathbedThoughtPanel',
         workedHours: 0,
         neededHours: 1
