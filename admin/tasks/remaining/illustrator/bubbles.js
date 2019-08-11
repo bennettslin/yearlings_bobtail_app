97c7417662cd261b369f1fbe057857aada39647d
Bennett Lin <bennettslin@gmail.com>
2019-08-10 23:37:57 -0700

    Revise bubble hours.

--- admin/tasks/remaining/illustrator/bubbles.js
+++ admin/tasks/remaining/illustrator/bubbles.js
@@ -2,41 +2,41 @@ export default [
     {
         taskName: 'togetherSketch',
         workedHours: 0,
-        neededHours: 1.5
+        neededHours: 2
     },
     {
         taskName: 'apartSketch',
         workedHours: 0,
-        neededHours: 1.5
+        neededHours: 2
     },
     {
         taskName: 'cinemaThoughtRecording',
         workedHours: 0,
-        neededHours: 2
+        neededHours: 2.5
     },
     {
         taskName: 'cinemaThoughtBuying',
         workedHours: 0,
-        neededHours: 1.5
+        neededHours: 2
     },
     {
         taskName: 'waitingThoughtStudying',
         workedHours: 0,
-        neededHours: 1.5
+        neededHours: 2
     },
     {
         taskName: 'khariAttackSpeech',
         workedHours: 0,
-        neededHours: 1.5
+        neededHours: 2
     },
     {
         taskName: 'lizDefenceSpeech',
         workedHours: 0,
-        neededHours: 1.5
+        neededHours: 2
     },
     {
         taskName: 'lizThought',
         workedHours: 0,
-        neededHours: 1.5
+        neededHours: 2
     }
 ]
