c1692bfeffa260acad484a10f247f589b2b91144
Bennett Lin <bennettslin@gmail.com>
2019-08-19 21:28:41 -0700

    Organise fixture tasks.

--- admin/tasks/remaining/illustrator/fixtures.js
+++ admin/tasks/remaining/illustrator/fixtures.js
@@ -1,204 +1,5 @@
 export default [
-    {
-        taskName: 'safewayBag',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'recordPlayerStool',
-        workedHours: 0,
-        neededHours: 2.5
-    },
-    {
-        taskName: 'shoppingCartFull',
-        workedHours: 0,
-        neededHours: 3
-    },
-    {
-        taskName: 'beethovenBust',
-        workedHours: 0,
-        neededHours: 1.5
-    },
-    {
-        taskName: 'christopherLetters',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'backpackBennett',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'fridgeDrawings',
-        workedHours: 0,
-        neededHours: 1.5
-    },
-    {
-        taskName: 'bennettLetters',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'redEnvelopeMoney',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'beyonderEnvelope',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'thrownFruit',
-        workedHours: 0,
-        neededHours: 1.5
-    },
-    {
-        taskName: 'bennettRecordPlayer',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'keyboard',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'cashBox',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'drumKit',
-        workedHours: 0,
-        neededHours: 4
-    },
-    {
-        taskName: 'bassCaseUpright',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'guitarCaseUpright',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'basementBeerCase',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'basementBeerMara',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'basementBeerBennett',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'drumsStacked',
-        workedHours: 0,
-        neededHours: 3
-    },
-    {
-        taskName: 'cavemanBones',
-        workedHours: 0,
-        neededHours: 3
-    },
-    {
-        taskName: 'nudeDrawings',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'vaseline',
-        workedHours: 0,
-        neededHours: 1.5
-    },
-    {
-        taskName: 'bloodyTowel',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'bloodyTowelReflection',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'stageMicSide',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'guitar',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'guitarCaseOpen',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'kickDrum',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'drumThrone',
-        workedHours: 0,
-        neededHours: 1.5
-    },
-    {
-        taskName: 'bennettShoe',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'bennettShoeReflection',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'tchotchkes',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'recordingBeers',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'recordingMic',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'recordingConsole',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'crackedPicture',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'hospitalRecordPlayer',
-        workedHours: 0,
-        neededHours: 2
-    },
-    {
-        taskName: 'burgerWrapper',
-        workedHours: 0,
-        neededHours: 1
-    },
+    // Easy.
     {
         taskName: 'lizPhone',
         workedHours: 0,
@@ -218,5 +19,223 @@ export default [
         taskName: 'lizPhoneBaseReflection',
         workedHours: 0,
         neededHours: 1
+    },
+
+    // Paper.
+    {
+        taskName: 'bennettLetters',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'christopherLetters',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'safewayBag',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'redEnvelopeMoney',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'beyonderEnvelope',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'cashBox',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'tchotchkes',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Drawings.
+    {
+        taskName: 'fridgeDrawings',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'nudeDrawings',
+        workedHours: 0,
+        neededHours: 2
+    },
+
+    // Big.
+    {
+        taskName: 'shoppingCartFull',
+        workedHours: 0,
+        neededHours: 3
+    },
+    {
+        taskName: 'cavemanBones',
+        workedHours: 0,
+        neededHours: 3
+    },
+
+    // Beers.
+    {
+        taskName: 'basementBeerCase',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'basementBeerMara',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'basementBeerBennett',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'recordingBeers',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Crumpled edges.
+    {
+        taskName: 'vaseline',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'thrownFruit',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'burgerWrapper',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Cloth.
+    {
+        taskName: 'backpackBennett',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'bloodyTowel',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'bloodyTowelReflection',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'bennettShoe',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'bennettShoeReflection',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Straight edges.
+    {
+        taskName: 'crackedPicture',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'bennettRecordPlayer',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'recordPlayerStool',
+        workedHours: 0,
+        neededHours: 2.5
+    },
+    {
+        taskName: 'hospitalRecordPlayer',
+        workedHours: 0,
+        neededHours: 2
+    },
+
+    // Instruments.
+    {
+        taskName: 'stageMicSide',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'recordingMic',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'recordingConsole',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'keyboard',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'bassCaseUpright',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'guitar',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'guitarCaseUpright',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'guitarCaseOpen',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'drumThrone',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'kickDrum',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'drumsStacked',
+        workedHours: 0,
+        neededHours: 3
+    },
+    {
+        taskName: 'drumKit',
+        workedHours: 0,
+        neededHours: 4
+    },
+
+    // People.
+    {
+        taskName: 'beethovenBust',
+        workedHours: 0,
+        neededHours: 1.5
     }
 ]
