29de7d97913b8d972fe1f182b5c89d8c07db2e54
Bennett Lin <bennettslin@gmail.com>
2019-08-27 07:43:55 -0700

    Log Mara face.

--- admin/scenes/11_LookBackInAnger.js
+++ admin/scenes/11_LookBackInAnger.js
@@ -90,7 +90,7 @@ export default [
                 },
                 [MARA]: {
                     todo: true,
-                    workedHours: 3.75,
+                    workedHours: 4.25,
                     instance: CARRYING
                 }
             }
