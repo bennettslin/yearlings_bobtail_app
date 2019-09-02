b0ec9a78f5f9cfe8a7e550b46b7ade5a028a8320
Bennett Lin <bennettslin@gmail.com>
2019-09-01 23:19:04 -0700

    Add actor props to fixture tasks.

--- admin/tasks/remaining/illustrator/fixtures.js
+++ admin/tasks/remaining/illustrator/fixtures.js
@@ -37,7 +37,7 @@ export default [
     {
         taskName: 'violin',
         workedHours: 0,
-        neededHours: 2
+        neededHours: 2.5
     },
     {
         taskName: 'mandolin',
@@ -63,5 +63,245 @@ export default [
         taskName: 'drumKit',
         workedHours: 0,
         neededHours: 4
+    },
+
+    {
+        taskName: 'Hi-hat stand',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Actor props.
+
+    // Food and containers.
+    {
+        taskName: 'Remaining beers * 8 (Amy, Bennett, Mara, Stephanie, Shane, Wade * 2, Willy)',
+        workedHours: 0,
+        neededHours: 3
+    },
+    {
+        taskName: 'Evian bottles.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Andrew fruit.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Ana beer in bag.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Tomer bottle',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Brad chips',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Basement food.',
+        workedHours: 0,
+        neededHours: 2.5
+    },
+    {
+        taskName: 'Robitussin.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Dropped meats',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Chinese food * 4',
+        workedHours: 0,
+        neededHours: 2.5
+    },
+    {
+        taskName: 'Demonstration cupcakes.',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Wearables.
+    {
+        taskName: 'Backpacks * 11 (Anita * 2, Catherine, Esther * 2, Liz * 2, Wade, young Bennett * 3)',
+        workedHours: 0,
+        neededHours: 5
+    },
+    {
+        taskName: 'Bennett messenger bag * 6',
+        workedHours: 0,
+        neededHours: 3
+    },
+    {
+        taskName: 'Liz purse * 3',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'Catherine purse',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Amy bag.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Walkman and headphones',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'Baby',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'Police horse.',
+        workedHours: 0,
+        neededHours: 3
+    },
+    {
+        taskName: 'Khari hat.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Flapper hat.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Golden bracelet.',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Props.
+    {
+        taskName: 'Steam tunnel weapons.',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'Bennett phone.',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'Book or magazine * 7 (Bennett * 2, Jewish boy, Catherine * 2, preteen Bennett * 2)',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'Tray and lancet pen * 2',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'Steering wheel * 4',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'Rubik cube',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Tennis racket',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'X-ray specs',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Taiwan pillow',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Bennett condom.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Bennett light switch.',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Miriam perfume.',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Instruments.
+    {
+        taskName: 'Guitar cable',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'Tenor recorder',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'Drumsticks * 2 (Jacob, Nestor)',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Print.
+    {
+        taskName: 'Vegan signs.',
+        workedHours: 0,
+        neededHours: 1.5
+    },
+    {
+        taskName: 'Liz tchotchkes',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Horseshoe album',
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'Stop sign',
+        workedHours: 0,
+        neededHours: 1
+    },
+
+    // Large.
+    {
+        taskName: 'Buoy',
+        workedHours: 0,
+        neededHours: 2
+    },
+    {
+        taskName: 'Rickshaw',
+        workedHours: 0,
+        neededHours: 3
+    },
+    {
+        taskName: 'Tomer garbage bag',
+        workedHours: 0,
+        neededHours: 2.5
     }
 ]
