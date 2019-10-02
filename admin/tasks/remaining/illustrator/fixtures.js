a1ede1166ddc2baf1428839813c74202a082a71f
Bennett Lin <bennettslin@gmail.com>
2019-10-01 23:07:50 -0700

    Add notes to scene tasks.

--- admin/tasks/remaining/illustrator/fixtures.js
+++ admin/tasks/remaining/illustrator/fixtures.js
@@ -4,5 +4,11 @@ export default [
         taskName: 'Khari hat.',
         workedHours: 0,
         neededHours: 1
+    },
+
+    {
+        taskName: 'Pillows * 12 (preteen Bennett * 3, Bennett * 3, Liz * 4, Miriam * 2).',
+        workedHours: 0,
+        neededHours: 6
     }
 ]
--- admin/tasks/remaining/scene/index.js
+++ admin/tasks/remaining/scene/index.js
@@ -17,324 +17,385 @@
 export default [
     {
         taskName: 'theatre',
+        notes: `logue versus song theatre colours, gradient for sky backdrop`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'logue',
+        notes: `cube colours`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'nohoHouses',
+        notes: `Fire hydrant colour less bright.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'nohoSchool',
+        notes: `Raise bubble floor, ball more rubber colour, bubble wall same as Van Nuys hallway colour.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'bathroom',
-        todo: 'expand wall',
+        notes: `expand wall, rechoose towel and sink colours, make magic scene sepia.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'freeway',
+        notes: `Make shopping cart a fixture.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'porch',
+        notes: `Include NoHo backdrop, plant colour.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'bedroom',
-        todo: 'fix bed wonkiness',
+        notes: `Fix bed wonkiness by making mattress part of panel, skewing more closely, accommodate cube seat, colour cubes without bed.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'changelingCave',
+        notes: `Fire colour, shadow colours, add Noho backdrop with night colours, Noho house with night colours and more even size.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'puppetShow',
+        notes: `Mountain colours, lower sketchbook.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'schoolbus',
+        notes: `Bus seats on platforms.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'tetherballCourt',
+        notes: `Just one bungalow?`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'taiwanRoom',
-        todo: 'expand door',
+        notes: `Expand doorway, make Buddha gold colour.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'taiwanStairs',
+        notes: `Night colours, make Bennett cutout shorter.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'taiwanGate',
+        notes: `Night colours`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'rickshaw',
+        notes: `Night colours`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'shore',
+        notes: `Night colours, lower backdrop, move grill, make buoy smaller.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'buoy',
+        notes: `Make all cubes coloured, colour sea serpents.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'oceanDepths',
+        notes: `Ocean colours.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'oceanFloor',
+        notes: `Ocean colours.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'vanNuysLockers',
+        notes: `Make ground lawn.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'vanNuysQuad',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'vanNuysHallway',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'catholicChurch',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'ucBerkeley',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'cinema',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'cinemaStreet',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'basement',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'museum',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'backstage',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'goKarts',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'dishroom',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'waitingRoom',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'clubFront',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'bennettBed',
-        workedHours: 0,
-        neededHours: 1
-    },
-    {
-        taskName: 'bennettCouch',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'courtyard',
+        notes: `Door size, no gap for gate, backdrop.`,
         workedHours: 0,
         neededHours: 1
     },
     {
-        taskName: 'bennettGate',
+        taskName: 'bennettCouch',
+        notes: `Police and Television album skew, expand room.`,
         workedHours: 0,
         neededHours: 1
     },
     {
-        taskName: 'lizBed',
+        taskName: 'catholicChurch',
+        notes: `Night colours, lower backdrop.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'ucBerkeley',
+        notes: `Lower backdrop, set back Bancroft building, Bancroft shrub colour.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'clubFront',
+        notes: `Expand room, rethink time allotted in M.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'basement',
+        notes: `Lower backdrop, raise ceiling, more steps.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'cinema',
+        notes: ``,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'cinemaStreet',
+        notes: `Lower backdrop, allow flat to go behind building.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'lizGate',
+        notes: `More plants, expand window presence, add backdrops, make same in Grasshoppers.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'museum',
+        notes: ``,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'backstage',
+        notes: `Resize sandbags and stage lights.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'goKarts',
+        notes: `Lower backdrop.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'dishroom',
+        notes: ``,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'bennettGate',
+        notes: `Make gate wider, make roof the same thickness.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'bennettBed',
+        notes: `Make door correct height, add mattress to bed panel, expand room, darken neighbour door and window.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'waitingRoom',
+        notes: `All colours.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: 'lizBed',
+        notes: `Add mattress to bed panel, expand room.`,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'clubSide',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'clubLoading',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'lizCouch',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'royceHall',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'walgreens',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'steamTunnel',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'cemetery',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'stationWagon',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'attic',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'oaklandHouses',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'oaklandResidential',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'oaklandCommercial',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'oaklandPublic',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'snowglobesPast',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'snowglobesPresent',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'snowglobesFuture',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'elCerrito',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'clubFar',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'alley',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'elTorito',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'pampas',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'wedding',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'cliff',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     },
     {
         taskName: 'deathbed',
+        notes: ``,
         workedHours: 0,
         neededHours: 1
     }
