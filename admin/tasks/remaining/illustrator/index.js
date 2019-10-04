7420cd156ea63176e57378aadaa8cc432a71a59c
Bennett Lin <bennettslin@gmail.com>
2019-10-03 22:40:26 -0700

    Remove problem tasks.

--- admin/tasks/remaining/illustrator/index.js
+++ admin/tasks/remaining/illustrator/index.js
@@ -1,18 +1,15 @@
 import app from './app'
 import doors from './doors'
 import fixtures from './fixtures'
-import problem from './problem'
 
 export const illustratorRemainingTasks = {
     app,
     doors,
-    fixtures,
-    problem
+    fixtures
 }
 
 export default [
     ...app,
     ...doors,
-    ...fixtures,
-    ...problem
+    ...fixtures
 ]
