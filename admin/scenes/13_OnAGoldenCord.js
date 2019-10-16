a3b80ac2500ac02f891170730b6bd1ad6d9e49a5
Bennett Lin <bennettslin@gmail.com>
2019-10-16 05:40:32 -0700

    Finish Bennett jeans seated.

--- admin/scenes/13_OnAGoldenCord.js
+++ admin/scenes/13_OnAGoldenCord.js
@@ -95,7 +95,6 @@ export default [
         presences: {
             [ACTOR]: {
                 [BENNETT]: {
-                    legs: true,
                     trunk: true,
                     hands: true,
                     head: true,
