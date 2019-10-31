639caaf886638826e1090d92b21969ab3baae749
Bennett Lin <bennettslin@gmail.com>
2019-10-30 21:14:50 -0700

    Add some tasks.

--- admin/tasks/remaining/svg/index.js
+++ admin/tasks/remaining/svg/index.js
@@ -11,5 +11,20 @@ export default [
         taskName: `dot icons, shapes, and dots slide button, including wormhole.`,
         workedHours: 0,
         neededHours: 8
+    },
+    {
+        taskName: `Redo lyric expand and collapse`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: `Drop shadow on tooltips`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: `Border on favicon`,
+        workedHours: 0,
+        neededHours: 1
     }
 ]
