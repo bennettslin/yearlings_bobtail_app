19d9c133473663d71c74e10bd837e521f6327a01
Bennett Lin <bennettl@zillowgroup.com>
2020-02-19 08:22:46 -0800

    Add task.

--- admin/tasks/remaining/important/index.js
+++ admin/tasks/remaining/important/index.js
@@ -13,5 +13,15 @@ export default [
         taskName: `Confirm that GA works for both development and production, and prevent hacking. https://neilpatel.com/blog/protect-analytics-from-hacking/`,
         workedHours: 0,
         neededHours: 1
+    },
+    {
+        taskName: `Confirm that GA works for both development and production, and prevent hacking. https://neilpatel.com/blog/protect-analytics-from-hacking/`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: `Upgrade to Babel 7, and change to empty tags for fragments.`,
+        workedHours: 0,
+        neededHours: 1
     }
 ]
