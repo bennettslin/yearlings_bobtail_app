2d0a2ba2e43dc686e259690f55093bcbb8ccd97e
Bennett Lin <bennettslin@gmail.com>
2020-04-03 00:23:34 -0700

    Prepare tomorrow's tasks.

--- admin/tasks/remaining/next/index.js
+++ admin/tasks/remaining/next/index.js
@@ -1,5 +1,20 @@
 export default [
     {
+        taskName: `Make Liz gate outside open.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: `Give couch panels seat cushions.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
+        taskName: `Don't show tips hands in phone width.`,
+        workedHours: 0,
+        neededHours: 1
+    },
+    {
         taskName: `Position tips hand for each song. Don't show in phone width.`,
         workedHours: 5,
         neededHours: 8
