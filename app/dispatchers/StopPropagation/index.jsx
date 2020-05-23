588eac6b18ec5be8b9f4ddca46ce8217e5575a7a
Bennett Lin <bennettslin@gmail.com>
2020-05-23 10:40:51 -0700

    Make stop propagation dispatcher a function.

--- app/dispatchers/StopPropagation/index.jsx
+++ app/dispatchers/StopPropagation/index.jsx
@@ -1,54 +1,40 @@
 // Child that knows rules to stop propagation.
 
-import { PureComponent } from 'react'
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { updateFocusStore } from '../../redux/focus/action'
 import { updateAccessStore } from '../../redux/access/action'
 
-class StopPropagationDispatcher extends PureComponent {
+const StopPropagationDispatcher = ({ getRefs }) => {
+    const dispatch = useDispatch()
 
-    static propTypes = {
-        // Through Redux.
-        updateFocusStore: PropTypes.func.isRequired,
-        updateAccessStore: PropTypes.func.isRequired,
+    useEffect(() => {
+        getRefs({
+            dispatchStopPropagation(e) {
+                if (e) {
+                    const { type } = e
 
-        // From parent.
-        getRefs: PropTypes.func.isRequired
-    }
+                    // Turn access off if not from a keyboard event.
+                    if (type === 'click' || type === 'mousedown') {
+                        dispatch(updateAccessStore({ isAccessOn: false }))
+                    }
 
-    componentDidMount() {
-        this.props.getRefs({
-            dispatchStopPropagation: this.dispatchStopPropagation
+                    if (e.stopPropagation) {
+                        e.stopPropagation()
+
+                        dispatch(updateFocusStore({ queuedFocus: true }))
+                    }
+                }
+            }
         })
-    }
+    }, [])
 
-    dispatchStopPropagation = (e) => {
-        if (e) {
-            const { type } = e
-
-            // Turn access off if not from a keyboard event.
-            if (type === 'click' || type === 'mousedown') {
-                this.props.updateAccessStore({ isAccessOn: false })
-            }
-
-            if (e.stopPropagation) {
-                e.stopPropagation()
-
-                this.props.updateFocusStore({ queuedFocus: true })
-            }
-        }
-    }
-
-    render() {
-        return null
-    }
+    return null
 }
 
-export default connect(
-    null,
-    {
-        updateFocusStore,
-        updateAccessStore
-    }
-)(StopPropagationDispatcher)
+StopPropagationDispatcher.propTypes = {
+    getRefs: PropTypes.func.isRequired
+}
+
+export default StopPropagationDispatcher
