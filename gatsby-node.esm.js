365b2231f9308c75fbf640d8b9df56ede7d6c069
Bennett Lin <bennettslin@gmail.com>
2020-05-24 14:43:51 -0700

    Comment everything out.

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -23,140 +23,140 @@ export const onCreateWebpackConfig = ({
 
 } = {}) => {
     actions.setWebpackConfig({
-        entry: path.resolve(__dirname, 'app'),
-        output: {
-            path: isDeliveryEnvironment ?
-                path.resolve(__dirname, 'build__delivery') :
-                path.resolve(__dirname, 'build'),
-            filename: '[name]-[hash].js',
-            ...isLocalDevelopment && {
-                devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]'
-            }
-        },
-        plugins: [
-            // Define global constant at compile time.
-            new webpack.DefinePlugin({
-                // Grab album from global env only when not running locally.
-                ...!isLocalDevelopment && {
-                    ALBUM: JSON.stringify(album)
-                },
-                BUILD_DATE_TIME: JSON.stringify(
-                    `${format(new Date(), 'MMMM d, yyyy, h:mmaaaaa')}m`
-                ),
-                IS_DELIVERY: isDeliveryEnvironment,
-                IS_LOCAL: isLocalDevelopment
-            }),
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, 'app/index.html')
-            }),
-            ...isLocalDevelopment ? [
-                new webpack.HotModuleReplacementPlugin(),
+        // entry: path.resolve(__dirname, 'src'),
+        // output: {
+        //     path: isDeliveryEnvironment ?
+        //         path.resolve(__dirname, 'build__delivery') :
+        //         path.resolve(__dirname, 'build'),
+        //     filename: '[name]-[hash].js',
+        //     ...isLocalDevelopment && {
+        //         devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]'
+        //     }
+        // },
+        // plugins: [
+        //     // Define global constant at compile time.
+        //     new webpack.DefinePlugin({
+        //         // Grab album from global env only when not running locally.
+        //         ...!isLocalDevelopment && {
+        //             ALBUM: JSON.stringify(album)
+        //         },
+        //         BUILD_DATE_TIME: JSON.stringify(
+        //             `${format(new Date(), 'MMMM d, yyyy, h:mmaaaaa')}m`
+        //         ),
+        //         IS_DELIVERY: isDeliveryEnvironment,
+        //         IS_LOCAL: isLocalDevelopment
+        //     }),
+        //     new HtmlWebpackPlugin({
+        //         template: path.resolve(__dirname, 'src/index.html')
+        //     }),
+        //     ...isLocalDevelopment ? [
+        //         new webpack.HotModuleReplacementPlugin(),
 
-                ...SHOW_BUNDLE_ANALYZER ? [new BundleAnalyzerPlugin()] : []
-            ] : [
-                new webpack.optimize.OccurrenceOrderPlugin(),
+        //         ...SHOW_BUNDLE_ANALYZER ? [new BundleAnalyzerPlugin()] : []
+        //     ] : [
+        //         new webpack.optimize.OccurrenceOrderPlugin(),
 
-                // Remove the previous build.
-                new CleanWebpackPlugin()
-            ]
-        ],
-        resolve: {
-            // Import from files without specifying extensions.
-            extensions: ['.js', '.jsx', '.mp3', '.pdf', '.scss', '.svg'],
-            alias: {
-                // Allow admin routes only in delivery.
-                routes: isDeliveryEnvironment ?
-                    path.resolve(__dirname, './admin/routes') :
-                    path.resolve(__dirname, './app/routes'),
-                // Grab data from admin folder in local development.
-                data: isLocalDevelopment ?
-                    path.resolve(__dirname, './admin/data') :
-                    path.resolve(__dirname, './app/data')
-            }
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.jsx?$/,
-                    include: [
-                        path.resolve(__dirname, './app'),
-                        ...isDeliveryEnvironment ? [path.resolve(__dirname, './admin')] : []
-                    ],
-                    enforce: 'pre',
-                    loaders: [
-                        'babel-loader',
-                        'eslint-loader'
-                    ],
-                    exclude: /node_modules/
-                },
-                {
-                    test: /\.scss$/,
-                    include: [
-                        path.resolve(__dirname, './app'),
-                        ...isDeliveryEnvironment ? [path.resolve(__dirname, './admin')] : []
-                    ],
-                    loaders: [
-                        'style-loader',
-                        'css-loader',
-                        'postcss-loader',
-                        'sass-loader'
-                    ]
-                },
-                {
-                    test: /\.(png|ico)$/,
-                    include: path.resolve(__dirname, './assets/favicon'),
-                    loader: 'file-loader',
-                    options: {
-                        name: './assets/favicon/[name].[ext]'
-                    }
-                },
-                {
-                    test: /\.ttf$/,
-                    include: path.resolve(__dirname, './assets/fonts'),
-                    loader: 'file-loader',
-                    options: {
-                        name: './assets/fonts/[name]-[hash].[ext]'
-                    }
-                },
-                {
-                    test: /\.mp3$/,
-                    include: path.resolve(__dirname, './assets/mp3s'),
-                    loader: 'file-loader',
-                    options: {
-                        name: './assets/mp3s/[name]-[hash].[ext]'
-                    }
-                },
-                {
-                    test: /\.pdf$/,
-                    include: path.resolve(__dirname, './assets/scores'),
-                    loader: 'file-loader',
-                    options: {
-                        name: './assets/scores/[name]-[hash].[ext]'
-                    }
-                },
-                {
-                    test: /\.svg$/,
-                    include: path.resolve(__dirname, './assets/svgs'),
-                    loader: 'file-loader',
-                    options: {
-                        name: './assets/svgs/[name]-[hash].[ext]'
-                    }
-                }
-            ]
-        },
-        ...isLocalDevelopment && {
-            devServer: {
-                host: process.env.HOST,
-                port: 1113 || process.env.PORT,
-                disableHostCheck: true,
-                // Needed for proper routing.
-                historyApiFallback: true,
-                hot: true,
-                inline: true,
-                overlay: true,
-                stats: 'minimal'
-            },
-            devtool: 'cheap-module-eval-source-map'
-        }
+        //         // Remove the previous build.
+        //         new CleanWebpackPlugin()
+        //     ]
+        // ],
+        // resolve: {
+        //     // Import from files without specifying extensions.
+        //     extensions: ['.js', '.jsx', '.mp3', '.pdf', '.scss', '.svg'],
+        //     alias: {
+        //         // Allow admin routes only in delivery.
+        //         routes: isDeliveryEnvironment ?
+        //             path.resolve(__dirname, './admin/routes') :
+        //             path.resolve(__dirname, './src/routes'),
+        //         // Grab data from admin folder in local development.
+        //         data: isLocalDevelopment ?
+        //             path.resolve(__dirname, './admin/data') :
+        //             path.resolve(__dirname, './src/data')
+        //     }
+        // },
+        // module: {
+        //     rules: [
+        //         {
+        //             test: /\.jsx?$/,
+        //             include: [
+        //                 path.resolve(__dirname, './src'),
+        //                 ...isDeliveryEnvironment ? [path.resolve(__dirname, './admin')] : []
+        //             ],
+        //             enforce: 'pre',
+        //             loaders: [
+        //                 'babel-loader',
+        //                 'eslint-loader'
+        //             ],
+        //             exclude: /node_modules/
+        //         },
+        //         {
+        //             test: /\.scss$/,
+        //             include: [
+        //                 path.resolve(__dirname, './src'),
+        //                 ...isDeliveryEnvironment ? [path.resolve(__dirname, './admin')] : []
+        //             ],
+        //             loaders: [
+        //                 'style-loader',
+        //                 'css-loader',
+        //                 'postcss-loader',
+        //                 'sass-loader'
+        //             ]
+        //         },
+        //         {
+        //             test: /\.(png|ico)$/,
+        //             include: path.resolve(__dirname, './assets/favicon'),
+        //             loader: 'file-loader',
+        //             options: {
+        //                 name: './assets/favicon/[name].[ext]'
+        //             }
+        //         },
+        //         {
+        //             test: /\.ttf$/,
+        //             include: path.resolve(__dirname, './assets/fonts'),
+        //             loader: 'file-loader',
+        //             options: {
+        //                 name: './assets/fonts/[name]-[hash].[ext]'
+        //             }
+        //         },
+        //         {
+        //             test: /\.mp3$/,
+        //             include: path.resolve(__dirname, './assets/mp3s'),
+        //             loader: 'file-loader',
+        //             options: {
+        //                 name: './assets/mp3s/[name]-[hash].[ext]'
+        //             }
+        //         },
+        //         {
+        //             test: /\.pdf$/,
+        //             include: path.resolve(__dirname, './assets/scores'),
+        //             loader: 'file-loader',
+        //             options: {
+        //                 name: './assets/scores/[name]-[hash].[ext]'
+        //             }
+        //         },
+        //         {
+        //             test: /\.svg$/,
+        //             include: path.resolve(__dirname, './assets/svgs'),
+        //             loader: 'file-loader',
+        //             options: {
+        //                 name: './assets/svgs/[name]-[hash].[ext]'
+        //             }
+        //         }
+        //     ]
+        // },
+        // ...isLocalDevelopment && {
+        //     devServer: {
+        //         host: process.env.HOST,
+        //         port: 1113 || process.env.PORT,
+        //         disableHostCheck: true,
+        //         // Needed for proper routing.
+        //         historyApiFallback: true,
+        //         hot: true,
+        //         inline: true,
+        //         overlay: true,
+        //         stats: 'minimal'
+        //     },
+        //     devtool: 'cheap-module-eval-source-map'
+        // }
     })
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,5 +5,5 @@
  * The gatsby-node file does not allow for ES6, so this is a workaround.
  * https://github.com/gatsbyjs/gatsby/issues/7810#issuecomment-449741977
  */
-// require = require('esm')(module)
-// module.exports = require('./gatsby-node.esm.js')
+require = require('esm')(module)
+module.exports = require('./gatsby-node.esm.js')
