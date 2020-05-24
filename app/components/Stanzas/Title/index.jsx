58c1329fea5b6c5f33d7889db23b7c3e3f35abba
Bennett Lin <bennettslin@gmail.com>
2020-05-24 00:04:01 -0700

    Make song stanzas title a function.

--- app/components/Stanzas/Title/index.jsx
+++ app/components/Stanzas/Title/index.jsx
@@ -1,45 +1,30 @@
-// Component to show the played audio time.
-
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+// Component to show the song title.
+import React from 'react'
+import { useSelector } from 'react-redux'
 import cx from 'classnames'
 
+import { LYRIC_SONG_INDEX_SELECTOR } from '../../../redux/lyric/selectors'
 import { getIndexedTitleForSong } from '../../../album/api/songs'
 
-const mapStateToProps = ({
-    lyricStore: { lyricSongIndex }
-}) => ({
-    lyricSongIndex
-})
+const SongStanzasTitle = () => {
+    const
+        lyricSongIndex = useSelector(LYRIC_SONG_INDEX_SELECTOR),
+        songTitle = getIndexedTitleForSong(lyricSongIndex)
 
-class SongStanzasTitle extends PureComponent {
-
-    static propTypes = {
-        // Through Redux.
-        lyricSongIndex: PropTypes.number.isRequired
-    }
-
-    render() {
-        const
-            { lyricSongIndex } = this.props,
-            songTitle = getIndexedTitleForSong(lyricSongIndex)
-
-        return (
-            <div
-                {...{
-                    className: cx(
-                        'SongStanzasTitle',
-                        'flexCentreContainer',
-                        'textShadow__dark',
-                        'Rancho'
-                    )
-                }}
-            >
-                {songTitle}
-            </div>
-        )
-    }
+    return (
+        <div
+            {...{
+                className: cx(
+                    'SongStanzasTitle',
+                    'flexCentreContainer',
+                    'textShadow__dark',
+                    'Rancho'
+                )
+            }}
+        >
+            {songTitle}
+        </div>
+    )
 }
 
-export default connect(mapStateToProps)(SongStanzasTitle)
+export default SongStanzasTitle
