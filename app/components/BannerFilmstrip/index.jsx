76a3478cfdccef4add5d4f7469e81abb8a27490a
Bennett Lin <bennettslin@gmail.com>
2020-05-29 21:22:06 -0700

    Make BannerFilmstrip a function.

--- app/components/BannerFilmstrip/index.jsx
+++ app/components/BannerFilmstrip/index.jsx
@@ -1,24 +1,24 @@
 // Component to show played song title, time played, and slider interface.
-import React, { PureComponent } from 'react'
+import React from 'react'
 import cx from 'classnames'
 import AlbumBanner from './AlbumBanner'
 import SongBanner from './SongBanner'
 import Filmstrip from './Filmstrip'
 import './style'
 
-class BannerFilmstrip extends PureComponent {
-    render() {
-        return (
-            <div className={cx(
+const BannerFilmstrip = () => (
+    <div
+        {...{
+            className: cx(
                 'BannerFilmstrip',
                 'flexAlignContainer'
-            )}>
-                <AlbumBanner />
-                <SongBanner />
-                <Filmstrip />
-            </div>
-        )
-    }
-}
+            )
+        }}
+    >
+        <AlbumBanner />
+        <SongBanner />
+        <Filmstrip />
+    </div>
+)
 
 export default BannerFilmstrip
