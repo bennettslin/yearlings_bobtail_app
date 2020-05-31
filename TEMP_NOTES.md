ed0f005906b8dc9acb8f21ae03eee83a05afabd6
Bennett Lin <bennettslin@gmail.com>
2020-05-30 23:10:13 -0700

    Make LyricToggleScroll a function.

--- TEMP_NOTES.md
+++ TEMP_NOTES.md
@@ -1,11 +1,15 @@
-# Bugs while production hasn't changed.
+# Bugs.
 * After accessed wormhole, key navigation does not work until manual focus.
 * R and F keys are wonky.
-* Anchor still shows in activated/slider mode.
+
+# Minor issues, to be investigated while production is on master before Gatsby.
 * Figure out why window resizing timeout seems to have regressed.
 * PT Sans Italic is small? e.g. The Man in the High Castle.
 * Switching between songs in local is slow, not as slow in staging
 * tips hand in garryowen for score
+
+# Bugs that may get fixed after logic styling tear out and redo.
+* Anchor still shows in activated/slider mode.
 * Even future verse does not have activated or hover colour.
 
 # Brainless JS tasks
@@ -19,14 +23,14 @@
 * Logic styles should live together. (So that they can be removed together if needed.)
 
 # Easy reduction tasks
+* Make dispatchers functions.
 * Internal stylesheet fixes
 * Simplify Anchor for social media anchors
 * Listeners know to render themselves
 * Mount, responsive, proscenium, and theatre values can just be aggregate selectors. Basically, get rid of everything in window that's not the window width and height. Replace in map state to props first.
 * Also, the listeners.
-* Make dispatchers functions.
 * Make isActivated and isSelected selectors.
-* Remove extraneous bit number helpers.
+* Remove extraneous bit number helpers used only by players now.
 
 # Ongoing Gatsby tasks
 * Dynamic routes for song indices
--- app/components/Lyric/Toggles/Scroll/index.jsx
+++ app/components/Lyric/Toggles/Scroll/index.jsx
@@ -1,10 +1,8 @@
 // Button to toggle between left and right columns.
 
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React, { useRef } from 'react'
 import cx from 'classnames'
-import { connect } from 'react-redux'
-
+import { useSelector } from 'react-redux'
 import CSSTransition from 'react-transition-group/CSSTransition'
 import ScrollVerseDispatcher from '../../../../dispatchers/ScrollVerse'
 import Button from '../../../Button'
@@ -14,64 +12,46 @@ import { LYRIC_SCROLL_BUTTON_KEY } from '../../../../constants/buttons'
 import { AUTOSCROLL } from '../../../../constants/tips'
 import { IS_AUTO_SCROLL_SELECTOR } from '../../../../redux/toggle/selectors'
 
-const mapStateToProps = state => {
-    const isAutoScroll = IS_AUTO_SCROLL_SELECTOR(state)
+const LyricToggleScroll = () => {
+    const
+        dispatchScrollVerse = useRef(),
+        isAutoScroll = useSelector(IS_AUTO_SCROLL_SELECTOR),
+        _handleScrollClick = () => {
+            dispatchScrollVerse.current(true)
+        }
 
-    return {
-        isAutoScroll
-    }
+    return (
+        <CSSTransition
+            appear
+            mountOnEnter
+            unmountOnExit
+            {...{
+                in: !isAutoScroll,
+                timeout: 200,
+                classNames: {
+                    enterActive: 'LyricToggle__shown',
+                    enterDone: 'LyricToggle__shown'
+                }
+            }}
+        >
+            <div className={cx(
+                'LyricToggleScroll',
+                'LyricToggle',
+                'length__buttonLarge'
+            )}>
+                <Button
+                    isLargeSize
+                    {...{
+                        buttonName: LYRIC_SCROLL_BUTTON_KEY,
+                        accessKey: LYRIC_SCROLL_TOGGLE_KEY,
+                        handleButtonClick: _handleScrollClick
+                    }}
+                />
+                <TipsHand reverse {...{ tipType: AUTOSCROLL }} />
+                <ScrollVerseDispatcher {...{ ref: dispatchScrollVerse }} />
+            </div>
+        </CSSTransition>
+    )
 }
 
-class LyricToggleScroll extends PureComponent {
-
-    static propTypes = {
-    // Through Redux.
-        isAutoScroll: PropTypes.bool.isRequired
-    }
-
-    _handleScrollClick = () => {
-        this.dispatchScrollVerse(true)
-    }
-
-    getDispatchScrollVerse = dispatch => {
-        this.dispatchScrollVerse = dispatch
-    }
-
-    render() {
-        const { isAutoScroll } = this.props
-        return (
-            <CSSTransition
-                appear
-                mountOnEnter
-                unmountOnExit
-                {...{
-                    in: !isAutoScroll,
-                    timeout: 200,
-                    classNames: {
-                        enterActive: 'LyricToggle__shown',
-                        enterDone: 'LyricToggle__shown'
-                    }
-                }}
-            >
-                <div className={cx(
-                    'LyricToggleScroll',
-                    'LyricToggle',
-                    'length__buttonLarge'
-                )}>
-                    <Button
-                        isLargeSize
-                        {...{
-                            buttonName: LYRIC_SCROLL_BUTTON_KEY,
-                            accessKey: LYRIC_SCROLL_TOGGLE_KEY,
-                            handleButtonClick: this._handleScrollClick
-                        }}
-                    />
-                    <TipsHand reverse {...{ tipType: AUTOSCROLL }} />
-                    <ScrollVerseDispatcher {...{ ref: this.getDispatchScrollVerse }} />
-                </div>
-            </CSSTransition>
-        )
-    }
-}
-
-export default connect(mapStateToProps)(LyricToggleScroll)
+export default LyricToggleScroll
