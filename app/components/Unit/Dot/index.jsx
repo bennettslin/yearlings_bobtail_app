0cc1bd04caf9c16c3f324c2d75162db382c65db0
Bennett Lin <bennettslin@gmail.com>
2020-05-31 14:03:05 -0700

    Make UnitDot a function.

--- app/components/Unit/Dot/index.jsx
+++ app/components/Unit/Dot/index.jsx
@@ -1,9 +1,9 @@
 // Component to show single dot anchor as its own stanza.
 
-import React, { memo, PureComponent } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { updateAnnotationStore } from '../../../redux/annotation/action'
 import Anchor from '../../Anchor'
 import { getPrefixedDotLetterClassNames } from '../../../helpers/dot'
@@ -18,121 +18,85 @@ import { IS_SLIDER_MOVING_SELECTOR } from '../../../redux/slider/selectors'
 import './logic'
 import './style'
 
-const mapStateToProps = state => {
+const UnitDot = ({
+    // unitIndex,
+    unitDot: {
+        annotationIndex,
+        dotBit
+    },
+    setLyricAnnotationElement
+
+}) => {
     const
-        isAccessedIndexedAnchorShown = IS_ACCESSED_INDEXED_ANCHOR_SHOWN_SELECTOR(state),
-        accessedAnnotationIndex = ACCESSED_ANNOTATION_INDEX_SELECTOR(state),
-        isActivated = IS_ACTIVATED_SELECTOR(state),
-        lyricAnnotationIndex = LYRIC_ANNOTATION_INDEX_SELECTOR(state),
-        isSliderMoving = IS_SLIDER_MOVING_SELECTOR(state)
+        dispatch = useDispatch(),
+        isAccessedIndexedAnchorShown = useSelector(IS_ACCESSED_INDEXED_ANCHOR_SHOWN_SELECTOR),
+        accessedAnnotationIndex = useSelector(ACCESSED_ANNOTATION_INDEX_SELECTOR),
+        isActivated = useSelector(IS_ACTIVATED_SELECTOR),
+        lyricAnnotationIndex = useSelector(LYRIC_ANNOTATION_INDEX_SELECTOR),
+        isSliderMoving = useSelector(IS_SLIDER_MOVING_SELECTOR),
 
-    return {
-        isAccessedIndexedAnchorShown,
-        accessedAnnotationIndex,
-        lyricAnnotationIndex,
-        isActivated,
-        isSliderMoving
-    }
-}
+        isAccessed =
+            isAccessedIndexedAnchorShown &&
+            annotationIndex === accessedAnnotationIndex,
 
-class UnitDot extends PureComponent {
+        isSelected =
+            annotationIndex === lyricAnnotationIndex,
 
-    static propTypes = {
-        // Through Redux.
-        isAccessedIndexedAnchorShown: PropTypes.bool.isRequired,
-        accessedAnnotationIndex: PropTypes.number.isRequired,
-        lyricAnnotationIndex: PropTypes.number.isRequired,
-        isSliderMoving: PropTypes.bool.isRequired,
-        isActivated: PropTypes.bool.isRequired,
-        updateAnnotationStore: PropTypes.func.isRequired,
+        handleAnchorClick = () => {
+            if (isSelected || isSliderMoving || isActivated) {
+                return false
+            }
 
-        // From parent.
-        unitIndex: PropTypes.number.isRequired,
-        unitDot: PropTypes.object.isRequired,
-        setLyricAnnotationElement: PropTypes.func.isRequired
-    }
+            dispatch(updateAnnotationStore({
+                queuedAnnotationIndex: annotationIndex
+            }))
+            return true
+        },
 
-    _handleDotButtonClick = () => {
-        const {
-                unitDot: { annotationIndex },
-                lyricAnnotationIndex,
-                isSliderMoving,
-                isActivated
-            } = this.props,
-
-            isSelected = annotationIndex === lyricAnnotationIndex
-
-        if (isSelected || isSliderMoving || isActivated) {
-            return false
+        setAnnotationForUnit = node => {
+            setLyricAnnotationElement({
+                node,
+                index: annotationIndex
+            })
         }
 
-        this.props.updateAnnotationStore({
-            queuedAnnotationIndex: annotationIndex
-        })
-        return true
-    }
+    return (
+        <div
+            className={cx(
+                'UnitDot',
+                'Unit__column',
 
-    setLyricAnnotationElement = node => {
-        this.props.setLyricAnnotationElement({
-            node,
-            index: this.props.unitDot.annotationIndex
-        })
-    }
+                // Scroll to dot stanza block upon annotation selection.
+                annotationIndex &&
+                `${LYRIC_ANNOTATION_SCROLL}__${annotationIndex}`,
 
-    render() {
-        // TODO: Ideal to get unitDot object from indices?
-        const {
-                unitDot: {
+                // Show and hide dot stanza block in and out based on dot keys.
+                getPrefixedDotLetterClassNames(
+                    dotBit,
+                    // "Child unit dot letter."
+                    'CuD'
+                )
+            )}
+        >
+            {/* Scroll to unit dot at bottom, not unit middle. */}
+            <Anchor
+                {...{
+                    stanzaDotBit: dotBit,
+                    isSelected,
+                    isAccessed,
                     annotationIndex,
-                    dotBit
-                },
-                lyricAnnotationIndex,
-                isAccessedIndexedAnchorShown,
-                accessedAnnotationIndex
-            } = this.props,
-
-            isAccessed =
-                isAccessedIndexedAnchorShown &&
-                annotationIndex === accessedAnnotationIndex,
-
-            isSelected =
-                annotationIndex === lyricAnnotationIndex
-
-        return (
-            <div
-                className={cx(
-                    'UnitDot',
-                    'Unit__column',
-
-                    // Scroll to dot stanza block upon annotation selection.
-                    annotationIndex &&
-                    `${LYRIC_ANNOTATION_SCROLL}__${annotationIndex}`,
-
-                    // Show and hide dot stanza block in and out based on dot keys.
-                    getPrefixedDotLetterClassNames(
-                        dotBit,
-                        // "Child unit dot letter."
-                        'CuD'
-                    )
-                )}
-            >
-                {/* Scroll to unit dot at bottom, not unit middle. */}
-                <Anchor
-                    {...{
-                        stanzaDotBit: dotBit,
-                        isSelected,
-                        isAccessed,
-                        annotationIndex,
-                        handleAnchorClick: this._handleDotButtonClick,
-                        setLyricAnnotationElement: this.setLyricAnnotationElement
-                    }}
-                />
-            </div>
-        )
-    }
+                    handleAnchorClick,
+                    setLyricAnnotationElement: setAnnotationForUnit
+                }}
+            />
+        </div>
+    )
 }
 
-export default connect(
-    mapStateToProps,
-    { updateAnnotationStore }
-)(memo(UnitDot))
+UnitDot.propTypes = {
+    // unitIndex: PropTypes.number.isRequired,
+    unitDot: PropTypes.object.isRequired,
+    setLyricAnnotationElement: PropTypes.func.isRequired
+}
+
+export default memo(UnitDot)
