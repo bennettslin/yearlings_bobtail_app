438ba6ce0e387e65d02c1abba479d6f5fb6c973b
Bennett Lin <bennettslin@gmail.com>
2020-05-31 22:44:56 -0700

    Make LyricToggleExpand a function.

--- app/components/Lyric/Toggles/Expand/index.jsx
+++ app/components/Lyric/Toggles/Expand/index.jsx
@@ -1,8 +1,8 @@
 // Button to collapse and expand lyric column in mobile widths.
 
-import React, { PureComponent } from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import cx from 'classnames'
 import CSSTransition from 'react-transition-group/CSSTransition'
 import LyricDispatcher from '../../../../handlers/Lyric/Dispatcher'
@@ -18,88 +18,61 @@ import { LYRIC_EXPAND } from '../../../../constants/tips'
 import { IS_LYRIC_EXPANDED_SELECTOR } from '../../../../redux/toggle/selectors'
 import './style'
 
-const mapStateToProps = state => {
+const LyricToggleExpand = ({ inMain }) => {
     const
-        isLyricExpandable = IS_LYRIC_EXPANDABLE_SELECTOR(state),
-        isHeightlessLyric = IS_HEIGHTLESS_LYRIC_SELECTOR(state),
-        isLyricExpanded = IS_LYRIC_EXPANDED_SELECTOR(state)
+        dispatchLyricExpand = useRef(),
+        isLyricExpandable = useSelector(IS_LYRIC_EXPANDABLE_SELECTOR),
+        isHeightlessLyric = useSelector(IS_HEIGHTLESS_LYRIC_SELECTOR),
+        isLyricExpanded = useSelector(IS_LYRIC_EXPANDED_SELECTOR),
+        handleButtonClick = () => {
+            dispatchLyricExpand.current()
+        }
 
-    return {
-        isHeightlessLyric,
-        isLyricExpandable,
-        isLyricExpanded
-    }
+    // Render button in main if lyric column is heightless.
+    // TODO: Make this a selector.
+    const shouldRender =
+            Boolean(inMain) === (isHeightlessLyric && !isLyricExpanded)
+
+    return (
+        <CSSTransition
+            appear
+            mountOnEnter
+            unmountOnExit
+            {...{
+                in: isLyricExpandable && shouldRender,
+                timeout: 200,
+                classNames: {
+                    enterActive: 'LyricToggle__shown',
+                    enterDone: 'LyricToggle__shown'
+                }
+            }}
+        >
+            <div className={cx(
+                'LyricToggleExpand',
+                'LyricToggle',
+                inMain ?
+                    'LyricToggleExpand__inMain' :
+                    'LyricToggleExpand__inLyric',
+                'length__buttonLarge'
+            )}>
+                <Button
+                    isLargeSize
+                    {...{
+                        buttonName: LYRIC_EXPAND_BUTTON_KEY,
+                        buttonIdentifier: isLyricExpanded,
+                        accessKey: LYRIC_SECTION_EXPAND_KEY,
+                        handleButtonClick
+                    }}
+                />
+                <TipsHand {...{ tipType: LYRIC_EXPAND }} />
+                <LyricDispatcher {...{ ref: dispatchLyricExpand }} />
+            </div>
+        </CSSTransition>
+    )
 }
 
-class LyricToggleExpand extends PureComponent {
-
-    static propTypes = {
-    // Through Redux.
-        isHeightlessLyric: PropTypes.bool.isRequired,
-        isLyricExpanded: PropTypes.bool.isRequired,
-        isLyricExpandable: PropTypes.bool.isRequired,
-
-        // From parent.
-        inMain: PropTypes.bool
-    }
-
-    handleLyricClick = () => {
-        this.dispatchLyricExpand()
-    }
-
-    getDispatchLyricExpand = dispatch => {
-        this.dispatchLyricExpand = dispatch
-    }
-
-    render() {
-        const {
-                isHeightlessLyric,
-                isLyricExpanded,
-                isLyricExpandable,
-                inMain
-            } = this.props,
-
-            // Render button in main if lyric column is heightless.
-            shouldRender =
-                Boolean(inMain) === (isHeightlessLyric && !isLyricExpanded)
-
-        return (
-            <CSSTransition
-                appear
-                mountOnEnter
-                unmountOnExit
-                {...{
-                    in: isLyricExpandable && shouldRender,
-                    timeout: 200,
-                    classNames: {
-                        enterActive: 'LyricToggle__shown',
-                        enterDone: 'LyricToggle__shown'
-                    }
-                }}
-            >
-                <div className={cx(
-                    'LyricToggleExpand',
-                    'LyricToggle',
-                    inMain ?
-                        'LyricToggleExpand__inMain' :
-                        'LyricToggleExpand__inLyric',
-                    'length__buttonLarge'
-                )}>
-                    <Button
-                        isLargeSize
-                        {...{
-                            buttonName: LYRIC_EXPAND_BUTTON_KEY,
-                            buttonIdentifier: isLyricExpanded,
-                            accessKey: LYRIC_SECTION_EXPAND_KEY,
-                            handleButtonClick: this.handleLyricClick
-                        }}
-                    />
-                    <TipsHand {...{ tipType: LYRIC_EXPAND }} />
-                    <LyricDispatcher {...{ ref: this.getDispatchLyricExpand }} />
-                </div>
-            </CSSTransition>
-        )
-    }
+LyricToggleExpand.propTypes = {
+    inMain: PropTypes.bool
 }
 
-export default connect(mapStateToProps)(LyricToggleExpand)
+export default LyricToggleExpand
