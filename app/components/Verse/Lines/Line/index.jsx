165fdbdb86dfff8123f507cfe02d429aebe92fc2
Bennett Lin <bennettslin@gmail.com>
2020-06-02 00:41:20 -0700

    Make VerseLine a function.

--- app/components/Verse/Lines/Line/index.jsx
+++ app/components/Verse/Lines/Line/index.jsx
@@ -1,105 +1,72 @@
 // Component for a single line in a verse.
 
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import cx from 'classnames'
 import Texts from '../../../Texts'
-import { mapLyricSongIndex } from '../../../../redux/lyric/selectors'
 import './style'
 
-const mapStateToProps = state => {
-    const lyricSongIndex = mapLyricSongIndex(state)
+const VerseLine = ({
+    columnKey,
+    isTruncatable,
+    ...other
 
-    return {
-        lyricSongIndex
-    }
-}
+}) => {
+    const { inVerseBar } = other
 
-class VerseLine extends PureComponent {
+    /**
+     * TODO: Can this truncatable functionality be done without having to
+     * render fragments?
+     */
+    /**
+     * If it's truncatable, we will only ever show either the complete or
+     * the truncated text. Applies to Golden Cord and Uncanny Valley.
+     */
+    return (
+        <>
+            <div
+                className={cx(
+                    'VerseLine',
 
-    static defaultProps = {
-        inVerseBar: false,
-        isTruncatable: false
-    }
+                    /**
+                     * Allow anchor in a verse line to know it's in a
+                     * cursor verse.
+                     */
+                    !inVerseBar && 'sibling__verseCursor',
 
-    static propTypes = {
-        // Through Redux.
-        lyricSongIndex: PropTypes.number.isRequired,
-        dispatch: PropTypes.func.isRequired,
+                    // Lyric, left, centre, right.
+                    `VerseLine__${columnKey}`,
 
-        // From parent.
-        text: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.array,
-            PropTypes.object
-        ]).isRequired,
-
-        // This is passed for verse bar to know when to update.
-        verseIndex: PropTypes.number,
-
-        isTruncatable: PropTypes.bool.isRequired,
-        inVerseBar: PropTypes.bool.isRequired,
-        columnKey: PropTypes.string.isRequired
-    }
-
-    render() {
-        const {
-                /* eslint-disable no-unused-vars */
-                lyricSongIndex,
-                verseIndex,
-                dispatch,
-                /* eslint-enable no-unused-vars */
-
-                columnKey,
-                isTruncatable,
-                ...other
-            } = this.props,
-
-            { inVerseBar } = other
-
-        /**
-         * TODO: Can this truncatable functionality be done without having to
-         * render fragments?
-         */
-        /**
-         * If it's truncatable, we will only ever show either the complete or
-         * the truncated text. Applies to Golden Cord and Uncanny Valley.
-         */
-        return (
-            <>
-                <div
-                    className={cx(
-                        'VerseLine',
-
-                        /**
-                         * Allow anchor in a verse line to know it's in a
-                         * cursor verse.
-                         */
-                        !inVerseBar && 'sibling__verseCursor',
-
-                        // Lyric, left, centre, right.
-                        `VerseLine__${columnKey}`,
-
-                        { 'VerseLine__truncatable': isTruncatable }
-                    )}
-                >
-                    <Texts {...other}
-                        isVerseLyric
-                    />
-                </div>
-
-                {/* Only render if this is a truncatable verse. */}
-                {isTruncatable && (
-                    <div
-                        className="VerseLine__truncated"
-                    >
-                        {'\u2026'}
-                    </div>
+                    { 'VerseLine__truncatable': isTruncatable }
                 )}
-            </>
-        )
-    }
+            >
+                <Texts {...other}
+                    isVerseLyric
+                />
+            </div>
+
+            {/* Only render if this is a truncatable verse. */}
+            {isTruncatable && (
+                <div
+                    className="VerseLine__truncated"
+                >
+                    {'\u2026'}
+                </div>
+            )}
+        </>
+    )
 }
 
-export default connect(mapStateToProps)(VerseLine)
+VerseLine.propTypes = {
+    text: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.array,
+        PropTypes.object
+    ]).isRequired,
+
+    isTruncatable: PropTypes.bool.isRequired,
+    inVerseBar: PropTypes.bool.isRequired,
+    columnKey: PropTypes.string.isRequired
+}
+
+export default memo(VerseLine)
