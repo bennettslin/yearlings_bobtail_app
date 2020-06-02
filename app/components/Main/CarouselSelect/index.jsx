639cc8f023e4fa714b721e9ae034441facfa6198
Bennett Lin <bennettslin@gmail.com>
2020-06-01 19:40:06 -0700

    Make CarouselSelect a function.

--- app/components/Main/CarouselSelect/index.jsx
+++ app/components/Main/CarouselSelect/index.jsx
@@ -1,6 +1,5 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { useRef } from 'react'
+import { useSelector } from 'react-redux'
 import AnnotationDispatcher from '../../../handlers/Annotation/Dispatcher'
 import Button from '../../Button'
 import {
@@ -14,58 +13,40 @@ import {
 import { mapCanCarouselMount } from '../../../redux/mount/selectors'
 import './style'
 
-const mapStateToProps = state => {
-    const canCarouselMount = mapCanCarouselMount(state)
+const CarouselSelect = () => {
+    const
+        dispatchAnnotation = useRef(),
+        canCarouselMount = useSelector(mapCanCarouselMount)
 
-    return {
-        canCarouselMount
+    const handlePreviousClick = () => {
+        dispatchAnnotation.current.dispatchAnnotationDirection(-1)
     }
+
+    const handleNextClick = () => {
+        dispatchAnnotation.current.dispatchAnnotationDirection(1)
+    }
+
+    return canCarouselMount && (
+        <div className="CarouselSelect">
+            <Button
+                isLargeSize
+                {...{
+                    buttonName: CAROUSEL_PREVIOUS_BUTTON_KEY,
+                    accessKey: ARROW_LEFT,
+                    handleButtonClick: handlePreviousClick
+                }}
+            />
+            <Button
+                isLargeSize
+                {...{
+                    buttonName: CAROUSEL_NEXT_BUTTON_KEY,
+                    accessKey: ARROW_RIGHT,
+                    handleButtonClick: handleNextClick
+                }}
+            />
+            <AnnotationDispatcher {...{ ref: dispatchAnnotation }} />
+        </div>
+    )
 }
 
-class CarouselSelect extends PureComponent {
-    static propTypes = {
-        canCarouselMount: PropTypes.bool.isRequired
-    }
-
-    _handleAnnotationPrevious = () => {
-        this.dispatchAnnotationDirection(-1)
-    }
-
-    _handleAnnotationNext = () => {
-        this.dispatchAnnotationDirection(1)
-    }
-
-    getDispatchAnnotation = dispatch => {
-        if (dispatch) {
-            this.dispatchAnnotationDirection = dispatch.dispatchAnnotationDirection
-        }
-    }
-
-    render() {
-        const { canCarouselMount } = this.props
-
-        return canCarouselMount && (
-            <div className="CarouselSelect">
-                <Button
-                    isLargeSize
-                    {...{
-                        buttonName: CAROUSEL_PREVIOUS_BUTTON_KEY,
-                        accessKey: ARROW_LEFT,
-                        handleButtonClick: this._handleAnnotationPrevious
-                    }}
-                />
-                <Button
-                    isLargeSize
-                    {...{
-                        buttonName: CAROUSEL_NEXT_BUTTON_KEY,
-                        accessKey: ARROW_RIGHT,
-                        handleButtonClick: this._handleAnnotationNext
-                    }}
-                />
-                <AnnotationDispatcher {...{ ref: this.getDispatchAnnotation }} />
-            </div>
-        )
-    }
-}
-
-export default connect(mapStateToProps)(CarouselSelect)
+export default CarouselSelect
