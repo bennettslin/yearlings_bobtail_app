5e5bd58f5a90704659798bd836e01930827b4df6
Bennett Lin <bennettslin@gmail.com>
2020-06-03 01:22:43 -0700

    Make OverviewPopup a function.

--- app/components/Popups/Overview/index.jsx
+++ app/components/Popups/Overview/index.jsx
@@ -1,13 +1,14 @@
 // Popup container for overview section.
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import Overview from '../../Overview'
 import Popup from '../../Popup'
 import {
     getIsOverviewVisibleBySection,
     getIsOverviewVisibleBySong
 } from './helper'
+import { mapCanLyricCarouselEnter } from '../../../redux/entrance/selectors'
 import { mapIsLyricLogue } from '../../../redux/lyric/selectors'
 import {
     mapIsLogueOverviewShown,
@@ -17,92 +18,57 @@ import {
 import { mapIsOverlayShown } from '../../../redux/transient/selectors'
 import './style'
 
-const mapStateToProps = state => {
-    const {
-            entranceStore: { canLyricCarouselEnter }
-        } = state,
-        isLyricLogue = mapIsLyricLogue(state),
-        isLogueOverviewShown = mapIsLogueOverviewShown(state),
-        selectedOverviewOption = mapSelectedOverviewOption(state),
-        selectedTipsOption = mapSelectedTipsOption(state),
-        isOverlayShown = mapIsOverlayShown(state)
+const OverviewPopup = ({
+    inMain = false
 
-    return {
-        canLyricCarouselEnter,
-        isOverlayShown,
-        isLogueOverviewShown,
-        selectedOverviewOption,
-        selectedTipsOption,
-        isLyricLogue
-    }
+}) => {
+    const
+        isLyricLogue = useSelector(mapIsLyricLogue),
+        isLogueOverviewShown = useSelector(mapIsLogueOverviewShown),
+        selectedOverviewOption = useSelector(mapSelectedOverviewOption),
+        selectedTipsOption = useSelector(mapSelectedTipsOption),
+        isOverlayShown = useSelector(mapIsOverlayShown),
+        canLyricCarouselEnter = useSelector(mapCanLyricCarouselEnter),
+
+        // TODO: Make this a selector.
+        isVisibleBySection = getIsOverviewVisibleBySection({
+            inMain,
+            isLyricLogue
+        }),
+
+        isVisibleBySong = getIsOverviewVisibleBySong({
+            isLyricLogue,
+            isOverlayShown,
+            isLogueOverviewShown,
+            selectedTipsOption,
+            selectedOverviewOption
+        }),
+
+        isVisible =
+            canLyricCarouselEnter &&
+            isVisibleBySection &&
+            isVisibleBySong
+
+    return (
+        <Popup
+            doMountonEnter
+            doUnmountOnExit
+            bounceAnimate
+            hasNarrowPadding
+            noAbsoluteFull
+            {...{
+                popupName: 'OverviewPopup',
+                isVisible,
+                noFlexCentre: inMain
+            }}
+        >
+            <Overview />
+        </Popup>
+    )
 }
 
-class OverviewPopup extends PureComponent {
-
-    static defaultProps = {
-        inMain: false
-    }
-
-    static propTypes = {
-        // Through Redux.
-        isLyricLogue: PropTypes.bool.isRequired,
-        isOverlayShown: PropTypes.bool.isRequired,
-        isLogueOverviewShown: PropTypes.bool.isRequired,
-        selectedOverviewOption: PropTypes.string.isRequired,
-        selectedTipsOption: PropTypes.string.isRequired,
-        canLyricCarouselEnter: PropTypes.bool.isRequired,
-
-        // From parent.
-        inMain: PropTypes.bool.isRequired
-    }
-
-    render() {
-        const
-            {
-                inMain,
-                isLyricLogue,
-                isOverlayShown,
-                isLogueOverviewShown,
-                selectedOverviewOption,
-                selectedTipsOption,
-                canLyricCarouselEnter
-            } = this.props,
-
-            isVisibleBySection = getIsOverviewVisibleBySection({
-                inMain,
-                isLyricLogue
-            }),
-
-            isVisibleBySong = getIsOverviewVisibleBySong({
-                isLyricLogue,
-                isOverlayShown,
-                isLogueOverviewShown,
-                selectedTipsOption,
-                selectedOverviewOption
-            }),
-
-            isVisible =
-                canLyricCarouselEnter &&
-                isVisibleBySection &&
-                isVisibleBySong
-
-        return (
-            <Popup
-                doMountonEnter
-                doUnmountOnExit
-                bounceAnimate
-                hasNarrowPadding
-                noAbsoluteFull
-                {...{
-                    popupName: 'OverviewPopup',
-                    isVisible,
-                    noFlexCentre: inMain
-                }}
-            >
-                <Overview />
-            </Popup>
-        )
-    }
+OverviewPopup.propTypes = {
+    inMain: PropTypes.bool
 }
 
-export default connect(mapStateToProps)(OverviewPopup)
+export default memo(OverviewPopup)
