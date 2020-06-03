b3d9e012e8b9f4e68f35e6822dc6ef8ae8c91374
Bennett Lin <bennettl@zillowgroup.com>
2020-06-03 13:15:50 -0700

    Make Floor a function.

--- app/components/Theatre/Floor/index.jsx
+++ app/components/Theatre/Floor/index.jsx
@@ -1,101 +1,73 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import cx from 'classnames'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import FloorSeat from './Seat'
 import { getSeatingRowCoordinates } from './helper'
+import {
+    mapProsceniumLeft,
+    mapProsceniumWidth
+} from '../../../redux/proscenium/selectors'
 import { mapFloorHeight } from '../../../redux/theatre/selectors'
 import { mapWindowWidth } from '../../../redux/viewport/selectors'
 import './style'
 
-const mapStateToProps = state => {
-    const {
-            prosceniumStore: {
-                prosceniumLeft,
-                prosceniumWidth
-            }
-        } = state,
-        floorHeight = mapFloorHeight(state),
-        windowWidth = mapWindowWidth(state)
+const Floor = () => {
+    const
+        prosceniumLeft = useSelector(mapProsceniumLeft),
+        prosceniumWidth = useSelector(mapProsceniumWidth),
+        floorHeight = useSelector(mapFloorHeight),
+        windowWidth = useSelector(mapWindowWidth),
+        seatingRowCoordinates = getSeatingRowCoordinates({
+            windowWidth,
+            prosceniumLeft,
+            prosceniumWidth,
+            floorHeight
+        })
 
-    return {
-        windowWidth,
-        prosceniumLeft,
-        prosceniumWidth,
-        floorHeight
-    }
+    return (
+        <div
+            {...{
+                className: cx(
+                    'Floor',
+                    'Theatre__field'
+                ),
+                style: {
+                    width: `${windowWidth}px`,
+                    height: `${floorHeight}px`
+                }
+            }}
+        >
+            {seatingRowCoordinates.map((seatsArray, rowIndex) => (
+                <div
+                    {...{
+                        key: rowIndex,
+                        className: `FloorSeats__${rowIndex}`
+                    }}
+                >
+                    {seatsArray.map(({
+                        chairIndex,
+                        rowIndex,
+                        top,
+                        left,
+                        width,
+                        height
+                    }) => (
+                        <FloorSeat
+                            {...{
+                                key: chairIndex,
+                                chairIndex,
+                                rowIndex,
+                                top,
+                                left,
+                                width,
+                                height
+                            }}
+                        />
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
 }
 
-class Floor extends PureComponent {
-
-    static propTypes = {
-        // Through Redux.
-        windowWidth: PropTypes.number.isRequired,
-        prosceniumLeft: PropTypes.number.isRequired,
-        prosceniumWidth: PropTypes.number.isRequired,
-        floorHeight: PropTypes.number.isRequired
-    }
-
-    render() {
-        const {
-                windowWidth,
-                prosceniumLeft,
-                prosceniumWidth,
-                floorHeight
-            } = this.props,
-
-            seatingRowCoordinates = getSeatingRowCoordinates({
-                windowWidth,
-                prosceniumLeft,
-                prosceniumWidth,
-                floorHeight
-            })
-
-        return (
-            <div
-                {...{
-                    className: cx(
-                        'Floor',
-                        'Theatre__field'
-                    ),
-                    style: {
-                        width: `${windowWidth}px`,
-                        height: `${floorHeight}px`
-                    }
-                }}
-            >
-                {seatingRowCoordinates.map((seatsArray, rowIndex) => (
-                    <div
-                        {...{
-                            key: rowIndex,
-                            className: `FloorSeats__${rowIndex}`
-                        }}
-                    >
-                        {seatsArray.map(({
-                            chairIndex,
-                            rowIndex,
-                            top,
-                            left,
-                            width,
-                            height
-                        }) => (
-                            <FloorSeat
-                                {...{
-                                    key: chairIndex,
-                                    chairIndex,
-                                    rowIndex,
-                                    top,
-                                    left,
-                                    width,
-                                    height
-                                }}
-                            />
-                        ))}
-                    </div>
-                ))}
-            </div>
-        )
-    }
-}
-
-export default connect(mapStateToProps)(Floor)
+export default Floor
