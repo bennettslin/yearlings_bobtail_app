dbbec1252925789c86279f7c2c8ad06ccd750ed4
Bennett Lin <bennettl@zillowgroup.com>
2020-06-03 13:14:09 -0700

    Make Ceiling a function.

--- app/components/Theatre/Ceiling/index.jsx
+++ app/components/Theatre/Ceiling/index.jsx
@@ -1,88 +1,61 @@
 // Section to show the stage illustrations.
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import cx from 'classnames'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import CeilingRafter from './Rafter'
 import { getRaftersRowCoordinates } from './helper'
+import {
+    mapProsceniumLeft,
+    mapProsceniumWidth
+} from '../../../redux/proscenium/selectors'
 import { mapCeilingHeight } from '../../../redux/theatre/selectors'
 import { mapWindowWidth } from '../../../redux/viewport/selectors'
 import './style'
 
-const mapStateToProps = state => {
-    const {
-            prosceniumStore: {
-                prosceniumLeft,
-                prosceniumWidth
-            }
-        } = state,
-        ceilingHeight = mapCeilingHeight(state),
-        windowWidth = mapWindowWidth(state)
+const Ceiling = () => {
+    const
+        prosceniumLeft = useSelector(mapProsceniumLeft),
+        prosceniumWidth = useSelector(mapProsceniumWidth),
+        ceilingHeight = useSelector(mapCeilingHeight),
+        windowWidth = useSelector(mapWindowWidth),
 
-    return {
-        windowWidth,
-        prosceniumLeft,
-        prosceniumWidth,
-        ceilingHeight
-    }
+        raftersRowCoordinates = getRaftersRowCoordinates({
+            prosceniumLeft,
+            prosceniumWidth,
+            ceilingHeight
+        })
+
+    return (
+        <div
+            {...{
+                className: cx(
+                    'Ceiling',
+                    'Theatre__field'
+                ),
+                style: {
+                    width: `${windowWidth}px`,
+                    height: `${ceilingHeight}px`
+                }
+            }}
+        >
+            {raftersRowCoordinates.map(({
+                top,
+                left,
+                width,
+                height
+            }, index) => (
+                <CeilingRafter
+                    {...{
+                        key: index,
+                        top,
+                        left,
+                        width,
+                        height
+                    }}
+                />
+            ))}
+        </div>
+    )
 }
 
-class Ceiling extends PureComponent {
-
-    static propTypes = {
-        // Through Redux.
-        windowWidth: PropTypes.number.isRequired,
-        prosceniumLeft: PropTypes.number.isRequired,
-        prosceniumWidth: PropTypes.number.isRequired,
-        ceilingHeight: PropTypes.number.isRequired
-    }
-
-    render() {
-        const {
-                windowWidth,
-                prosceniumLeft,
-                prosceniumWidth,
-                ceilingHeight
-            } = this.props,
-
-            raftersRowCoordinates = getRaftersRowCoordinates({
-                prosceniumLeft,
-                prosceniumWidth,
-                ceilingHeight
-            })
-
-        return (
-            <div
-                {...{
-                    className: cx(
-                        'Ceiling',
-                        'Theatre__field'
-                    ),
-                    style: {
-                        width: `${windowWidth}px`,
-                        height: `${ceilingHeight}px`
-                    }
-                }}
-            >
-                {raftersRowCoordinates.map(({
-                    top,
-                    left,
-                    width,
-                    height
-                }, index) => (
-                    <CeilingRafter
-                        {...{
-                            key: index,
-                            top,
-                            left,
-                            width,
-                            height
-                        }}
-                    />
-                ))}
-            </div>
-        )
-    }
-}
-
-export default connect(mapStateToProps)(Ceiling)
+export default Ceiling
