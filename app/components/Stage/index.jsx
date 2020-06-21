27c13500b43acbfc1902539ba55efb41b7648dd3
Bennett Lin <bennettslin@gmail.com>
2020-06-21 10:11:13 -0700

    Remove transition from stage.

--- app/components/Stage/index.jsx
+++ app/components/Stage/index.jsx
@@ -1,45 +1,26 @@
-// Stage elements that change based on the scene.
-
 import React, { useEffect } from 'react'
 import cx from 'classnames'
-import { useSelector } from 'react-redux'
-import { mapIsSceneChangeDone } from '../../redux/entrance/selector'
-import Transition from 'react-transition-group/Transition'
 import AspectRatio from './AspectRatio'
 import Scene from '../Scene'
 import Sky from '../Scene/Sky'
 import Wood from '../Scene/Wood'
 
 const Stage = () => {
-    const isSceneChangeDone = useSelector(mapIsSceneChangeDone)
-
-    const onEntered = () => {
-        logTransition('Stage did enter.')
-    }
-
     useEffect(() => {
         logMount('Stage')
     }, [])
 
     return (
         <AspectRatio>
-            <Transition
-                {...{
-                    in: isSceneChangeDone,
-                    timeout: 200,
-                    onEntered
-                }}
-            >
-                <div className={cx(
-                    'Stage',
-                    'abF',
-                    'ovH'
-                )}>
-                    <Sky />
-                    <Wood />
-                    <Scene />
-                </div>
-            </Transition>
+            <div className={cx(
+                'Stage',
+                'abF',
+                'ovH'
+            )}>
+                <Sky />
+                <Wood />
+                <Scene />
+            </div>
         </AspectRatio>
     )
 }
