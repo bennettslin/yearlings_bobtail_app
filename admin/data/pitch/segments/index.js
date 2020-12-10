da54f7a3acd1aec4ad02a93f6324e8d477a75faa
Bennett Lin <bennettslin@gmail.com>
2020-12-09 22:34:41 -0800

    More pitch content.

--- admin/data/pitch/segments/index.js
+++ admin/data/pitch/segments/index.js
@@ -18,64 +18,66 @@ import {
 
 export default [
     {
-        title: 'Introduction',
+        title: `Introduction`,
         text: [
-            `If you're interested in working with the Bobtail Yearlings but feeling hesitant, I totally understand! The way we're going about things is certainly unusual and untested.`,
-            `In this pitch, I'd like to convince you that the Bobtail Yearlings are no riskier to bet on than any other artist you've worked with in the past.`
+            `Hi there! Are you curious to work with the Bobtail Yearlings, but our untested way of doing things is making you nervous?`,
+            `Read on, and I'll convince you that the Bobtail Yearlings are no more of a risky bet than any other artist you've worked with in the past.`
         ],
         tempImageDescription: `Old Bennett`,
         imageKey: TEMP_PITCH_ICON_1
     },
     {
-        title: 'Stores and startups',
+        title: `Startups and stores`,
         text: [
-            `Let's start by talking about large companies. It's easy to think of all big companies as being the same, but some begin as startups, and others as stores.`
+            `Let's start by talking about the world of business. It's easy to describe all big companies using the same terms, but when they're just starting out, they can be very different. Some companies begin as startups, and others as stores.`
         ],
         tempImageDescription: `Google and Walmart`,
         imageKey: TEMP_PITCH_ICON_2
     },
     {
-        title: 'pitch page 3',
+        title: `Stores profit, then scale`,
         text: [
-            `Stores, like Walmart, profit first, then scale. If a store is profitable in one town, it proves that a store in another town can also be. Linear growth.`
+            `A store seeks first to profit, then scale. For example, Walmart began as a neighborhood store. Once that store became profitable, Sam Walton opened another one in the next town over, and then another.`,
+            `If a store has a chance to scale, then there's little risk involved, since each new store can repeat the same proven strategy. The downside is that scaling might not be possible at all. Since stores fulfill a known need, they're largely the same. So wherever one competitor has opened a store, there's much less room for another.`
         ],
         tempImageDescription: `Walmart stores`,
         imageKey: TEMP_PITCH_ICON_3
     },
     {
-        title: 'pitch page 4',
+        title: `Startups scale, then profit`,
         text: [
-            `Startups, like Google, need to scale first, then think about profits. They may not see a profit until they reach millions of users. Superlinear growth.`
+            `Unlike stores, a startup needs to scale first, even if this means going without profit in the short term. A good example is Google, which wasn't profitable in its first few years until it reached millions of users.`,
+            `In other words, founding a startup is a risky venture at every step along the way. The upside is that the need to self-differentiate is baked into a startup's business model. So if its founders have correctly identified an unknown need that they can fulfill, then the path to scale and success is probably assured.`
         ],
         tempImageDescription: `Google suite`,
         imageKey: TEMP_PITCH_ICON_4
     },
     {
-        title: 'pitch page 5',
+        title: `Startups and network effects`,
         text: [
-            `If a startup makes no profit when it has thousands of users, why would anyone expect it can suddenly profit with a million users? One reason is the network effect, Facebook.`
+            `If a startup makes no profit when it only has thousands of users, how can it expect to suddenly be profitable with a million users? One reason is something called network effects. For example, what makes Facebook valuable is not the users themselves, but the connections between them.`
         ],
         tempImageDescription: `Facebook nodes`,
         imageKey: TEMP_PITCH_ICON_5
     },
     {
-        title: 'pitch page 6',
+        title: `Startups and economies of scale`,
         text: [
-            `Another reason is economies of scale, Amazon.`
+            `Another reason a startup might only later become profitable is something called economies of scale. Amazon`
         ],
         tempImageDescription: `Amazon delivery cycle`,
         imageKey: TEMP_PITCH_ICON_6
     },
     {
-        title: 'pitch page 7',
+        title: `pitch page 7`,
         text: [
-            `It's easy for investors to invest in stores. But startups may not see a profit for a long time, they depend on seed money from investors. But it's precisely for this reason that they are so risky to investors.`
+            `It's easy for investors to invest in stores. But startups may not see a profit for a long time, they depend on seed money from investors. But it's precisely for this reason that they are so risky to investors. Linear sublinear growth.`
         ],
         tempImageDescription: ``,
         imageKey: TEMP_PITCH_ICON_7
     },
     {
-        title: 'pitch page 8',
+        title: `pitch page 8`,
         text: [
             `Compounding the problem is that unlike stores, which are the oldest business model, our world has only recently begun to understand the phenomenon of the startup. That's why the word "startup" has grown more popular in recent decades. It's a constant reminder to investors not to judge startups as if they were stores. It's a different business model.`
         ],
@@ -83,15 +85,15 @@ export default [
         imageKey: TEMP_PITCH_ICON_8
     },
     {
-        title: 'pitch page 9',
+        title: `A world without startups`,
         text: [
-            `To mitigate risk, could investors only invest in stores, not startups? Could we expect the same innovation to come from stores? Probably not. (Mention DNA in Paul Graham's essay Startup = Growth.)`
+            `To mitigate risk, could investors only invest in stores, not startups? Could we expect the same innovation to come from stores? Probably not. Foreign countries. (Mention DNA in Paul Graham's essay Startup = Growth.)`
         ],
         tempImageDescription: `Walmart suite.`,
         imageKey: TEMP_PITCH_ICON_9
     },
     {
-        title: 'pitch page 10',
+        title: `Indies are like stores`,
         text: [
             `Corollary in the world of popular music. Labels want to invest in indie artists, because indie artists promise linear growth, just like stores. Traditionally, indie artists show they can sustain themselves with a regional following. Linear growth.`
         ],
@@ -99,7 +101,7 @@ export default [
         imageKey: TEMP_PITCH_ICON_10
     },
     {
-        title: 'pitch page 11',
+        title: `Artups are like startups`,
         text: [
             `But what if some artists are more like startups, in that they need to scale before they prove sustainable? And what if certain kinds of artistic innovation are only possible from such artists? There's one quick fix. We don't have a word for these artists. I propose artups.`
         ],
@@ -107,7 +109,7 @@ export default [
         imageKey: TEMP_PITCH_ICON_11
     },
     {
-        title: 'pitch page 12',
+        title: `Artups and network effects`,
         text: [
             `How could an artup that isn't sustainable with only thousands of fans, be able to attract millions of them? Like startups, network effect. Difficult works, like Ulysses.`
         ],
@@ -115,7 +117,7 @@ export default [
         imageKey: TEMP_PITCH_ICON_12
     },
     {
-        title: 'pitch page 13',
+        title: `Artups and economies of scale`,
         text: [
             `Another reason is economies of scale. Complex works that require collaboration and coordination. Pet Sounds or Illinois?`
         ],
@@ -123,7 +125,7 @@ export default [
         imageKey: TEMP_PITCH_ICON_13
     },
     {
-        title: 'pitch page 14',
+        title: `Yearling's Bobtail`,
         text: [
             `Yearling's Bobtail is both a difficult and a complex work, so it's an artup.`
         ],
@@ -131,7 +133,7 @@ export default [
         imageKey: TEMP_PITCH_ICON_14
     },
     {
-        title: 'pitch page 15',
+        title: `Bet on the Bobtail Yearlings`,
         text: [
             `Of course, betting on the Bobtail Yearlings comes with a risk, just as betting on any other startup or artup does. This is just to say it wouldn't be unreasonably risky. Good conclusion. Happy to work with you.`
         ],
--- admin/tasks/remaining/next/index.js
+++ admin/tasks/remaining/next/index.js
@@ -6,7 +6,7 @@ export default [
     },
     {
         taskName: `Pitch content.`,
-        workedHours: 10
+        workedHours: 13
     },
 
     /* Ongoing. */
