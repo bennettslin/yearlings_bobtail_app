cc5a6973b2a0c51bb6216a33e4c04d993bab2ffe
Bennett Lin <bennettslin@gmail.com>
2020-12-12 01:31:55 -0800

    More updates to pitch content.

--- admin/data/pitch/segments/index.js
+++ admin/data/pitch/segments/index.js
@@ -18,17 +18,17 @@ export default [
     {
         title: `Introduction`,
         text: [
-            `Hi there! Are you interested in working with the Bobtail Yearlings, but our untested way of doing things is making you nervous?`,
-            `Read on, and I'll convince you that the Bobtail Yearlings are no more of a risky bet than other artists you've worked with in the past.`
+            `Hi there! Are you otherwise interested in working with the Bobtail Yearlings, but our untested way of doing things is making you nervous?`,
+            `Read on, and I'll convince you that the Bobtail Yearlings are as reasonable to bet on as any other artist you've worked with in the past.`
         ],
         tempImageDescription: `Old Bennett`,
         imageKey: TEMP_PITCH_ICON_1
     },
     {
-        title: `Stores and startups`,
+        title: `Stores versus startups`,
         text: [
             `Let's start by talking about the world of business. It's easy to paint all big companies with the same brush, but when they're starting out, they can be very different. Some begin as startups, and others as stores.`,
-            `(For the sake of this pitch, a store is any business selling products or services that involve in-person transactions.)`
+            `(For the sake of this pitch, we'll call any company a store if it sells products or services that involve in-person transactions.)`
         ],
         tempImageDescription: `Google and Walmart`,
         imageKey: TEMP_PITCH_ICON_2
@@ -36,7 +36,7 @@ export default [
     {
         title: `Stores profit, then scale`,
         text: [
-            `So what's the difference? A store seeks to profit first, then scale. For example, Walmart began as a neighborhood store. Once that became profitable, Sam Walton opened another store in the next town over, and then another.`,
+            `What's the difference between startups and stores? A store seeks first to profit, then scale. For example, Walmart began as a neighborhood store. Once it became profitable, Sam Walton opened another store in the next town over, and then another.`,
             `There's little risk in trying to scale a store, since each new store can repeat the same proven strategy. The downside is that scaling might not be possible. Since stores fulfill a known need, one isn't much different from the next. So wherever one competitor has opened a store, there's now less room for others.`
         ],
         tempImageDescription: `Walmart stores, linear graph`,
@@ -45,7 +45,7 @@ export default [
     {
         title: `Startups scale, then profit`,
         text: [
-            `Unlike stores, a startup needs to scale first, even if this means going without profit in the short term. A good example is Google, which stayed unprofitable in its first few years until it reached millions of users.`,
+            `Unlike stores, a startup seeks to scale first, even if this means going without profit in the short term. A good example is Google, which stayed unprofitable in its first few years until it reached millions of users.`,
             `In other words, founding a startup is a risky venture at every step along the way. The upside is that the need to self-differentiate is baked into a startup's business model. So if its founders have correctly identified an unknown need that they can fulfill, then the path to scale and success is probably assured.`
         ],
         tempImageDescription: `Google suite, superlinear graph`,
@@ -54,8 +54,8 @@ export default [
     {
         title: `Startups and network effects`,
         text: [
-            `If a startup makes no profit when it only has thousands of users, how could it suddenly turn profitable with a million users? One reason is the network effect, where a product offers more value to each user as more users use the product.`,
-            `For example, what makes Facebook so valuable is not the users themselves, but the connections between them.`
+            `If a startup makes no profit when it only has thousands of users, how can it suddenly turn profitable with a million users? One reason is the network effect, where a product becomes more valuable to each user as more users use the product.`,
+            `For example, back when Facebook launched, its thousandth user was only able to connect with fellow classmates at Harvard. By the time it had millions of users, however, each user could now connect with old friends, distant family members, and more.`
         ],
         tempImageDescription: `Facebook nodes`,
         imageKey: TEMP_PITCH_ICON_5
@@ -63,16 +63,17 @@ export default [
     {
         title: `Startups and economies of scale`,
         text: [
-            `Another reason a startup might only later become profitable is something called economies of scale. Amazon`
+            `Another reason a startup might only later become profitable is something called economies of scale. These are when fixed costs, spread over a greater number of users, level out to lower costs per user.`,
+            `This neatly sums up Amazon's advantage. In its first five years, Amazon lost money because the upfront costs of packaging and shipping physical books are so high. Once the infrastructure was in place to handle this, however, any additional costs of taking on more users and offering more products became negligible.`
         ],
         tempImageDescription: `Amazon delivery cycle`,
         imageKey: TEMP_PITCH_ICON_6
     },
     {
-        title: `Greater risk, greater reward`,
+        title: `Greater risks, greater rewards`,
         text: [
-            `While stores are the oldest form of business, startups are more recent and behave differently. So in the early days, investors would often dismiss a startup as a bad deal, which was unfortunate. Since startups need to scale first before making a profit, they absolutely depend on investors for initial support.`,
-            `This is why we now have the word "startup," to remind us that startups are their own thing. They're not simply stores that are more risky because they're less competent. A startup comes with more risk because it aims to fulfill an unknown need. If it succeeds, then it becomes the leader of a new market. And for investors, this yields a greater reward.`
+            `While stores are the oldest form of business, startups are more recent, and they behave much differently. So in the early days, investors would often dismiss a startup as a bad deal. This was unfortunate, since startups truly depend on investors. Without their initial support, it isn't possible to scale first without making a profit.`,
+            `This is why we have the word "startup," to remind us that startups are their own thing. They're not simply stores that are more risky due to being less competent. A startup comes with more risk precisely because it aims to fulfill an unknown need. But if it succeeds, then it becomes the leader of a new market. And for investors, this means a greater reward.`
         ],
         tempImageDescription: `Comparison of linear and superlinear graphs`,
         imageKey: TEMP_PITCH_ICON_7
@@ -80,8 +81,8 @@ export default [
     {
         title: `Indies are like stores`,
         text: [
-            `Let's go back now to the world of music. For the past few decades, record labels have preferred to sign indie artists, who pose fewer risks. But it's no wonder why indie artists are less risky; they're like stores.`,
-            `Indie artists typically gain fans by playing live shows. This is possible because audiences for shows already exist, and they arrive with certain expectations. In other words, indie artists fulfill a known need. And once an indie artist has grown a regional fanbase of a thousand, they can then repeat their strategy to win over the next thousand in a neighboring region, and then the next.`
+            `Let's get back now to music. For the past couple decades, record labels have preferred to sign indie artists, since they pose fewer risks. But it's no wonder why indie artists are less risky; they're the stores of the music world.`,
+            `Indie artists typically gain fans by playing live shows. This is possible because audiences for shows already exist, and they arrive with certain expectations. In other words, indie artists fulfill a known need. And once an indie artist has grown a regional fanbase of a thousand, they can repeat the same strategy to win over the next thousand fans in a neighboring region, and then the next.`
         ],
         tempImageDescription: `Indie artist playing to audiences in different cities, linear graph.`,
         imageKey: TEMP_PITCH_ICON_8
@@ -89,8 +90,8 @@ export default [
     {
         title: `Artups are like startups`,
         text: [
-            `But what if other artists are like startups, trying to fulfill an unknown need? Artists who believe, as Steve Jobs did, that people don't know what they want until you show them? If their artistic hunches are correct, then their path to scale and success might be assured. They'll be the only one of their kind to reach an audience that previously didn't exist.`,
-            `Yet unless we recognise these artists as being their own thing, we'll only ever see them as less competent indie artists. Which is unfortunate, because like startups, these artists absolutely need initial support from others. So let's create a new word for them. I'll call them "artups."`
+            `But what if other artists are like startups, trying to fulfill an unknown need? Artists who believe, as Steve Jobs did, that people don't know what they want until you show them? If their artistic hunches are correct, then their path to scale and success might be assured. After all, they'll be the only one of their kind to reach an audience that previously didn't exist.`,
+            `Yet unless we recognise these artists as being their own thing, we'll only ever see them as less competent indie artists. Which is unfortunate, because like startups, these artists absolutely need others to provide initial support. So let's create a new word for them. I'll call them "artups."`
         ],
         tempImageDescription: `Artist creating works, superlinear graph.`,
         imageKey: TEMP_PITCH_ICON_9
@@ -98,23 +99,24 @@ export default [
     {
         title: `Artups and network effects`,
         text: [
-            `If an artup isn't capable of winning over a thousand fans on its own, why would it be able to attract a million with external support? Like startups, an artup might benefit from the network effect. Difficult works, like Ulysses.`
+            `If an artup isn't capable of winning over a thousand fans on their own, why should we think that label support could help them gain a million? One reason is that artups can benefit from the network effect.`,
+            `For example, James Joyce's Ulysses.`
         ],
-        tempImageDescription: ``,
+        tempImageDescription: `Ulysses nodes`,
         imageKey: TEMP_PITCH_ICON_10
     },
     {
         title: `Artups and economies of scale`,
         text: [
-            `Another reason is economies of scale. Complex works that require collaboration and coordination. Pet Sounds or Illinois?`
+            `Similarly, economies of scale might also help an artup. Complex works that require collaboration and coordination. Pet Sounds or Illinois?`
         ],
-        tempImageDescription: ``,
+        tempImageDescription: `Pet Sounds, `,
         imageKey: TEMP_PITCH_ICON_11
     },
     {
         title: `The artup of Yearling's Bobtail`,
         text: [
-            `Yearling's Bobtail is both a difficult and a complex work, so it's an artup.`
+            `If you've already checked out our audio webcomic, then maybe you know where this is going. Yearling's Bobtail is both a difficult and a complex work, so it's an artup.`
         ],
         tempImageDescription: ``,
         imageKey: TEMP_PITCH_ICON_12
--- admin/tasks/remaining/next/index.js
+++ admin/tasks/remaining/next/index.js
@@ -6,7 +6,7 @@ export default [
     },
     {
         taskName: `Pitch content.`,
-        workedHours: 15
+        workedHours: 21
     },
 
     /* Ongoing. */
