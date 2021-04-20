31e89417ea78351fe9e9d228b126829dd8fcb327
Bennett Lin <bennettslin@gmail.com>
2021-04-19 20:46:55 -0700

    Scroll to selected annotation.

--- admin/containers/Annotations/Annotation/index.jsx
+++ admin/containers/Annotations/Annotation/index.jsx
@@ -1,21 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import cx from 'classnames'
+import { useDispatch } from 'react-redux'
 import AnnotationCard from '../AnnotationCard'
 import {
     getTitleForAnnotation,
     getTodoForAnnotation,
 } from '../../../../app/api/album/annotations'
 import { getAnnotationCardIndices } from '../../../../app/api/album/cards'
+import { updateSelectedStore } from '../../../../app/redux/selected/action'
 
 const Annotation = ({ songIndex, annotationIndex }) => {
-    if (!songIndex || !annotationIndex) {
-        return null
-    }
-
     const
+        dispatch = useDispatch(),
         annotationTitle = getTitleForAnnotation(songIndex, annotationIndex),
         annotationTodo = getTodoForAnnotation(songIndex, annotationIndex)
+
+    const selectAnnotation = () => {
+        dispatch(updateSelectedStore({
+            selectedSongIndex: songIndex,
+            selectedAnnotationIndex: annotationIndex,
+        }))
+    }
 
     return (
         <div
@@ -23,10 +29,18 @@ const Annotation = ({ songIndex, annotationIndex }) => {
                 className: cx(
                     'TempGlobalAnnotation',
                     annotationTodo && 'TempGlobalAnnotation__todo',
+                    `TempGlobalAnnotation-${songIndex}-${annotationIndex}`,
                 ),
             }}
         >
-            <div className="TempGlobalAnnotationHeader">
+            <div
+                {...{
+                    className: cx(
+                        'TempGlobalAnnotationHeader',
+                    ),
+                    onClick: selectAnnotation,
+                }}
+            >
                 {annotationIndex}. {annotationTitle}
             </div>
             {getAnnotationCardIndices(
