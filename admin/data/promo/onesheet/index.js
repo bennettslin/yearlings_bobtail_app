9704bf5af42e49cccbae90d6bcd4cf6ab042a0d4
Bennett Lin <bennettslin@gmail.com>
2021-06-12 01:31:03 -0700

    Almost finish onesheet.

--- admin/data/promo/onesheet/index.js
+++ admin/data/promo/onesheet/index.js
@@ -2,133 +2,135 @@ import { getFormattedSlideMetadata } from '../helpers/slide'
 
 export default getFormattedSlideMetadata({
     body: [
-        // {
-        //     paragraph: [
-        //         `Can a rock album ever win the Pulitzer Prize? It might seem unlikely. To earn popular approval and critical acclaim, today's rock artists usually aim for subjective qualities that don't intrigue literary scholars and musicologists the way hip hop does. But what if rock music can also merit academic study while appealing to everyone? The Bobtail Yearlings accept this challenge! And to have us hold them to it, they promise to win rock a Pulitzer in their lifetime. With that now said, they present`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Yearling's Bobtail`,
-        //         },
-        //         `, an ambitious multimedia project that merges a double album with an audio webcomic.`,
-        //     ],
-        // },
-        // {
-        //     paragraph: [
-        //         `Written and recorded by frontperson Bennett Lin,`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Yearling's Bobtail`,
-        //         },
-        //         `tells the autobiographical tale of an artistically inclined Asian boy growing up in Los Angeles, whose autistic brother and first crush on a Jewish girl cause him to question his Buddhist upbringing. Bullied at school, he finds solace in a mail-order cult that instructs him to shut himself away. Years later, he embarks on a five-year plan for self-improvement that ultimately wins the hand of his first love, who brings him into her Hindu faith. But as his band gains exposure in the local scene, he bolsters his ego by seeking affection from other women, leading to heartbreak and loss.`,
-        //     ],
-        // },
-        // {
-        //     paragraph: [
-        //         `Born from Bennett's vision to create "The`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Ulysses`,
-        //         },
-        //         `of Rock Albums,"`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Yearling's Bobtail`,
-        //         },
-        //         `follows in the footsteps of modernist writer James Joyce, with lyrics employing stream of consciousness ("`,
-        //         {
-        //             anchor: `Good Night, Sita`,
-        //             songIndex: 8,
-        //         },
-        //         `"), meta-reference ("`,
-        //         {
-        //             anchor: `Grasshoppers Lie Heavy`,
-        //             songIndex: 9,
-        //         },
-        //         `"), and neologistic puns ("`,
-        //         {
-        //             anchor: `Constellations!`,
-        //             songIndex: 10,
-        //         },
-        //         `"). It even uses a literary device of Bennett's own invention called doublespeaker rhyme, in which two sets of lyrics that rhyme syllable for syllable are panned hard to separate speakers ("`,
-        //         {
-        //             anchor: `Odin`,
-        //             songIndex: 4,
-        //         },
-        //         `"), creating the musical version of a cinematic split screen.`,
-        //     ],
-        // },
-        // {
-        //     paragraph: [
-        //         `Inspired by such classic rock albums as`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Pet Sounds`,
-        //         },
-        //         `and`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Village Green Preservation Society`,
-        //         },
-        //         `,`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Yearling's Bobtail`,
-        //         },
-        //         `serves up memorable pop tunes with folk instruments arranged in intricate counterpoint. The music is a hodgepodge of styles, from Tuvan throatsinging ("`,
-        //         {
-        //             anchor: `Pchelka's Starry Journey`,
-        //             songIndex: 5,
-        //         },
-        //         `") and Arabic quarter tones ("`,
-        //         {
-        //             anchor: `Ash Wednesday`,
-        //             songIndex: 6,
-        //         },
-        //         `") to Irish reel ("`,
-        //         {
-        //             anchor: `Garryowen`,
-        //             songIndex: 12,
-        //         },
-        //         `") and honky tonk ("`,
-        //         {
-        //             anchor: `On a Golden Cord`,
-        //             songIndex: 13,
-        //         },
-        //         `"). It also includes Shepard tones ("`,
-        //         {
-        //             anchor: `Cremated`,
-        //             songIndex: 16,
-        //         },
-        //         `"), an auditory illusion in which a tone gradually descends in pitch while remaining fixed in place.`,
-        //     ],
-        // },
-        // {
-        //     paragraph: [
-        //         `Completed a decade ago,`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Yearling's Bobtail`,
-        //         },
-        //         `was never released. At the time, the indie labels were most eager to sign artists who didn't need them, but the Bobtail Yearlings need a label as much as they hope to be needed back. Still, by being overlooked, they became a rare controlled experiment that only happens by chance in the wild. What does innovation sound like if it's never heard and thus influences no one? At once otherworldly and uncannily familiar,`,
-        //         {
-        //             isItalic: true,
-        //             lyric: `Yearling's Bobtail`,
-        //         },
-        //         `traces a lineage but belongs to no time, offering a fascinating glimpse into what rock music today might have been.`,
-        //     ],
-        // },
-        // {
-        //     paragraph: `To bide his time during indie rock's era of peak influence, Bennett learned new creative skills such as music composition, web development, and illustration. This allowed him to build the audio webcomic, which presents the album's story as a theatrical play, with illustrated characters and stage props that enter and exit as the music progresses. Annotations offer background context for the lyrics, and links to Wikipedia explain obscure references. Meanwhile, full music scores offer insight into how each song was written.`,
-        // },
         {
             paragraph: [
-                // TODO: Make Malkmus line more about maximising possibilities. Revise line about inherited beliefs.
-                `Thanks to Donald Trump's failed presidency, fewer of us accept the standard indie rock belief that winning over live crowds is the best signifier of potential, or that willful naivete should always triumph over cultivated talent. Perhaps now is the time to reconsider`,
+                // TODO: While appealing to everyone.
+                `Can a rock album ever win the Pulitzer Prize? It might seem unlikely. To earn popular approval and critical acclaim, today's rock artists usually aim for subjective qualities that don't intrigue literary scholars and musicologists the way hip hop does. But what if rock music can also merit academic study while appealing to everyone? The Bobtail Yearlings accept this challenge! And to have us hold them to it, they promise to win rock music a Pulitzer in their lifetime. With that now said, they present`,
+                {
+                    isItalic: true,
+                    lyric: `Yearling's Bobtail`,
+                },
+                `, an ambitious multimedia project that merges a double album with an audio webcomic.`,
+            ],
+        },
+        {
+            paragraph: [
+                `Written and recorded by frontperson Bennett Lin,`,
+                {
+                    isItalic: true,
+                    lyric: `Yearling's Bobtail`,
+                },
+                `tells the autobiographical tale of an artistically inclined Asian boy growing up in Los Angeles, whose autistic brother and first crush on a Jewish girl cause him to question his Buddhist upbringing. Bullied at school, he finds solace in a mail-order cult that instructs him to shut himself away. Years later, he embarks on a five-year plan for self-improvement that ultimately wins the hand of his first love, who brings him into her Hindu faith. But as his band gains exposure in the local scene, he bolsters his ego by seeking affection from other women, leading to heartbreak and loss.`,
+            ],
+        },
+        {
+            paragraph: [
+                `Born from Bennett's vision to create "The`,
+                {
+                    isItalic: true,
+                    lyric: `Ulysses`,
+                },
+                `of Rock Albums,"`,
+                {
+                    isItalic: true,
+                    lyric: `Yearling's Bobtail`,
+                },
+                `follows in the footsteps of modernist writer James Joyce, with lyrics employing stream of consciousness ("`,
+                {
+                    anchor: `Good Night, Sita`,
+                    songIndex: 8,
+                },
+                `"), meta-reference ("`,
+                {
+                    anchor: `Grasshoppers Lie Heavy`,
+                    songIndex: 9,
+                },
+                `"), and neologistic puns ("`,
+                {
+                    anchor: `Constellations!`,
+                    songIndex: 10,
+                },
+                `"). It even uses a literary device of Bennett's own invention called doublespeaker rhyme, in which two sets of lyrics that rhyme syllable for syllable are panned hard to separate speakers ("`,
+                {
+                    anchor: `Odin`,
+                    songIndex: 4,
+                },
+                `"), creating the musical version of a cinematic split screen.`,
+            ],
+        },
+        {
+            paragraph: [
+                `Inspired by such classic rock albums as`,
+                {
+                    isItalic: true,
+                    lyric: `Pet Sounds`,
+                },
+                `and`,
+                {
+                    isItalic: true,
+                    lyric: `Village Green Preservation Society`,
+                },
+                `,`,
+                {
+                    isItalic: true,
+                    lyric: `Yearling's Bobtail`,
+                },
+                `serves up memorable pop tunes with folk instruments arranged in intricate counterpoint. The music is a hodgepodge of styles, from Tuvan throatsinging ("`,
+                {
+                    anchor: `Pchelka's Starry Journey`,
+                    songIndex: 5,
+                },
+                `") and Arabic quarter tones ("`,
+                {
+                    anchor: `Ash Wednesday`,
+                    songIndex: 6,
+                },
+                `") to Irish reel ("`,
+                {
+                    anchor: `Garryowen`,
+                    songIndex: 12,
+                },
+                `") and honky tonk ("`,
+                {
+                    anchor: `On a Golden Cord`,
+                    songIndex: 13,
+                },
+                `"). It also includes Shepard tones ("`,
+                {
+                    anchor: `Cremated`,
+                    songIndex: 16,
+                },
+                `"), an auditory illusion in which a tone gradually descends in pitch while remaining fixed in place.`,
+            ],
+        },
+        {
+            // TODO: Path that rock music didn't take.
+            paragraph: [
+                `Completed a decade ago,`,
+                {
+                    isItalic: true,
+                    lyric: `Yearling's Bobtail`,
+                },
+                `was never released. At the time, the indie labels were most eager to sign artists who didn't need them, but the Bobtail Yearlings need a label as much as they hope to be needed back. Still, by being overlooked, they became a rare controlled experiment that can only happen by chance in the wild. What does innovation sound like if it's never heard and thus influences no one? At once otherworldly and uncannily familiar,`,
+                {
+                    isItalic: true,
+                    lyric: `Yearling's Bobtail`,
+                },
+                `traces a lineage but belongs to no time, offering a fascinating glimpse into what rock music today might have been.`,
+            ],
+        },
+        {
+            paragraph: `To bide his time during indie rock's era of peak influence, Bennett learned new creative skills such as music composition, web development, and illustration. This allowed him to build the audio webcomic, which presents the album's story as a theatrical play, with illustrated characters and stage props that enter and exit as the music progresses. Annotations offer background context for the lyrics, and links to Wikipedia explain obscure references. Meanwhile, full music scores offer insight into how each song was written.`,
+        },
+        {
+            paragraph: [
+                // TODO: Revise cultivated talent part.
+                `Thanks to Donald Trump's failed presidency, fewer of us accept the standard indie rock wisdom that winning over live crowds is the best signal of future potential, or that the serendipity of willful naivete should always triumph over the cumulative talent of lifelong ambition. Perhaps it's now time to reconsider`,
                 {
                     isItalic: true,
                     lyric: `all`,
                 },
-                `our inherited beliefs. For example, by asking artists to do the work of the labels themselves, indie rock built redundancy into the system. But this makes the whole less than the sum of its parts! What if our generation's gift to rock music lies in the rediscovery of interdependence— which allows the whole to be`,
+                `our inherited wisdom. For example, by asking artists to do the work of the labels themselves, indie rock built redundancy into the system. But doesn't this make the whole less than the sum of its parts? What if the next generation's contribution to rock music will be its rediscovery of interdependence— allowing the whole to be`,
                 {
                     isItalic: true,
                     lyric: `greater`,
--- admin/tasks/completed/general/index.js
+++ admin/tasks/completed/general/index.js
@@ -5141,7 +5141,7 @@ export default [
     },
     {
         taskName: `Promo content.`,
-        workedHours: 93,
+        workedHours: 100,
     },
     {
         taskName: `Promo pages.`,
