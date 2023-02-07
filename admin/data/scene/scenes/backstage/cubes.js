1506dc3f95548b4020ca5abd4f2d22a58076d478
Bennett Lin <bennettslin@gmail.com>
2023-02-06 23:11:45 -0800

    Reposition backstage curtains.

--- admin/data/scene/scenes/backstage/cubes.js
+++ admin/data/scene/scenes/backstage/cubes.js
@@ -1,14 +1,19 @@
 import { BACKSTAGE_KEY } from '../../../../../src/constants/scene/scenes'
-import { GROUND_CLEAR_COLOUR as G } from '../../../../../src/constants/scene/cubes/colours'
+import {
+    GROUND_CLEAR_COLOUR as G,
+    PLATFORM_COLOUR as P,
+} from '../../../../../src/constants/scene/cubes/colours'
 
 export default {
     [BACKSTAGE_KEY]: {
         floor: {
             hslaColours: [
                 [G],
+                [P],
             ],
             zIndices: [
                 [0],
+                [1],
             ],
         },
     },
--- admin/data/scene/scenes/backstage/things.js
+++ admin/data/scene/scenes/backstage/things.js
@@ -39,43 +39,43 @@ export default {
     },
     [FLAT]: {
         [THEATRE_DRAPE_SIDE__LEFT]: {
-            yIndex: 0,
+            yIndex: 1,
             xPosition: 0,
         },
         [THEATRE_DRAPE_SIDE__RIGHT]: {
-            yIndex: 0,
+            yIndex: 1,
             xPosition: 11,
         },
         [THEATRE_DRAPE_TOP]: {
-            yIndex: 0,
-            zOffset: 7,
-            layerYIndex: -1,
+            yIndex: 1,
+            zOffset: 6.5,
+            layerYIndex: 0,
         },
         [THEATRE_LEG_SIDE__BACK_LEFT]: {
-            yIndex: 4,
+            yIndex: 2,
             xPosition: 0,
         },
         [THEATRE_LEG_SIDE__BACK_RIGHT]: {
-            yIndex: 4,
+            yIndex: 2,
             xPosition: 11,
         },
         [THEATRE_LEG_SIDE__FRONT_LEFT]: {
-            yIndex: 2,
+            yIndex: 3,
             xPosition: 0,
         },
         [THEATRE_LEG_SIDE__FRONT_RIGHT]: {
-            yIndex: 2,
+            yIndex: 3,
             xPosition: 11,
         },
         [THEATRE_LEG_TOP__BACK]: {
-            yIndex: 4,
-            zOffset: 7,
-            layerYIndex: 3,
+            yIndex: 2,
+            zOffset: 6.5,
+            layerYIndex: 1,
         },
         [THEATRE_LEG_TOP__FRONT]: {
-            yIndex: 2,
-            zOffset: 7,
-            layerYIndex: 1,
+            yIndex: 3,
+            zOffset: 6.5,
+            layerYIndex: 2,
         },
     },
 }
