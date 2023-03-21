26ee1608b48878b05c3fe28f68aa619c73039199
Bennett Lin <bennettslin@gmail.com>
2023-03-20 19:24:34 -0700

    Add bubbles to mermaid presence.

--- admin/data/scene/sharedStyles/actors/sashaBennett/index.js
+++ admin/data/scene/sharedStyles/actors/sashaBennett/index.js
@@ -4,9 +4,10 @@ import {
 } from '../../../../../../src/constants/scene/actors/sashaBennett'
 import { EPILOGUE } from '../../../../../../src/constants/scene/actors/songs'
 import { PAST__SHARED } from '../../../../../../src/constants/scene/sharedStyles'
+import { CONSTELLATION__SHARED } from '../../../../../../src/constants/scene/sharedStyles/comic'
 
 export default {
-    [MERMAID]: null,
+    [MERMAID]: CONSTELLATION__SHARED,
     [COMFORTING_HUMILIATED]: PAST__SHARED,
     [EPILOGUE]: null,
 }
