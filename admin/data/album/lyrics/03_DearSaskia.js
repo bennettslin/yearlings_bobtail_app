afb3195b700b46d17a647249037fd4e1690cc1ae
Bennett Lin <bennettslin@gmail.com>
2023-11-06 13:45:19 -0800

    Revise a Saskia annotation.

--- admin/data/album/lyrics/03_DearSaskia.js
+++ admin/data/album/lyrics/03_DearSaskia.js
@@ -31,8 +31,8 @@ export default {
     // This is a magic number adjusted by ear. In the mix, it's actually 44.96.
     mixSamplingFrequency: 45.39,
     todo: true,
-    problem: `Decide if the em-dash really is the best way to break up those two statements.`,
-    overview: `Sasha was a childhood friend as well as my first crush. The strength she drew from her Jewish identity fascinated me, but it also clued me in on a cruel paradox— namely, that nothing ever comes from longing to share in someone's faith and roots, which stay strong precisely by not being shared. Sasha moved away during middle school at the peak of my struggles with bullies. That summer, I wrote her a letter on a family trip to Taiwan.`,
+    problem: `Decide if this is the best way to break up those two statements.`,
+    overview: `Sasha was a childhood friend, as well as my first crush. The strength she drew from her Jewish identity fascinated me, but it also clued me in on a cruel paradox: Namely, that nothing ever comes from longing to share in someone's faith and roots, which stay strong precisely by not being shared. Sasha moved away during middle school, at the peak of my struggles with bullies. That summer, I wrote her a letter on a family trip to Taiwan.`,
     lyricUnits: [
         {
             unitMap: {},
