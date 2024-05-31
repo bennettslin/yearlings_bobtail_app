c10e5d954bf033e1a4fb52cdc6861e43b89fcce0
Bennett Lin <bennettslin@gmail.com>
2024-05-31 13:32:38 -0700

    Revise prologue overview.

--- admin/data/album/lyrics/00_Prologue.js
+++ admin/data/album/lyrics/00_Prologue.js
@@ -4,11 +4,12 @@ export default {
     todo: true,
     problem: `Review.`,
     overview: [
-        `Hi, I'm Bennett! A long while back, I went through an awful breakup. I then spent the next five years writing all the songs and recording all the parts for`,
+        `Hi, I'm Bennett! A long time ago, while going through a difficult breakup, I set out to make`,
         {
             isItalic: true,
             lyric: `Yearling's Bobtail`,
         },
-        `, an epic double album examining all that had led up to my life at that point, from growing up with an autistic brother to joining a mail-order cult. When I submitted my demo to the indie labels and press, however, they all dismissed it as a vanity project. Since then, I've struggled to bring this complex and dense work to the public, given that I remain a complete unknown. Hopefully, the seven additional years I've now spent coding, illustrating, and annotating this audio webcomic will do the trick!`,
+        `, a double album examining all that had led up to my life at that point, from growing up with an autistic brother to joining a mail-order cult. I then spent the next five years writing all the songs and recording all the parts. Once it was finished, I submitted promo copies to the indie labels and press; unfortunately, they all dismissed it as a vanity project. Ever since, I've struggled to bring this complex and dense work to the public, given that I remain a complete unknown. Hopefully, the seven additional years I've now spent coding, illustrating, and annotating this audio webcomic will do the trick!`,
+        ``,
     ],
 }
