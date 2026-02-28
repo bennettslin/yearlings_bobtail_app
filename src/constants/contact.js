0f197f50ee8a3432ebfb818c3c0bf5eca813910a
Bennett Lin <bennettslin@gmail.com>
2026-02-28 00:11:39 -0800

    Change about text.

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,5 +1,5 @@
 export const CONTACT_TEXT = [
-    `We'd love to hear from record labels, the music press, and anyone with an offer or opportunity for us to consider. Please email `,
+    `We'd love to hear from record labels, the music press, and anyone with an opportunity or offer for us to consider. Please email `,
     {
         isContact: true,
     },
