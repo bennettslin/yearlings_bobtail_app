80d5eefd09d1b9dac213b965cb11f31b2b09ce21
Bennett Lin <bennettslin@gmail.com>
2026-07-20 16:30:12 -0700

    Revise contact text.

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,5 +1,5 @@
 export const CONTACT_TEXT = [
-    `We'd love to hear from record labels, the music press, and anyone with an opportunity or offer for us to consider. Please email `,
+    `We'd love to hear from anyone with an opportunity or offer for us to consider. Please email us at `,
     {
         isContact: true,
     },
