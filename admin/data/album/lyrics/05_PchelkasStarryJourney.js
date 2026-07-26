913d29129a126b79c631f678389597cbaf2d89e8
Bennett Lin <bennettslin@gmail.com>
2026-07-26 02:33:28 -0700

    Revise Pchelka annotation.

--- admin/data/album/lyrics/05_PchelkasStarryJourney.js
+++ admin/data/album/lyrics/05_PchelkasStarryJourney.js
@@ -802,12 +802,12 @@ export default {
                             anchor: `Paul Pena`,
                             wiki: `Paul_Pena`,
                         },
-                        `. Luckily, I found a book on the subject in a campus library, and within days I was sounding the main`,
+                        `. Luckily, I found a book on the subject in a campus library, so within days I was sounding all the`,
                         {
                             anchor: `partials`,
                             wiki: `Harmonic_series_(music)`,
                         },
-                        `. This honestly took Catherine by surprise, and thus impressed her to no end. I've been trying to recapture the glory of that moment ever since.`,
+                        `. This took Catherine by surprise, and thus impressed her to no end; my life ever since has been about recapturing the glory of that moment.`,
                     ],
                     dotKeys: {
                         [BACKSTORY]: true,
